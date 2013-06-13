621d9ab825456530e1cf4cbff183b8bcdcc419d4
Bryan Jensen <bryanjensen@horus.(none)>
2013-06-13 17:50:26 -0400

    Minor changes to analysis page, label changing fully implemented (by Julia)

--- analysis.py
+++ analysis.py
@@ -71,7 +71,7 @@ def dendrogram(transposed, names, folder, linkage_method, distance_metric, pruni
 #-----------------TITLE-----------------------------------------------------------
 #change to what the user wants to type in, and if they type nothing leave title blank
 	
-	FN = title	
+	FN = title
 #---------------------------------------------------------------------------------
 	
 	
@@ -83,29 +83,28 @@ def dendrogram(transposed, names, folder, linkage_method, distance_metric, pruni
 
 #_______________________________________________________________________________________
 
-# Subplot allows two plots on the same figure, 2 - two rows , 1- one column, 1 - top subplot(row one)
+	# Subplot allows two plots on the same figure, 2 - two rows , 1- one column, 1 - top subplot(row one)
 	pyplot.subplot(2,1,1)
-#creates a title for the figure, sets size to TITLE_FONT_SIZE	
+	# creates a title for the figure, sets size to TITLE_FONT_SIZE	
 	pyplot.title(FN, fontsize = TITLE_FONT_SIZE)
 
-#Creates the dendrogram
 	hierarchy.dendrogram(Z, p=pruning, truncate_mode="lastp", labels=names, leaf_rotation=LEAF_ROTATION_DEGREE, orientation=orientation)
 
-# second of the subplot 2 - two rows , 1- one column, 2 - bottom subplot(row 2)
+	# second of the subplot 2 - two rows , 1- one column, 2 - bottom subplot(row 2)
 	pyplot.subplot(2,1,2)
-#turns the border off
+	# disables border
 	pyplot.axis("off")
-#turns of the tick marks off
+	# disabled tick marks
 	pyplot.xticks([]), pyplot.yticks([])
 
-#builds the texts from what the user chose and sets them to have CHARACTERS_PER_LINE_IN_LEGEND (how many characters you want on each line in the second subplot)
+	#builds the texts from what the user chose and sets them to have CHARACTERS_PER_LINE_IN_LEGEND (how many characters you want on each line in the second subplot)
 	wrappedscrubo = textwrap.fill("Scrubbing Options: " + str(session['scrubbingoptions']), CHARACTERS_PER_LINE_IN_LEGEND)
 
 	wrappedcuto = textwrap.fill("Cutting Options: " + str(session['cuttingoptions']), CHARACTERS_PER_LINE_IN_LEGEND)
 
 	wrappedanalyzeo = textwrap.fill("Analyzing Options: " + str(session['analyzingoptions']), CHARACTERS_PER_LINE_IN_LEGEND)
 
-#puts the text into the second subplot with two blank lines in between each text	
+	#puts the text into the second subplot with two blank lines in between each text	
 	pyplot.text(0,1.001, wrappedscrubo+ "\n\n" + wrappedcuto + "\n\n" + wrappedanalyzeo, ha = 'left', va = 'top', size = LEGEND_FONT_SIZE, alpha = .5)
 	#text(.5,.5, wrappedcuto, ha = 'center', va = 'center', size = 14, alpha = .5)
 	#text(.5,.2, wrappedanalyzeo, ha = 'center', va = 'center', size = 14, alpha = .5)
