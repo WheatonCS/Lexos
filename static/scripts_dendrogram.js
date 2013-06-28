ed8ed3183a663014a1755c36e1e39e12fb3acb4f
Devin Delfino <devindelfino@meret.(none)>
2013-06-28 16:27:06 -0400

    tooltips for supercutting/cutset labelling, added multiple alert boxes on dendrogram.html and cut.html

--- static/scripts_dendrogram.js
+++ static/scripts_dendrogram.js
@@ -5,3 +5,23 @@ $(function() {
 		$("#modifylabels").slideToggle(timeToToggle);
 	});
 });
+
+function attemptdendro() {
+	var activeFiles = $('.filenames').length;
+	if (activeFiles == 0) {
+		alert("There are currently no active files.");
+	}
+	else if (activeFiles == 1) {
+		alert("A dendrogram requires at least 2 active files to be created (Currently 1 active file).");
+	}
+	else {
+		return true;
+	}
+	return false;
+}
+
+$(function() {
+	$('#getdendro').click( function() {
+		return attemptdendro();
+	});
+});
