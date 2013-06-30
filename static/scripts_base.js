42a7b08c9d66fba397fbc86ab2e82455e392adc5
Bryan Jensen <bryanjensen@horus.(none)>
2013-06-30 19:42:09 -0400

    Removing javascript cookies behavior - seems to break the server

--- static/scripts_base.js
+++ static/scripts_base.js
@@ -1,14 +1,4 @@
 $(function() {
 	$("form").submit(function() {
-		if (!$(this).prop('name') == 'reset') {
-			if (/nofiles/.test(document.cookie)) {
-				$("#submiterrormessage1").show().fadeOut(1200, "easeInOutCubic");
-				return false;
-			}
-		}
-	});
-
-	$("#bttn-restart").click(function() {
-		document.cookie = 'nofiles';
 	});
 });
