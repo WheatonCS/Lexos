bda5bd7457a62e06787e4b04c286ec8e8692bea7
Clayton Rieck <cjrieck123@gmail.com>
2014-05-14 23:53:45 -0400

    cleaning css

---
+++ static/scripts_scrub.js
@@ -0,0 +1,100 @@
+$(function() {
+
+	var timeToToggle = 300;
+
+	function displayFileName(ev) {
+		var files = ev.target.files || ev.dataTransfer.files;
+
+		for (var i = 0, file; file = files[i]; i++) {
+			var label = $("#"+ev.target.id).parent().find("#"+ev.target.id+"bttnlabel");
+			label.html(file.name);
+			label.css('color', '#00B226');
+			$("#usecache"+ev.target.id).attr('disabled', 'disabled');
+		}
+	}
+
+	function changePreviewHeight() {
+		var optionsHeight = $("#alloptions").height();
+		$("#preview").stop().animate({
+			height: optionsHeight
+		}, timeToToggle);
+	}
+
+	$("#swfileselect").change(displayFileName);
+	$("#lemfileselect").change(displayFileName);
+	$("#consfileselect").change(displayFileName);
+	$("#scfileselect").change(displayFileName);
+
+	$(".bttnfilelabels").click( function() {
+		var filetype = $(this).attr('id').replace('bttnlabel', '');
+		if ($("#usecache"+filetype).attr('disabled') != 'disabled') {
+			$(this).css('color', '#FF0000');
+			$(this).text($(this).text().replace('(using stored)', ''));
+			$("#usecache"+filetype).attr('disabled', 'disabled');
+		}
+	});
+
+	//-----------------------------------------------------
+	// var timeToToggle = 300;
+	$("#stopwords").click( function() {
+		$("#stopwordinput").slideToggle(timeToToggle, function(){
+			changePreviewHeight();
+		});
+	});
+	$("#prettystopwordsupload").click( function() {
+		$("#swfileselect").click();
+	});
+	//-----------------------------------------------------
+	$("#lemmas").click( function() {
+		$("#lemmainput").slideToggle(timeToToggle, function(){
+			changePreviewHeight();
+		});
+	});
+	$("#prettylemmasupload").click( function() {
+		$("#lemfileselect").click();
+	});
+	//-----------------------------------------------------
+	$("#consolidations").click( function() {
+		$("#consolidationsinput").slideToggle(timeToToggle, function(){
+			changePreviewHeight();
+		});
+	});
+	$("#prettyconsolidationsupload").click( function() {
+		$("#consfileselect").click();
+	});
+	//-----------------------------------------------------
+	$("#specialchars").click( function() {
+		$("#specialcharsinput").slideToggle(timeToToggle, function(){
+			changePreviewHeight();
+		});
+	});
+	$("#prettyspecialcharsupload").click( function() {
+		$("#scfileselect").click();
+	});
+});
+
+
+
+$(function() {
+	var timeToToggle = 100;
+	$("#punctbox").click( function() {
+		if ($(this).children('input').is(':checked')) {
+			$("#aposhyph").fadeOut(timeToToggle);
+		}
+		else {
+			$("#aposhyph").fadeIn(timeToToggle);
+		}
+	});
+});
+
+$(function() {
+	$(document).tooltip({
+		position:{
+			relative: true,
+			at: "center top-500", // location on the mouse
+								  // Negative horizontal is left, negative vertical is up 
+			my: "center bottom",  // location on the tooltip popup window
+			collision: "fit"
+		}
+	});
+});
