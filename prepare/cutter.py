22f16c187f061dbc3e5fcd84e9474759eb721e7d
Bryan Jensen <bryanawjensen@gmail.com>
2014-05-20 17:14:35 -0400

    Commenting

--- prepare/cutter.py
+++ prepare/cutter.py
@@ -20,21 +20,17 @@ def stripFrontWhitespace(q):
 
 def cut(text, cuttingValue=2, cuttingBySize=True, overlap=0, lastProp='50%'):
 	"""
-	Cuts each text file into various segments according to the options chosen by the user.
-
-	*Called in cut() in lexos.py
+	Cuts each text string into various segments according to the options chosen by the user.
 
 	Args:
-		filepath: A string representing the path to the file.
-		over: A unicode string representing the number of words to be overlapped by each text segment.
-		folder: A string representing the path to the folder where the cut files will be stored.
-		lastProp: A unicode string representing the percentage for the last proportion.
-		cuttingValue: A unicode string representing the value at which the file are cut.
-		cuttingBySize: A boolean distinguishing whether the files are cut by size or number.
+		text: A string the text to be split
+		cuttingValue: A unicode string representing the value by which to cut the files.
+		cuttingBySize: A boolean distinguishing whether the files are cut by number of elements per chunk or number of chunks per text.
+		overlap: A unicode string representing the number of words to be overlapped between each text segment.
+		lastProp: A unicode string representing the minimum proportion percentage the last chunk has to be to not get assimilated by the previous.
 
 	Returns:
-		A dictionary where the integer keys represent the various segments that have been cut,
-		and the string values represent the actual text for each corresponding text segment.
+		A list of strings, each representing a chunk of the original.
 	"""
 	overlap = int(overlap)
 	lastProp = float(lastProp.strip('%')) / 100
@@ -46,14 +42,12 @@ def cut(text, cuttingValue=2, cuttingBySize=True, overlap=0, lastProp='50%'):
 	else:
 		chunkSize = int(ceil(countWords(splitText) / float(cuttingValue)))
 
-	chunkList = []
-	chunkSoFar = Queue()
-	nextChunkSoFar = []
-	i = 0
-	trash = ''
-	tillNextWindow = chunkSize - overlap
+	chunkList = [] # The list of the chunks (a.k.a a list of list of strings)
+	chunkSoFar = Queue() # The rolling window representing the (potential) chunk
+	i = 0 # Index keeping track of whether or not it's time to make a chunk out of the window
+	tillNextChunk = chunkSize - overlap # The distance between the starts of chunks
 
-	# Create list of chunks (chunks are lists of words and whitespace) by using a rolling window
+	# Create list of chunks (chunks are lists of words and whitespace) by using a queue as a rolling window
 	for token in splitText:
 		if token in WHITESPACE:
 			chunkSoFar.put(token)
@@ -64,30 +58,25 @@ def cut(text, cuttingValue=2, cuttingBySize=True, overlap=0, lastProp='50%'):
 			if i > chunkSize:
 				chunkList.append(list(chunkSoFar.queue))
 
-				print "Striping the front off from:"
-				print list(chunkSoFar.queue)
-				for j in xrange(tillNextWindow):
+				for j in xrange(tillNextChunk):
 					stripFrontWhitespace(chunkSoFar)
 					trashWord = chunkSoFar.get()
 
-				print "After:"
-				print list(chunkSoFar.queue)
-
 				stripFrontWhitespace(chunkSoFar)
 
-				i -= tillNextWindow
+				i -= tillNextChunk
 
 			chunkSoFar.put(token)
 
+	# Make sure the last chunk is of a sufficient proportion
 	lastChunk = list(chunkSoFar.queue) # Append the final (partial) chunk
 
 	if (float(countWords(lastChunk)) / chunkSize) < lastProp: # If the proportion of the last chunk is too low
-		print 'Extending the last chunk (it was too small to be on its own)'
 		chunkList[-1].extend(lastChunk)
 	else:
-		print 'Appending as own chunk'
 		chunkList.append(lastChunk)
 
+	# Make the list of lists of strings into a list of strings
 	stringList = [''.join(subList) for subList in chunkList]
 
 	return stringList
