bc6baedf1363ff31c53ed0eacd207fdd703e5362
Clayton Rieck <cjrieck123@gmail.com>
2014-05-22 13:39:26 -0400

    working on buttons. almost done

--- static/js/scripts_base.js
+++ static/js/scripts_base.js
@@ -18,9 +18,11 @@ $(function() {
 	}, 10);
 
 	if ($("#prepare-options").height() >= $("#prepare-previews").height()) {
-		$(".optionsandpreviewwrapper").height($("#prepare-options").height()+60);
+		var newHeight = $("#prepare-options").height()+70;
+		$(".optionsandpreviewwrapper").height(newHeight);
 	} else {
-		$(".optionsandpreviewwrapper").height($("#prepare-previews").height()+60);
+		var newHeight = $("#prepare-previews").height()+70;
+		$(".optionsandpreviewwrapper").height(newHeight);
 	}
 
 	$("form").submit(function() {
@@ -30,6 +32,19 @@ $(function() {
 		}
 	});
 
+	var options = $('#prepare-options');
+	var buttons = $('#prepare-submit');
+
+	var buttonsFixed = true;
+	var lastScrollTop = 0;
+
+	if ((buttons.offset().top) <= ($(window).scrollTop()+options.height())) {
+		buttons.css("top", ($(window).scrollTop()+options.height()).toString());
+	}
+
+	var topHeight = $('#navbardiv').height() + $('header').height();
+	var atBottom = false;
+
 	// throttle scroll event
 	$(window).scroll(function(){
 		if (timer) {
@@ -37,29 +52,47 @@ $(function() {
 		}
 		timer = setTimeout(function(){
 			// fix buttons to bottom
-			var offset = $(".footer").height();
+			var scrollBottom = $(window).scrollTop() + $(window).height() + $('footer').height();
 
-			if($(window).scrollTop() > 90) {
-				$("#prepare-options").addClass("fixed");
+			if (scrollBottom >= $(document).height()){
+				atBottom = true;
 			} else {
-				$("#prepare-options").removeClass("fixed");
+				atBottom = false;
 			}
 
-			if($(window).scrollTop() + $(window).height() < $(document).height()-90) {
-				$("#prepare-submit").addClass("fixed");
+			var optionsBottom = (options.height()+options.offset().top);
 
-				$("#prepare-submit").stop().animate({
-					"background-color": "#fff",
-					opacity: 0.94
-				}, 100);
-			} else {
-				$("#prepare-submit").removeClass("fixed");
+			// if at bottom of the page, remove fixed class for buttons
+			if ((buttons.offset().top >= ($('footer').offset().top-buttons.height())) && buttonsFixed) {
+				buttons.removeClass("fixed");
+				buttonsFixed = false;
+			};
 
-				$("#prepare-submit").stop().animate({
-					"background-color": "none",
-					opacity: 1
-				}, 100);
+			// if bottom of window at bottom of buttons, add fixed class
+			if ((scrollBottom-50 < ($('footer').offset().top)) && !buttonsFixed) {
+				buttons.addClass("fixed");
+				buttonsFixed = true;
+			};
+
+			if ($(window).scrollTop() > topHeight && 
+				options.outerHeight() < ($(document).height()-
+										(buttons.outerHeight()+$('footer').outerHeight()+$('legend').outerHeight())) ){
+				
+				options.removeClass("fixbottom");
+				options.addClass("fixed");
 			}
+
+			// if nav bar and title come back into view
+			if ($(window).scrollTop() <= topHeight) {
+				options.removeClass("fixbottom");
+				options.removeClass("fixed");
+			}
+
+			if (($(document).height()-(buttons.height()*2+$('footer').height())) <= (options.offset().top+options.height())) {
+				options.removeClass("fixed");
+				options.addClass("fixbottom");
+			}
+
 		}, 10);
 	});
 });
