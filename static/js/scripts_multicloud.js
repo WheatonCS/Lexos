5ae8bdff14d409298acce88588ec8d951c21b276
lithiah <lithiah@users.noreply.github.com>
2014-06-19 12:32:51 -0400

    upload file on mc shows file name

--- static/js/scripts_multicloud.js
+++ static/js/scripts_multicloud.js
@@ -4,6 +4,14 @@ $(function() {
 		noneSelectedText: "Select Segments",
 		selectedText: "# of # checked"		
 	});
+
+
+	$('.multicloud-upload').change(function(ev) {
+		filename = ev.target.files[0].name;
+
+		$(this).siblings('.bttnfilelabels').html(filename);
+	});
+
 });
 
 
