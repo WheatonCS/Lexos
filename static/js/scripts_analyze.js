09f7e9d214928a275677855f4d7eda3ac28efd2b
akuisara <zhang_qi@wheatoncollege.edu>
2014-06-24 18:26:41 -0400

    Small changes on hierarchy section

--- static/js/scripts_analyze.js
+++ static/js/scripts_analyze.js
@@ -1,12 +1,10 @@
 $(function() {
 	function updateTokenizeCheckbox() {
 		if ($('input[type=radio][name=tokenType][value=word]').attr('checked')) {
-			console.log('ayy');
 			$('input[type=checkbox][name=inWordsOnly]').attr('disabled', 'true');
 			$('input[type=checkbox][name=inWordsOnly]').parent('label').addClass('disabled');
 		}
 		else {
-			console.log('no');
 			$('input[type=checkbox][name=inWordsOnly]').removeAttr('disabled');
 			$('input[type=checkbox][name=inWordsOnly]').parent('label').removeClass('disabled');
 		}
--- static/js/scripts_dendrogram.js
+++ static/js/scripts_dendrogram.js
@@ -23,12 +23,6 @@ $(function() {
 		}
 		else {
 			var thresholdValue = $('#threshold').val();
-			var inconsistentMax = Number(document.getElementById('inconsistentMax').innerHTML);
-			var maxclustMax = Number(document.getElementById('maxclustMax').innerHTML);
-			var distanceMax = Number(document.getElementById('distanceMax').innerHTML);
-			var distanceMin = Number(document.getElementById('distanceMin').innerHTML);
-			var monocritMax = Number(document.getElementById('monocritMax').innerHTML);
-			var monocritMin = Number(document.getElementById('monocritMin').innerHTML);
 			var cOption = $('#criterion').val();
 			if (cOption == 'inconsistent') {
 				if ((thresholdValue >= 0 && thresholdValue <= inconsistentMax)|| (thresholdValue == '')) {
@@ -70,12 +64,6 @@ $(function() {
 
 	});
 
-	var node = document.getElementById('pdfPageNumber');
-	var pdfPage = Number(node.innerHTML);
-	var pdfHeight = pdfPage * 1491;
-
-	document.getElementById("pdf").height = pdfHeight;
-
 	$('#criterion option').each(function() {
 		$("#tValue").show();
 	});
@@ -88,4 +76,9 @@ $(function() {
 			}
 		});
 	});
+
+	var pdfPage = Number(document.getElementById("pdfPageNumber").innerHTML);
+	var pdfHeight = pdfPage * 1491;
+
+	document.getElementById("pdf").height = pdfHeight;
 });
