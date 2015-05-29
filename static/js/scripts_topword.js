80ab329ff60f6858d8cfcfb34dcd085ae3243750
akuisara <zhang_qi@wheatoncollege.edu>
2015-05-29 10:58:08 -0400

    add more options for topword

--- static/js/scripts_topword.js
+++ static/js/scripts_topword.js
@@ -3,14 +3,29 @@ $(function() {
 	var totalGroups = 2, groupID;
 
 	$("#addOneGroup").click(function() {
-		$("#group-2").clone().appendTo("#addGroups");
+		var $block = $("#group-2");
+
+		var selected = [];
+		console.log($("#group-2 input:checked"));
+
+		$("#group-2 input:checked").each(function() {
+		    selected.push($(this).attr('name'));
+		});
+
+		for(var i=0; i<selected.length;i++){
+			$("#"+selected[i]).removeAttr('checked');
+		}
+
+		var $clone = $block.clone();
+		$clone.appendTo("#addGroups");
 		totalGroups++;
 		groupID = "group-" + String(totalGroups);
 
 		var newGroup = $("#addGroups :last-child");
 		newGroup.attr("id", groupID);
-
 		$("#addGroups :last-child :nth-child(2)").contents().first().replaceWith("Group "+String(totalGroups));
+
+		console.log($(".groupLabels").css("position"));
 	});
 
 	$("#deleteOneGroup").click(function() {
