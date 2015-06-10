fb957d671e06090cc210e6c1b6bd7d0cd597a245
chantisnake <13501393281@163.com>
2015-06-10 18:02:51 -0400

    add error message to the multicloud that will pop up if the file do not have the correct structure

--- analyze/multicloud_topic.py
+++ analyze/multicloud_topic.py
@@ -1,10 +1,10 @@
 import numpy as np
 
-def topicJSONmaker(malletPath):
 
+def topicJSONmaker(malletPath):
     # Configuration
-    inpath = malletPath # Insert the path to the Mallet file here
-    num_top_words = 100 # Top N number of words in each topic that will appear in output
+    inpath = malletPath  # Insert the path to the Mallet file here
+    num_top_words = 100  # Top N number of words in each topic that will appear in output
     mallet_vocab = []
     word_topic_counts = []
     topics = []
@@ -13,15 +13,19 @@ def topicJSONmaker(malletPath):
     n_topics = []
     with open(inpath) as data:
         for line in data:
-            l = line.rstrip().split(' ') # Split the line on spaces
-            id = l[0] # Get the word ID from the first item in the list
-            word = l[1] # Get the word token from the second item in the list
-            l[0:2] = [] # Delete the ID and word from the list
-            topic_count_pairs = [pair.split(':') for pair in l] # New list topic-count pairs
-            for topic, count in topic_count_pairs:
-                n_topics.append(int(topic)) # New list with topics
-    n_topics.sort() # Sort the topics
-    num_topics = max(n_topics) + 1 # The number of topics in the model is the highest in the list
+            try:
+                l = line.rstrip().split(' ')  # Split the line on spaces
+                id = l[0]  # Get the word ID from the first item in the list
+                word = l[1]  # Get the word token from the second item in the list
+                l[0:2] = []  # Delete the ID and word from the list
+                topic_count_pairs = [pair.split(':') for pair in l]  # New list topic-count pairs
+                for topic, count in topic_count_pairs:
+                    n_topics.append(int(topic))  # New list with topics
+            except:
+                raise IOError(
+                    'the file you uploaded cannot be interpreted, please check for error in column structure and then upload again.')
+    n_topics.sort()  # Sort the topics
+    num_topics = max(n_topics) + 1  # The number of topics in the model is the highest in the list
 
     # Re-shape the file data
     with open(inpath) as f:
@@ -44,15 +48,15 @@ def topicJSONmaker(malletPath):
 
     # Generate a topics dictionary
     for t in range(num_topics):
-        top_words_idx = np. argsort(word_topic[:,t])[::-1]
+        top_words_idx = np.argsort(word_topic[:, t])[::-1]
         top_words_idx = top_words_idx[:num_top_words]
         top_words = mallet_vocab[top_words_idx]
         top_words_shares = word_topic[top_words_idx, t]
-        #print("Topic{}".format(t))
+        # print("Topic{}".format(t))
         topics.append({})
         for word, share in zip(top_words, top_words_shares):
-            topics[t].update({word: np.round(share,3)}) # Create the topics dictionary
-            #print("{} : {}".format(word, np.round(share,3)))
+            topics[t].update({word: np.round(share, 3)})  # Create the topics dictionary
+            # print("{} : {}".format(word, np.round(share,3)))
 
     # For Lexos, build the json string
     jsonStr = ""
@@ -61,7 +65,7 @@ def topicJSONmaker(malletPath):
         jsonStr += '{"name": "Topic' + str(i) + '.txt", "children": ['
         children = ""
         for name, size in topics[i].iteritems():
-            children += ', {"text": "%s", "size": %s}' % (name, size*1000)
+            children += ', {"text": "%s", "size": %s}' % (name, size * 1000)
             children = children.lstrip(', ')
         jsonStr += children
         jsonStr += ']}, '
@@ -75,7 +79,7 @@ def topicJSONmaker(malletPath):
         newChildrenlist = []
 
         for name, size in topics[i].iteritems():
-            newChildrenlist.append({"text": name, "size": size*1000})
+            newChildrenlist.append({"text": name, "size": size * 1000})
 
         JSONObj.append({"name": "Topic" + str(i) + ".txt", "children": newChildrenlist})
 
