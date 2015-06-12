e475a8e7ae70d1f3689bd7b06bd36b07a2c0a52f
chantisnake <13501393281@163.com>
2015-06-12 18:29:43 -0400

    add topword to the program

---
+++ analyze/extra.py
@@ -0,0 +1,278 @@
+# -*- coding: utf-8 -*-
+from __future__ import division
+from scipy.spatial.distance import pdist
+from scipy.cluster import hierarchy
+
+"""
+this are some utility function to help do the analysis not only in topword.py
+"""
+from math import sqrt
+from operator import itemgetter
+from matplotlib import mlab
+import matplotlib.pyplot as plt
+
+
+class Files_Information:
+    def __init__(self, WordLists, FileNames):
+        """
+        takes in wordlists and convert that completely to statistic and give anomalies (about file size)
+        :param WordLists: an array contain dictionaries map from word to word count
+                            each dictionary is word count of a particular file
+        :param FileNames: an parallel array of WordLists, contain file name of the files(in order to plot)
+        """
+
+        # initialize
+        NumFile = len(WordLists)
+        FileAnomalyStdE = {}
+        FileAnomalyIQR = {}
+        FileSizes = {}
+        for i in range(NumFile):
+            FileSizes.update({FileNames[i]: sum(WordLists[i].values())})
+
+        # 1 standard error analysis
+        Average_FileSize = sum(FileSizes.values()) / NumFile
+        # calculate the StdE
+        StdE_FileSize = 0
+        for filesize in FileSizes.values():
+            StdE_FileSize += (filesize - Average_FileSize) ** 2
+        StdE_FileSize /= NumFile
+        StdE_FileSize = sqrt(StdE_FileSize)
+        # calculate the anomaly
+        for filename in FileNames:
+            if FileSizes[filename] > Average_FileSize + 2 * StdE_FileSize:
+                FileAnomalyStdE.update({filename: 'large'})
+            elif FileSizes[filename] < Average_FileSize - 2 * StdE_FileSize:
+                FileAnomalyStdE.update({filename: 'small'})
+
+        # 2 IQR analysis
+        TempList = sorted(FileSizes.items(), key=itemgetter(1))
+        Mid = TempList[int(NumFile / 2)][1]
+        Q3 = TempList[int(NumFile * 3 / 4)][1]
+        Q1 = TempList[int(NumFile / 4)][1]
+        IQR = Q3 - Q1
+        # calculate the anomaly
+        for filename in FileNames:
+            if FileSizes[filename] > Mid + 1.5 * IQR:
+                FileAnomalyIQR.update({filename: 'large'})
+            elif FileSizes[filename] < Mid - 1.5 * IQR:
+                FileAnomalyIQR.update({filename: 'small'})
+
+        # pack the data
+        self.NumFile = NumFile  # number of files
+        self.FileSizes = FileSizes  # an array of the total word count of each file
+        self.Average = Average_FileSize  # average file size
+        self.StdE = StdE_FileSize  # standard error of file size
+        self.FileAnomalyStdE = FileAnomalyStdE
+        # an array contain dictionary map anomaly file to how they are different from others(too large or too small)
+        # analyzed in using standard error
+
+        self.Q1 = Q1  # Q1 of a all the file sizes
+        self.Median = Mid  # median of all the file sizes
+        self.Q3 = Q3  # Q1 of a all the file sizes
+        self.IQR = IQR  # Q1 of a all the file sizes
+        self.FileAnomalyIQR = FileAnomalyIQR
+        # an array contain dictionary map anomaly file to how they are different from others(too large or too small)
+        # analyzed in using IQR
+
+    def list(self):
+        """
+        print all the statistics in a good manner
+
+        """
+        print
+        print 'average:', self.Average, ' standard error:', self.StdE
+        print 'file size anomaly calculated using standard error:', self.FileAnomalyStdE
+        print 'median:', self.Median, ' Q1:', self.Q1, ' Q3:', self.Q3, ' IQR', self.IQR
+        print 'file size anomaly calculated using IQR:', self.FileAnomalyIQR
+
+    def plot(self):
+        """
+        plot a bar chart to represent the statistics
+        x is the file name
+        y is the file size(using word count to represent)
+        """
+        plt.bar(range(self.NumFile), self.FileSizes.values(), align='center')
+        plt.xticks(range(self.NumFile), self.FileSizes.keys())
+        plt.xticks(rotation=50)
+        plt.xlabel('File Name')
+        plt.ylabel('File Size(in term of word count)')
+        plt.show()
+
+    def returnstatistcs(self):
+        """
+        :return: a dictionary map the statistic name to the actual statistics
+        """
+        return {'average': self.Average,
+                'StdE': self.StdE,
+                'fileanomalyStdE': self.FileAnomalyStdE,
+                'median': self.Median,
+                'Q1': self.Q1,
+                'Q3': self.Q3,
+                'IQR': self.IQR,
+                'fileanomalyIQR': self.FileAnomalyIQR}
+
+
+class Word_Information:
+    def __init__(self, WordList, FileName):
+        """
+        takes a WordList of a file and the file name of that file to give statistics of that particular file
+        :param WordList: an dictionary map word to word count representing the word count of particular file
+        :param FileName: the file name of that file
+        """
+
+        # initialize
+        NumWord = len(WordList)
+        TotalWordCount = sum(WordList.values())
+        # 1 standard error analysis
+        AverageWordCount = TotalWordCount / NumWord
+        # calculate the StdE
+        StdEWordCount = 0
+        for WordCount in WordList.values():
+            StdEWordCount += (WordCount - AverageWordCount) ** 2
+        StdEWordCount /= NumWord
+        StdEWordCount = sqrt(StdEWordCount)
+
+        # 2 IQR analysis
+        TempList = sorted(WordList.items(), key=itemgetter(1))
+        Mid = TempList[int(NumWord / 2)][1]
+        Q3 = TempList[int(NumWord * 3 / 4)][1]
+        Q1 = TempList[int(NumWord / 4)][1]
+        IQR = Q3 - Q1
+
+        # pack the data
+        self.FileName = FileName
+        self.NumWord = NumWord
+        self.TotalWordCount = TotalWordCount
+        self.WordCount = WordList
+        self.Average = AverageWordCount
+        self.StdE = StdEWordCount
+        self.Q1 = Q1
+        self.Median = Mid
+        self.Q3 = Q3
+        self.IQR = IQR
+
+    def list(self):
+        """
+        print all the statistics in a good manner
+
+        """
+
+        print
+        print 'information for', "'" + self.FileName + "'"
+        print 'total word count:', self.TotalWordCount
+        print '1. in term of word count:'
+        print '    average:', self.Average, ' standard error:', self.StdE
+        print '    median:', self.Median, ' Q1:', self.Q1, ' Q3:', self.Q3, ' IQR', self.IQR
+        print '2. in term of probability'
+        print '    average:', self.Average / self.TotalWordCount, ' standard error:', self.StdE / self.TotalWordCount
+        print '    median:', self.Median / self.TotalWordCount, ' Q1:', self.Q1 / self.TotalWordCount, \
+            ' Q3:', self.Q3 / self.TotalWordCount, ' IQR', self.IQR / self.TotalWordCount
+
+    def plot(self, num_bins=0):
+        """
+        draw a histogram to represent the data
+        :param num_bins: number of bars, default is (Number different word in the file )/ 2,
+                            if it is too large take 50 as default (see '#default of num_bins')
+        """
+        # plot data
+        mu = self.Average  # mean of distribution
+        sigma = self.StdE  # standard deviation of distribution
+        if num_bins == 0:  # default of num_bins
+            num_bins = min([self.NumWord / 2, 50])
+        # the histogram of the data
+        n, bins, patches = plt.hist(self.WordCount.values(), num_bins, normed=1, facecolor='green', alpha=0.5)
+        # add a 'best fit' line
+        y = mlab.normpdf(bins, mu, sigma)
+        plt.plot(bins, y, 'r--')
+        plt.xlabel('Word Count')
+        plt.ylabel('Probability(how many word has this word count)')
+        plt.title(r'Histogram of word count: $\mu=' + str(self.Average) + '$, $\sigma=' + str(self.StdE) + '$')
+
+        # Tweak spacing to prevent clipping of ylabel
+        plt.subplots_adjust(left=0.15)
+        plt.show()
+
+    def returnstatistics(self):
+        """
+        you are on your own pal. Mimic FileInformation.retrunstatistics().
+
+        """
+        pass
+
+
+def merge_list(wordlists):
+    """
+    this function merges all the wordlist(dictionary) into one, and return it
+
+    :param wordlists: an array contain all the wordlist(dictionary type)
+    :return: the merged word list (dictionary type)
+    """
+    mergelist = {}
+    for wordlist in wordlists:
+        for key in wordlist.keys():
+            try:
+                mergelist[key] += wordlist[key]
+            except:
+                mergelist.update({key: wordlist[key]})
+    return mergelist
+
+
+def loadstastic(file):
+    """
+    this method takes an ALREADY SCRUBBED chunk of file(string), and convert that into a WordLists
+    (see :return for this function or see the document for 'test' function, :param WordLists)
+
+    :param file: a string contain an AlREADY SCRUBBED file
+    :return: a WordLists: Array type
+            each element of array represent a chunk, and it is a dictionary type
+            each element in the dictionary maps word inside that chunk to its frequency
+    """
+    Words = file.split()
+    Wordlist = {}
+    for word in Words:
+        try:
+            Wordlist[word] += 1
+        except:
+            Wordlist.update({word: 1})
+    return Wordlist
+
+
+def matrixtodict(matrix):
+    """
+    convert a word matrix(which is generated in getMatirx() method in ModelClass.py) to
+    the one that is used in the test() method in this file.
+
+    :param matrix: the count matrix generated by getMatrix method
+    :return: a Result Array(each element is a dict) that test method can use
+    """
+
+    ResultArray = []
+    for i in range(1, len(matrix)):
+        ResultDict = {}
+        for j in range(1, len(matrix[0])):
+            ResultDict.update({matrix[0][j]: matrix[i][j]})
+        ResultArray.append(ResultDict)
+    return ResultArray
+
+
+def dicttomatrix(WordLists):
+    # convert into matrix
+    Totallist = merge_list(WordLists)
+    Matrix = []
+    Words = Totallist.keys()
+    wordlistnum = 0
+    for wordlist in WordLists:
+        row = []
+        for key in Totallist.keys():
+            try:
+                row.append(wordlist[key])
+            except KeyError:
+                row.append(0)
+        Matrix.append(row)
+        wordlistnum += 1
+
+    return Matrix, Words
+
+
+if __name__ == "__main__":
+    print dicttomatrix([{'la': 2, 'he': 10}, {'he': 3}, {'la': 3, 'he': 2}, {'lalala': 3, 'la': 2, 'he': 10}])
---
+++ analyze/topword.py
@@ -0,0 +1,322 @@
+# -*- coding: utf-8 -*-
+from __future__ import division
+
+# this program detects word anomaly using z-test for proportion and krusal wallis test
+# assume the possibility of a particular word appear in a text follows normal distribution
+# this program can be optimized in many way.
+from math import sqrt
+from operator import itemgetter
+from scipy.stats.stats import zprob
+from extra import merge_list, matrixtodict
+from scipy.stats.mstats import kruskalwallis
+import numpy.ma as ma
+
+
+def ztest(p1, pt, n1, nt):
+    """
+    this method examine whether a particular word in a particular chunk is an anomaly compare to all rest of the chunks
+    usually we think it is an anomaly if the return value is less than 0.05
+
+    :param p1: the probability of a word's occurrence in a particular chunk:
+                Number of word(the word we care about) occurrence in the chunk/ total word count in the chunk
+
+    :param pt: the probability of a word's occurrence in all the chunks(or the whole passage)
+                Number of word(the word we care about) occurrence in all the chunk/ total word count in all the chunk
+
+    :param n1: the number word in the chunk we care about (total word count)
+    :param nt: the number word in all the chunk selected (total word count)
+    :return: the probability that the particular word in a particular chunk is NOT an anomaly
+    """
+
+    p = (p1 * n1 + pt * nt) / (n1 + nt)
+    try:
+        standard_error = sqrt(p * (1 - p) * ((1 / n1) + (1 / nt)))
+        # print 'standard_error:', standard_error
+        z_scores = (p1 - pt) / standard_error
+        # print 'z_score', z_scores
+        p_values = (1 - zprob(abs(z_scores))) * 2
+        # print 'p_value:', p_values
+        return p_values
+    except:
+        return 'Insignificant'
+
+
+
+def wordfilter(option, Low, High, NumWord, TotalWordCount, MergeList):
+    # option
+    if option == 'CustomP':
+        pass
+
+    elif option == 'CustomF':
+        Low /= NumWord
+        High /= NumWord
+
+    elif option.endswith('StdE'):
+        StdE = 0
+        Average = TotalWordCount / NumWord
+        for word in MergeList:
+            StdE += (MergeList[word] - Average) ** 2
+        StdE = sqrt(StdE)
+        StdE /= NumWord
+
+        if option.startswith('Top'):
+            # TopStdE: only analyze the Right outlier of word, determined by standard deviation
+            Low = (Average + 2 * StdE) / NumWord
+
+        elif option.startswith('Mid'):
+            # MidStdE: only analyze the Non-Outlier of word, determined by standard deviation
+            High = (Average + 2 * StdE) / NumWord
+            Low = (Average - 2 * StdE) / NumWord
+
+        elif option.startswith('Low'):
+            # LowStdE: only analyze the Left Outlier of word, determined by standard deviation
+            High = (Average - 2 * StdE) / NumWord
+
+        else:
+            print('input error')
+            exit(-1)
+
+    elif option.endswith('IQR'):
+        TempList = sorted(MergeList.items(), key=itemgetter(1))
+        Mid = TempList[int(NumWord / 2)][1]
+        Q3 = TempList[int(NumWord * 3 / 4)][1]
+        Q1 = TempList[int(NumWord / 4)][1]
+        IQR = Q3 - Q1
+
+        if option.startswith('Top'):
+            # TopIQR: only analyze the Top outlier of word, determined by IQR
+            Low = (Mid + 1.5 * IQR) / TotalWordCount
+
+        elif option.startswith('Mid'):
+            # MidIQR: only analyze the non-outlier of word, determined by IQR
+            High = (Mid + 1.5 * IQR) / TotalWordCount
+            Low = (Mid - 1.5 * IQR) / TotalWordCount
+
+        elif option.startswith('Low'):
+            # LowIQR: only analyze the Left outlier of word, determined by IQR
+            High = (Mid - 1.5 * IQR) / TotalWordCount
+
+        else:
+            print('input error')
+            exit(-1)
+
+    else:
+        print('input error')
+        exit(-1)
+    return High, Low
+
+
+def testall(WordLists, option='CustomP', Low=0.0, High=1.0):
+    """
+    this method takes Wordlist and and then analyze each single word(*compare to the total passage(all the chunks)*),
+    and then pack that into the return
+
+    :param WordLists:   Array
+                        each element of array represent a chunk, and it is a dictionary type
+                        each element in the dictionary maps word inside that chunk to its frequency
+
+    :param option:  some default option to set for High And Low(see the document for High and Low)
+                    1. using standard deviation to find outlier
+                        TopStdE: only analyze the Right outlier of word, determined by standard deviation
+                                    (word frequency > average + 2 * Standard_Deviation)
+                        MidStdE: only analyze the Non-Outlier of word, determined by standard deviation
+                                    (average + 2 * Standard_Deviation > word frequency > average - 2 * Standard_Deviation)
+                        LowStdE: only analyze the Left Outlier of word, determined by standard deviation
+                                    (average - 2 * Standard_Deviation > word frequency)
+
+                    2. using IQR to find outlier *THIS METHOD DO NOT WORK WELL, BECAUSE THE DATA USUALLY ARE HIGHLY SKEWED*
+                        TopIQR: only analyze the Top outlier of word, determined by IQR
+                                    (word frequency > median + 1.5 * Standard)
+                        MidIQR: only analyze the non-outlier of word, determined by IQR
+                                    (median + 1.5 * Standard > word frequency > median - 1.5 * Standard)
+                        LowIQR: only analyze the Left outlier of word, determined by IQR
+                                    (median - 1.5 * Standard > word frequency)
+
+    :param Low:  this method will only analyze the word with higher frequency than this value
+                    (this parameter will be overwritten if the option is not 'Custom')
+    :param High: this method will only analyze the word with lower frequency than this value
+                    (this parameter will be overwritten if the option is not 'Custom')
+
+    :return:    contain a array
+                each element of array is a array, represent a chunk and it is sorted via p_value
+                each element array is a tuple: (word, corresponding p_value)
+    """
+
+    # init
+    MergeList = merge_list(WordLists)
+    AllResults = []  # the value to return
+    TotalWordCount = sum(MergeList.values())
+    NumWord = len(MergeList)
+
+    High, Low = wordfilter(option, Low, High, NumWord, TotalWordCount, MergeList)
+
+    # calculation
+    for wordlist in WordLists:
+        ResultList = {}
+        ListWordCount = sum(wordlist.values())
+
+        for word in wordlist.keys():
+            if Low < MergeList[word] / TotalWordCount < High:
+                p_value = ztest(wordlist[word] / ListWordCount, MergeList[word] / TotalWordCount,
+                                ListWordCount, TotalWordCount)
+                ResultList.update({word: p_value})
+
+        ResultList = sorted(ResultList.items(), key=itemgetter(1))
+        AllResults.append(ResultList)
+
+    return AllResults
+
+
+def sort(word_p_lists):
+    """
+    this method combine all the diction in word_p_list(word with its p_value) into totallist,
+    with a mark to indicate which file the element(word with p_value) belongs to
+    and then sort the totallist, to give user a clean output of which word in which file is the most abnormal
+
+    :param word_p_lists: a array of dictionary
+                            each element of array represent a chunk, and it is a dictionary type
+                            each element in the dictionary maps word inside that chunk to its p_value
+    :return: a array of tuple type (sorted via p_value):
+                each element is a tuple:    (the chunk it belong(the number of chunk in the word_p_list),
+                                            the word, the corresponding p_value)
+
+    """
+    totallist = []
+    i = 0
+    for list in word_p_lists:
+        templist = []
+        for word in list:
+            if not word[1] == 'Insignificant':
+                temp = ('junk', i + 1) + word  # add the 'junk' to make i+1 a tuple type
+                temp = temp[1:]
+                templist.append(temp)
+        totallist += templist
+        i += 1
+
+    totallist = sorted(totallist, key=lambda tup: tup[2])
+
+    return totallist
+
+
+def groupdivision(WordLists, ChunkMap):
+    # Chunk test, make sure no two chunk are the same
+    for i in range(len(ChunkMap)):
+        for j in range(i + 1, len(ChunkMap)):
+            if ChunkMap[i] == ChunkMap[j]:
+                raise Exception('Chunk ' + str(i) + ' and Chunk ' + str(j) + ' is the same')
+
+    # pack the Chunk data in to ChunkMap(because this is fast)
+    for i in range(len(ChunkMap)):
+        for j in range(len(ChunkMap[i])):
+            ChunkMap[i][j] = WordLists[ChunkMap[i][j]]
+    return ChunkMap
+
+
+def testgroup(GroupWordLists, option='CustomP', Low=0.0, High=1.0):
+    """
+    this method takes ChunkWordlist and and then analyze each single word(compare to all the other group),
+    and then pack that into the return
+
+    :param GroupWordLists:   Array
+                        each element of array represent a chunk, and it is a dictionary type
+                        each element in the dictionary maps word inside that chunk to its frequency
+
+    :param option:  some default option to set for High And Low(see the document for High and Low)
+                    1. using standard deviation to find outlier
+                        TopStdE: only analyze the Right outlier of word, determined by standard deviation
+                                    (word frequency > average + 2 * Standard_Deviation)
+                        MidStdE: only analyze the Non-Outlier of word, determined by standard deviation
+                                    (average + 2 * Standard_Deviation > word frequency > average - 2 * Standard_Deviation)
+                        LowStdE: only analyze the Left Outlier of word, determined by standard deviation
+                                    (average - 2 * Standard_Deviation > word frequency)
+
+                    2. using IQR to find outlier *THIS METHOD DO NOT WORK WELL, BECAUSE THE DATA USUALLY ARE HIGHLY SKEWED*
+                        TopIQR: only analyze the Top outlier of word, determined by IQR
+                                    (word frequency > median + 1.5 * Standard)
+                        MidIQR: only analyze the non-outlier of word, determined by IQR
+                                    (median + 1.5 * Standard > word frequency > median - 1.5 * Standard)
+                        LowIQR: only analyze the Left outlier of word, determined by IQR
+                                    (median - 1.5 * Standard > word frequency)
+
+    :param Low:  this method will only analyze the word with higher frequency than this value
+                    (this parameter will be overwritten if the option is not 'Custom')
+    :param High: this method will only analyze the word with lower frequency than this value
+                    (this parameter will be overwritten if the option is not 'Custom')
+
+    :return:    contain a array
+                each element of array is a dictionary map a tuple to a list
+                    tuple consist of 3 element (group number 1, list number, group number 2)
+                        means compare the words in list number of group number 1 to all the word in group number 2
+                    the list contain tuples, sorted by p value:
+                        tuple means (word, p value)
+                        this is word usage of word in group (group number 1), list (list number),
+                        compare to the word usage of the same word in group (group number 2)
+    """
+
+    # init
+    GroupLists = []
+    GroupWordCounts = []
+    GroupNumWords = []
+    for Chunk in GroupWordLists:
+        GroupLists.append(merge_list(Chunk))
+        GroupWordCounts.append(sum(GroupLists[-1].values()))
+        GroupNumWords.append(len(GroupLists[-1]))
+    TotalList = merge_list(GroupLists)
+    TotalWordCount = sum(GroupWordCounts)
+    TotalNumWords = len(TotalList)
+    AllResults = {}  # the value to return
+
+    High, Low = wordfilter(option, Low, High, TotalNumWords, TotalWordCount, TotalList)
+
+    # calculation
+    for i in range(len(GroupWordLists)):  # individual chunk
+        for j in range(len(GroupWordLists)):  # group compare
+            if i != j:  # each chunk in wordlist i, compare to each chunk in
+                wordlistnumber = 0  # the label of the word list in GroupWordList[i]
+                for wordlist in GroupWordLists[i]:  # focusing on a specific word on list i.
+                    for word in wordlist.keys():
+                        iWordCount = wordlist[word]
+                        iTotalWordCount = sum(wordlist.values())
+                        iWordProp = iWordCount / iTotalWordCount
+                        try:
+                            jWordCount = GroupLists[j][word]
+                        except KeyError:
+                            jWordCount = 0
+                        jTotalWordCount = GroupWordCounts[j]
+                        jWordProp = jWordCount / jTotalWordCount
+                        if Low < iWordProp < High:
+                            p_value = ztest(iWordProp, jWordProp, iTotalWordCount, jTotalWordCount)
+                            try:
+                                AllResults[(i, wordlistnumber, j)].append((word, p_value))
+                            except:
+                                AllResults.update({(i, wordlistnumber, j): [(word, p_value)]})
+                    wordlistnumber += 1
+    # sort the output
+    for tuple in AllResults.keys():
+        list = AllResults[tuple]
+        list = sorted(list, key=lambda tup: tup[1])
+        AllResults.update({tuple: list})
+    return AllResults
+
+def KWtest(Matrixs, Words):
+    Len = max(len(matrix) for matrix in Matrixs)
+    word_pvalue_dict = {}
+
+    for i in range(1, len(Matrixs[0][0])):
+        print Words[i-1]
+        samples = []
+        for k in range(len(Matrixs)):
+            print k
+            sample = []
+            for j in range(len(Matrixs[k])):
+                sample.append(Matrixs[k][j][i])
+            print sample
+            print
+            samples.append(ma.masked_array(sample + [0]*(Len - len(sample)),
+                                           mask=[0]*len(sample)+[1]*(Len-len(sample))))
+        print samples
+        pvalue = kruskalwallis(samples)[1]
+        print pvalue
+        word_pvalue_dict.update({Words[i-1]: pvalue})
+    print word_pvalue_dict
+    return sorted(word_pvalue_dict.items(), key=itemgetter(1))
