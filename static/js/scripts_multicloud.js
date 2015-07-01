d95f6c42a7ac0ea6f228ec6f170fc5993a2fae3d
Caleb <gryphonwastler@gmail.com>
2015-07-01 14:07:55 -0400

    fixed issue with clicking the scrollbar accidently starting drag select on the viz pages and stats page.

--- static/js/scripts_multicloud.js
+++ static/js/scripts_multicloud.js
@@ -91,7 +91,7 @@ $(document).ready(function(){
 		},
 		stop: function() {
 			//when you stop selecting, all inputs with the class 'ui-selected' get clicked
-			$(".ui-selected input", this).trigger("click"); 
+			$(".ui-selected input").trigger("click"); 
 		}
 	});
 });
