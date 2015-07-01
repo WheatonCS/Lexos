c1af6a8009afd8b9fff2b4541a90fd8059abdfac
chantisnake <13501393281@163.com>
2015-07-01 18:29:01 -0400

    fix the bug(feature) that mentioned by  @JingxianL in issue #242 https://github.com/WheatonCS/Lexos/issues/242#issuecomment-117839552

--- lexos.py
+++ lexos.py
@@ -311,7 +311,7 @@ def statistics():
         if 'analyoption' not in session:
             session['analyoption'] = constants.DEFAULT_ANALIZE_OPTIONS
         if 'statisticoption' not in session:
-            session['statisticoption'] = {'segmentlist': map(unicode, labels.keys())}  # default is all on
+            session['statisticoption'] = {'segmentlist': map(unicode, fileManager.files.keys())}  # default is all on
 
         return render_template('statistics.html', labels=labels, labels2=labels)
 
