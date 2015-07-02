c8459b24543faef97481803e335416749858534f
scottkleinman <scott.kleinman@csun.edu>
2015-07-02 13:13:53 -0700

    Updates to manage template/functions

--- static/js/scripts_manage.js
+++ static/js/scripts_manage.js
@@ -4,121 +4,15 @@
 $(document).ready( function () {
 
 /* #### INITIATE MAIN DATATABLE #### */
-
-
 /* #### END OF TABLE INITIATION #### */	
 
 /* #### DEFINE CONTEXT MENU #### */
-	$(document).contextmenu({
-		//delegate: 'tr[role="row"]', // Trigger on row
-		delegate: 'td', // Trigger on cell
-		preventContextMenuForPopup: true,
-		preventSelect: true,
-		taphold: true,
-		// Define menu items
-		menu: [
-			{title: "Preview Document", cmd: "preview", uiIcon: "ui-icon-search"},
-			{title: "Edit Document Label", cmd: "edit-label", uiIcon: "ui-icon-pencil"},
-			{title: "Edit Document Class", cmd: "edit-class", uiIcon: "ui-icon-tag"},
-			{title: "Delete Document", cmd: "delete", uiIcon: "ui-icon-trash"},
-			//{title: "Clone Document", cmd: "clone", uiIcon: "ui-icon-copy"},
-			{title: "----"},
-			{title: "Select All Documents", cmd: "select-all", uiIcon: "ui-icon-check"},
-			{title: "Deselect All Documents", cmd: "deselect-all", uiIcon: "ui-icon-cancel"},
-			{title: "----"},
-			{title: "Apply Class to Selected Documents", cmd: "apply-class", uiIcon: "ui-icon-tag"},
-			{title: "Delete Selected Documents", cmd: "delete-selected", uiIcon: "ui-icon-trash"}
-			],
-		// Handle menu selection 
-		select: function(event, ui) {
-			// $target is the element defined in delegate.
-			// Variables below assume it is <td>.
-			var $target = ui.target;
-			var $row = $target.parent();
-			var row_id = $row.attr('id');
-			var	title = $row.children().eq(1).text();
-			// Set function calls for each menu item.
-			switch(ui.cmd){
-			case "preview":
-				showPreviewText(row_id);
-				break
-			case "edit-label":
-				var	cellText = $row.children().eq(1).text();
-				editLabel($target, cellText, title, row_id);	
-				break
-			case "edit-class":
-				var	cellText = $row.children().eq(2).text();
-				editClass($target, cellText, title, row_id);
-				break
-			case "delete":
-				deleteRow($target, title, row_id);
-				break
-			case "clone":
-				clone($target, title, row_id);
-				break
-			case "select-all":
-				// Activate all documents by ajax.
-				selectAll();
-				break
-			case "deselect-all":
-				// De-activate all documents by ajax.
-				deselectAll();
-				break
-			case "apply-class":
-				// Apply Class to all selected documents.
-				applyClass($target, "");
-				break
-			case "delete-selected":
-				// Delete active files by ajax.
-				deleteSelectedRows();
-				break
-			}
-		},
-		// Change menu items based on the target.
-		beforeOpen: function(event, ui) {
-			var $menu = ui.menu,
-				$target = ui.target,
-				//* Is extraData needed?
-				extraData = ui.extraData; // passed when menu was opened by call to open()
-
-			// Enable menu items based on number of selected rows
-			var num_selected = enabled.length;
-			//* Clone is not enabled at this stage.
-			$(document).contextmenu("enableEntry", "clone", false);
-
-			switch (num_selected) {
-			// No rows selected
-			case 0:
-				$(document).contextmenu("enableEntry", "delete-selected", false);
-				$(document).contextmenu("enableEntry", "deselect-all", false);
-				$(document).contextmenu("enableEntry", "apply-class", false);
-				break;
-			// 1 row selected
-			case 1:
-				$(document).contextmenu("enableEntry", "delete-selected", false);
-				$(document).contextmenu("enableEntry", "deselect-all", true);
-				$(document).contextmenu("enableEntry", "apply-class", false);
-				break;
-			default:
-				$(document).contextmenu("enableEntry", "delete-selected", true);
-				$(document).contextmenu("enableEntry", "deselect-all", true);
-				$(document).contextmenu("enableEntry", "apply-class", true);				
-			}
-			// Place the menu on top of the target.
-			ui.menu.zIndex($(event.target).zIndex() + 1);
-		}
-	});
 /* #### END OF CONTEXT MENU #### */
 
-
 /* #### CLICK EVENT HANDLING #### */
-
-
-
 /* #### END OF CLICK EVENT HANDLING #### */
 
 /* #### TOOLBAR BUTTON EVENT HANDLING #### */
-
 /*	$("#selectall").click(function() {
 		selectAll();
 	});
@@ -130,16 +24,12 @@ $(document).ready( function () {
 	$("#delete").click(function() {
 		deleteSelectedRows();
 	});*/
+/* #### END OF TOOLBAR BUTTON EVENT HANDLING #### */	
 
-
-
-/* #### END OF TOOLBAR BUTTON EVENT HANDLING #### */
-	
 });
 /* #### END OF $(DOCUMENT).READY() SCRIPTS #### */
 
 /* #### SUPPORTING FUNCTIONS #### */
-
 /* #### toggleId() #### LEGACY FUNCTION: NO LONGER IN USE */
 // Toggles the document state by Ajax, then toggles the UI selected state for the item.
 function toggleId(id) {
@@ -189,7 +79,6 @@ function editLabel($target, cellText, title, row_id) {
 					$("#editLabel").dialog("close");
 					$("#editLabel").remove();
 					tr = $target.parent();
-					table.row(tr).invalidate().draw();
 				},
 				error: function(jqXHR, textStatus, errorThrown){
 					alert('Error: Your action could not be saved to the session file.')
@@ -227,7 +116,6 @@ function editClass($target, cellText, title, row_id) {
 					console.log("Success: "+response);
 					$target.text(val);
 					tr = $target.parent();
-					table.row(tr).invalidate().draw();
 					$("#editClass").dialog("close");
 					$("#editClass").remove();
 				},
@@ -270,7 +158,6 @@ function applyClass($target, cellText) {
 						}
 					});
 					tr = $target.parent();
-					table.row(tr).invalidate().draw();
 					$("#editClass").dialog("close");
 					$("#editClass").remove();
 				},
@@ -316,50 +203,6 @@ function showPreviewText(row_id) {
 	});
 }
 /* #### END OF showPreviewText() #### */
-
-/* #### selectAll() #### */
-// Sets the selected status of all documents in the File Manager and UI to selected.
-function selectAll() {
-	$.ajax({
-		type: "POST",
-		url: document.URL,
-		data: 'dummy',
-		headers: { 'selectAll': 'dummy' },
-		success: function() {
-			$("#demo tbody tr").addClass("ui-selected");
-		},
-		error: function(jqXHR, textStatus, errorThrown){
-			alert('Error: Lexos could not perform the requested function.')
-			// display error if one
-			console.log("bad: " + textStatus + ": " + errorThrown);
-		}
-	}).then(function(){
-		location.reload();
-	});
-}
-/* #### END OF selectAll() #### */
-
-/* #### deselectAll() #### */
-// Removes the selected status of all documents in the File Manager and UI.
-function deselectAll() {
-	$.ajax({
-		type: "POST",
-		url: document.URL,
-		data: 'dummy',
-		headers: { 'disableAll': 'dummy' },
-		success: function() {
-			$("#demo tbody tr").removeClass("ui-selected");
-		},
-		error: function(jqXHR, textStatus, errorThrown){
-			alert('Error: Lexos could not perform the requested function.')
-			// display error if one
-			console.log("bad: " + textStatus + ": " + errorThrown);
-		}
-	}).then(function(){
-		location.reload();
-	});
-}
-/* #### END OF deselectAll() #### */
 
 /* #### deleteRow() #### */
 // Deletes a single document in the File Manager and UI.
@@ -415,7 +258,7 @@ function deleteSelectedRows() {
 			success: function() {
 				$('#delete-confirm-wrapper').removeClass("showing");
 				$("#demo tbody tr").each(function() {
-					if ($(this).hasClass("ui-selected")) {
+					if ($(this).hasClass("selected")) {
 						$(this).remove();
 					}
 				});
@@ -425,8 +268,6 @@ function deleteSelectedRows() {
 				alert('Error: Lexos could save your changes to the session file.')
 				console.log(errorThrown);
 			}
-		}).then(function(){
-			location.reload();
 		});
 	});
 
