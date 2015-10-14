f27db76ca3dab118d1ac283d43927b047f4ef714
scottkleinman <scott.kleinman@csun.edu>
2015-10-14 11:17:03 -0700

    Fixed file upload de-activation in Scrub

--- static/js/scripts_scrub.js
+++ static/js/scripts_scrub.js
@@ -11,38 +11,60 @@ $(function() {
 */
 	$('#swfileselect').change(function(ev) {
 		filename = ev.target.files[0].name;
-		if (filename.length > 35) {filename = filename.substring(0, 34) + "...";}
+		if (filename.length > 25) {filename = filename.substring(0, 24) + "...";}
 		$("#swfileselectbttnlabel").html(filename);
 	});
 
 	$('#lemfileselect').change(function(ev) {
 		filename = ev.target.files[0].name;
-		if (filename.length > 35) {filename = filename.substring(0, 34) + "...";}
+		if (filename.length > 25) {filename = filename.substring(0, 24) + "...";}
 		$("#lemfileselectbttnlabel").html(filename);
 	});
 
 	$('#consfileselect').change(function(ev) {
 		filename = ev.target.files[0].name;
-		if (filename.length > 35) {filename = filename.substring(0, 34) + "...";}
+		if (filename.length > 25) {filename = filename.substring(0, 24) + "...";}
 		$("#consfileselectbttnlabel").html(filename);
 	});
 
 	$('#scfileselect').change(function(ev) {
 		filename = ev.target.files[0].name;
-		if (filename.length > 35) {filename = filename.substring(0, 34) + "...";}
+		if (filename.length > 25) {filename = filename.substring(0, 24) + "...";}
 		$("#scfileselectbttnlabel").html(filename);
 	});
 
 
 	$(".bttnfilelabels").click( function() {
+		//swfileselect, lemfileselect, consfileselect, scfileselect
 		var filetype = $(this).attr('id').replace('bttnlabel', '');
 		usingCache = $('#usecache'+filetype).attr('disabled') != 'disabled';
 
 		if ((usingCache) || ($(this).attr('id') != '')) {
-			$(this).siblings('.scrub-upload').attr('value', '');
+			//$(this).siblings('.scrub-upload').attr('value', '');
+			// Next two lines clear the file input; it's hard to find a cross-browser solution			
+			$("#"+filetype).val('');
+			$("#"+filetype).replaceWith($("#"+filetype).clone(true));
 			$("#usecache"+filetype).attr('disabled', 'disabled');
 			$(this).text('');
 		}
+
+		// Do Ajax
+        $.ajax({
+            type: "POST",
+            url: "/removeUploadLabels",
+            data: $(this).text().toString(),
+            contentType: 'text/plain',
+            headers: { 'option': filetype+'[]' },
+            beforeSend: function(){
+                //alert('Sending...');
+            },
+            success: function(response) {
+                //console.log(response);
+            },
+            error: function(jqXHR, textStatus, errorThrown){
+                console.log("Error: " + errorThrown);
+            }
+		});
 	});
 
 	$("#punctbox").click( function() {
