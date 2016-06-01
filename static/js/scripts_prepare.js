53a829fece8f2eed9ee2a78620546383150b6fed
lexos-steven <waterhouse_steven@wheatoncollege.edu>
2016-06-01 11:29:10 -0400

    bug fixes and css overhaul

--- static/js/scripts_prepare.js
+++ static/js/scripts_prepare.js
@@ -1,23 +1,27 @@
 /* Sets a timer to check for scroll events and moves the action buttons when the user 
    scrolls them off the top of the screen */
 scrolled = false;
+var myvar;
 var shiftElement = window.setInterval(shiftElementIfNeeded, 300);
 function shiftElementIfNeeded() {
-	scrolled = true;
-	if (scrolled == true) {
-		scrolled = false;
-	    var scrollTop = $(window).scrollTop();
-	    var targetTop = 100;
+
+	var scrollTop = $(window).scrollTop();
+	var targetTop = 100;
+	if (scrollTop > targetTop) {
 	    buttons = $("#action-buttons").html();
 	    newEl = $('<div id="action-buttons" style="text-align:right"></div>');
 	    newEl.append(buttons);
-	    if (scrollTop > targetTop) {
-		    $("#action-buttons").remove();
-			$("#preview-col").append(newEl);
-	    }
+
+		$("#action-buttons").remove();
+		$("#preview-col").append(newEl);
+
+
+
+
 	    if (scrollTop <= targetTop) {
 		    $("#action-buttons").remove();
 			$("#preview-col").prepend(newEl);
+			clearInterval(shiftElement);
 	    }
 	}
 }
