4bb53e07f85d4e86377c9a91a5e10b938b7fc315
scottkleinman <scott.kleinman@csun.edu>
2016-06-17 08:57:28 -0700

    Some added tokenizer test code

--- lexos.py
+++ lexos.py
@@ -853,6 +853,14 @@ def rollingwindow():
 
         dataPoints, dataList, graphTitle, xAxisLabel, yAxisLabel, legendLabels = utility.generateRWA(fileManager)
 
+        if 'get-RW-pdf' in request.form:
+            # The 'Generate and Download Matrix' button is clicked on rollingwindow.html.
+
+            savePath, fileExtension = utility.generateRWmatrixPlot(dataPoints, legendLabels)
+            fileExtension = ".pdf"
+
+            return send_file(savePath, attachment_filename="rollingwindow_matrix" + fileExtension, as_attachment=True)
+
         if 'get-RW-plot' in request.form:
             # The 'Generate and Download Matrix' button is clicked on rollingwindow.html.
 
@@ -1932,22 +1940,24 @@ def t():
             dtm = utility.generateCSVMatrixFromAjax(data, fileManager, roundDecimal=True)
             del dtm[0] # delete the labels
 
-            leftVals = []
             #Convert to json for DataTables
             for i in dtm:
                  q = [j for j in i]
                  matrix.append(q)
-                 leftVals.append(q[0])
 
         numRows = len(matrix)
         draw = 1
-        return render_template('t.html', labels=labels, headers=headerLabels, leftVals=leftVals, data=matrix, numRows=numRows, draw=draw, numActiveDocs=numActiveDocs)
+        return render_template('t.html', labels=labels, headers=headerLabels, data=matrix, numRows=numRows, draw=draw, orientation=csvorientation, numActiveDocs=numActiveDocs)
 
     if request.method == "POST":
         import json
         from operator import itemgetter
         data = request.json
         session_manager.cacheAnalysisOption()
+
+        dtm = utility.generateCSVMatrixFromAjax(data, fileManager, roundDecimal=True)
+        titles = dtm[0]
+        del dtm[0]
 
         # Print the request variables for de-bugging
         #print("request.json:")
@@ -1973,59 +1983,100 @@ def t():
         for fileID in labels:
             headerLabels.append(fileManager.files[int(fileID)].label)
         data = {'cullnumber': request.json["cullnumber"], 'tokenType': request.json["tokenType"], 'normalizeType': request.json["normalizeType"], 'csvdelimiter': request.json["csvdelimiter"], 'mfwnumber': request.json["mfwnumber"], 'csvorientation': request.json["csvorientation"], 'tokenSize': request.json["tokenSize"], 'norm': request.json["norm"]}
+
+        # Sort and Filter the cached DTM by column
+        if len(search) != 0:
+            dtmSorted = filter(lambda x: x[0].startswith(search), dtm)
+            dtmSorted = natsorted(dtmSorted,key=itemgetter(sortColumn), reverse=reverse)
+        else:
+            dtmSorted = natsorted(dtm,key=itemgetter(sortColumn), reverse=reverse)
+
+        # Get the number of filtered rows
+        numFilteredRows = len(dtmSorted)
+        terms = []
+        for line in dtmSorted:
+            terms.append(line[0])
+
+        #Convert to json for DataTables
         matrix = []
+        for i in dtmSorted:
+            q =[j for j in i]
+            matrix.append(q)
 
-        if numActiveDocs > 0:
-            dtm = utility.generateCSVMatrixFromAjax(data, fileManager, roundDecimal=True)
-            del dtm[0]
-            titles = dtm[0]
+        for row in matrix:
+            del row[0]
+        numRows = len(matrix)
+    
+        print("Matrix0")
+        print matrix[0]
 
-            # Sort and Filter the cached DTM by column
-            if len(search) != 0:
-                dtmSorted = filter(lambda x: x[0].startswith(search), dtm)
-                dtmSorted = natsorted(dtmSorted,key=itemgetter(sortColumn), reverse= reverse)
-            else:
-                dtmSorted = natsorted(dtm,key=itemgetter(sortColumn), reverse= reverse)
+        # Set the table headers
+        if orientation == "filecolumn":
+            columns = titles[:]
+            for i in range(len(matrix)):
+                matrix[i].insert(0, terms[i])
+        else:
+            columns = terms[:]
+            matrix = zip(*matrix)
+            for i in range(len(matrix)):
+                matrix[i].insert(0, titles[i])
 
-            # Get the number of filtered rows
-            numFilteredRows = len(dtmSorted)
-            terms = []
-            for line in dtmSorted:
-                terms.append(line[0])
-
-            #Convert to json for DataTables
-            matrix = []
-            for i in dtmSorted:
-                q =[j for j in i]
-                matrix.append(q)
-
-            firstColumn = []
-            for row in matrix:
-                firstColumn.append(row[0])
-                del row[0]
-            numRows = len(matrix)
-
-            # Set the table headers
-            if orientation == "filecolumn":
-                columns = titles[:]
-                for i in range(len(matrix)):
-                    matrix[i].insert(0, terms[i])
-            else:
-                columns = terms[:]
-                matrix = zip(*matrix)
-                for i in range(len(matrix)):
-                    matrix[i].insert(0, titles[i])
-
-            # Set the table length
-            if int(request.json["length"]) == -1:
-                matrix = matrix[0:]
-            else:
-                start = int(request.json["start"])
-                end = int(request.json["end"])
-                matrix = matrix[start:end]
+        # Set the table length
+        if int(request.json["length"]) == -1:
+            matrix = matrix[0:]
+        else:
+            start = int(request.json["start"])
+            end = int(request.json["end"])
+            matrix = matrix[start:end]
 
         response = {"draw": draw, "recordsTotal": numRows, "recordsFiltered": numFilteredRows, "length": int(length), "headers": columns, "data": matrix}
         return json.dumps(response)        
+
+@app.route("/transpose", methods=["GET", "POST"])  # Tells Flask to load this function when someone is at '/hierarchy'
+def transpose():
+    # Detect the number of active documents.
+    numActiveDocs = detectActiveDocs()
+
+    fileManager = managers.utility.loadFileManager()
+
+    if request.method == "GET":
+        labels = fileManager.getActiveLabels()
+        headerLabels = []
+        for fileID in labels:
+            headerLabels.append(fileManager.files[int(fileID)].label)
+        if 'analyoption' not in session:
+            session['analyoption'] = constants.DEFAULT_ANALYZE_OPTIONS
+        if 'csvoptions' not in session:
+            session['csvoptions'] = constants.DEFAULT_CSV_OPTIONS
+        csvorientation = session['csvoptions']['csvorientation']
+        csvdelimiter = session['csvoptions']['csvdelimiter']
+        cullnumber = session['analyoption']['cullnumber']
+        tokenType = session['analyoption']['tokenType']
+        normalizeType = session['analyoption']['normalizeType']
+        tokenSize = session['analyoption']['tokenSize']
+        norm = session['analyoption']['norm']
+        data = {'cullnumber': cullnumber, 'tokenType': tokenType, 'normalizeType': normalizeType, 'csvdelimiter': csvdelimiter, 'mfwnumber': '1', 'csvorientation': csvorientation, 'tokenSize': tokenSize, 'norm': norm}
+        session_manager.cacheAnalysisOption()
+        matrix = []
+        if numActiveDocs > 0:
+            dtm = utility.generateCSVMatrixFromAjax(data, fileManager, roundDecimal=True)
+            import pandas as pd
+            dtm = pd.DataFrame(dtm).T.values.tolist() # Transpose the DTM
+            headerLabels = dtm[0]
+            del dtm[0] # delete the labels
+            del headerLabels[0]
+
+            #Convert to json for DataTables
+            for i in dtm:
+                 q = [j for j in i]
+                 matrix.append(q)
+
+        print("headerLabels")
+        print(headerLabels)
+        numRows = len(matrix)
+        draw = 1
+        return render_template('t.html', labels=labels, headers=headerLabels, data=matrix, numRows=numRows, draw=draw, orientation=csvorientation, numActiveDocs=numActiveDocs)
+
 
 # ======= End of temporary development functions ======= #
 
