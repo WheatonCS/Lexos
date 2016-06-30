2471f70ecd757f51d7b21be88e62367f2f4f8739
Cheng Zhang <13501393281@163.com>
2016-06-30 15:56:54 -0400

    add a config file

---
+++ install/windows/config.ps1
@@ -0,0 +1,6 @@
+# define url
+$lexosVersion = "3.0"
+$lexosZipUrl = "https://github.com/WheatonCS/Lexos/archive/v$lexosVersion.zip"
+$lexosLocation = "C:\Lexos-$lexosVersion\"
+$lexosExecutableLocation = "$lexosLocation\windows\install\"
+$lexosAssets = "$lexosLocation\assets"
