e65f89b4bd5e66267898030d3b3b4d9fb29a5a47
Cheng Zhang <13501393281@163.com>
2016-06-30 16:47:05 -0400

    fix the bug with run.cmd

--- install/windows/run.cmd
+++ install/windows/run.cmd
@@ -1,1 +1,1 @@
-@powershell -NoProfile -WindowStyle Hidden -ExecutionPolicy Bypass -Command "& {C:/Lexos-master/install/windows/run.ps1}"
+@powershell -NoProfile -WindowStyle Hidden -ExecutionPolicy Bypass -Command "& {C:/Lexos-3.0/install/windows/run.ps1}"
