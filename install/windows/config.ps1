98fe1177a26c9838dd7a9cbd24747b87d82ac318
Cheng Zhang <13501393281@163.com>
2016-06-30 16:22:41 -0400

    fix typo in config.ps1

--- install/windows/config.ps1
+++ install/windows/config.ps1
@@ -2,5 +2,5 @@
 $lexosVersion = "3.0"
 $lexosZipUrl = "https://github.com/WheatonCS/Lexos/archive/v$lexosVersion.zip"
 $lexosLocation = "C:\Lexos-$lexosVersion\"
-$lexosExecutableLocation = "$lexosLocation\windows\install\"
-$lexosAssets = "$lexosLocation\assets"
+$lexosExecutableLocation = "$lexosLocation\install\windows\"
+$lexosAssets = "$lexosLocation\install\assets"
