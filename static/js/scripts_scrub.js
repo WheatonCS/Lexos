58d063e0dac37e6a0163be9f984bd27ad3464d9e
scottkleinman <scott.kleinman@csun.edu>
2016-07-18 11:32:16 -0700

    Improved Junicode loading

--- static/js/scripts_scrub.js
+++ static/js/scripts_scrub.js
@@ -87,6 +87,7 @@ $(function() {
 		console.log($("#entityrules")[0].value);
 		if($("#entityrules")[0].value == "MUFI-3" || $("#entityrules")[0].value == "MUFI-4") {
 			document.getElementById("MUFI-warning").style.display = "inline-block";
+			$("head").append("<link href='../static/lib/junicode/Junicode.woff' rel='stylesheet' type='text/css'>");
 			$(".filecontents").addClass("Junicode");
 			}
 		else 
