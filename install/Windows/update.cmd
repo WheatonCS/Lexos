90a911a9bf98b11ef6c5d57bb49485a509f257e1
Zhang Cheng <13501393281@163.com>
2017-01-27 23:18:47 +0000

    wrong url = =

--- install/Windows/update.cmd
+++ install/Windows/update.cmd
@@ -1,2 +1,2 @@
 @echo off
-cmd /C "cd C:\ && powershell -NoProfile -ExecutionPolicy Bypass -Command "iex ((new-object net.webclient).DownloadString('https://raw.githubusercontent.com/WheatonCS/Lexos/master/install/Windows/config.ps1'))""
+cmd /C "cd C:\ && powershell -NoProfile -ExecutionPolicy Bypass -Command "iex ((new-object net.webclient).DownloadString('https://raw.githubusercontent.com/WheatonCS/Lexos/master/install/Windows/update.ps1'))""
