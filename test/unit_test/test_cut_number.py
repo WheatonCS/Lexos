8f7849e97bfd1c908b6b6e0c9facc6f4448ecc5c
emma_grace <steffens_emma@wheatoncollege.edu>
2017-07-19 16:33:55 -0400

    Reorganized and added tests for split_keep_whitespace()

--- test/unit_test/test_cut_number.py
+++ test/unit_test/test_cut_number.py
@@ -2,10 +2,11 @@ from lexos.processors.prepare.cutter import split_keep_whitespace, \
     count_words, cut_by_number
 
 
-class TestCutByNumbers:
+class TestNumberCutHelpers:
     def test_split_keep_whitespace(self):
         assert split_keep_whitespace("Test string") == ["Test", " ", "string"]
         assert split_keep_whitespace("Test") == ["Test"]
+        assert split_keep_whitespace("Test ") == ["Test", " ", ""]  # intended?
         assert split_keep_whitespace(" ") == ["", " ", ""]  # intended?
         assert split_keep_whitespace("") == [""]
 
@@ -14,10 +15,15 @@ class TestCutByNumbers:
         assert count_words(['\n', '\t', ' ', '', '\u3000', "word"]) == 1
         assert count_words([""]) == 0
 
+
+class TestCutByNumbers:
     def test_cut_by_number_normal(self):
         assert cut_by_number("Text", 1) == ["Text"]
         assert cut_by_number("This text has five words", 5) == \
                ["This ", "text ", "has ", "five ", "words"]
+        assert len(cut_by_number("Odd number of words in this text", 6)) == 6
+        assert len(cut_by_number("Almost enough words here but not quite", 4))\
+               == 4
 
     def test_cut_by_number_spacing(self):
         assert cut_by_number("Hanging space ", 2) == ["Hanging ", "space "]
