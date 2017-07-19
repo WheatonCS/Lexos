2ff0c5137f019b2acfa426d7e9b13b6614b45c9d
Shi26 <darias0826@gmail.com>
2017-07-19 19:19:32 -0400

    adding test function in regular cases

--- test/unit_test/test_cut_line.py
+++ test/unit_test/test_cut_line.py
@@ -3,3 +3,12 @@ from lexos.processors.prepare.cutter import cut_by_lines
 
 def test_cut_by_lines_empty():
     assert cut_by_lines(text="", chunk_size=1, overlap=0, last_prop=0) == [""]
+
+
+def test_cut_by_lines_regular():
+    assert cut_by_lines(text="test", chunk_size=1,
+                        overlap=0, last_prop=0) == ["test"]
+    assert cut_by_lines(text="test test", chunk_size=2,
+                        overlap=1, last_prop=0) == ["test test"]
+    assert cut_by_lines(text="test test", chunk_size=1,
+                        overlap=0, last_prop=200) == ["test test"]
