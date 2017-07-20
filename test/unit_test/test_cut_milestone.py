038fbe0abe276126f25518e22eb189c03b270785
xliu <liu_xinru@wheatoncollege.edu>
2017-07-20 10:15:33 -0400

    revise the file test_cut_milestone.py

--- test/unit_test/test_cut_milestone.py
+++ test/unit_test/test_cut_milestone.py
@@ -5,22 +5,26 @@ class TestMileStone:
     def test_milestone_regular(self):
         text_content = "Today is raining."
         milestone = "is"
-        assert cut_by_milestone(text_content, milestone) == ["Today ", " raining."]
+        assert cut_by_milestone(text_content, milestone) == ["Today ",
+                                                             " raining."]
 
     def test_milestone_no_milestone_in_text(self):
         text_content = "Today is raining."
         milestone = "am"
-        assert cut_by_milestone(text_content, milestone) == ["Today is raining."]
+        assert cut_by_milestone(text_content, milestone) == ["Today is "
+                                                             "raining."]
 
     def test_milestone_longer_than_text(self):
         text_content = "Today is raining."
         milestone = "Today is still raining."
-        assert cut_by_milestone(text_content, milestone) == ["Today is raining."]
+        assert cut_by_milestone(text_content, milestone) == ["Today is "
+                                                             "raining."]
 
     def test_milestone_len_zero(self):
         text_content = "Today is raining."
         milestone = ""
-        assert cut_by_milestone(text_content, milestone) == ["Today is raining."]
+        assert cut_by_milestone(text_content, milestone) == ["Today is "
+                                                             "raining."]
 
     def test_milestone_empty_text(self):
         text_content = ""
