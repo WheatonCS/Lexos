317c29c2771437e069fc8e320a87ed745053e303
ariannaalfiero <alfiero_arianna@wheatoncollege.edu>
2017-07-20 16:10:22 -0400

    Corrected over-indentation

--- lexos/helpers/error_messages.py
+++ lexos/helpers/error_messages.py
@@ -1,6 +1,6 @@
 SEG_NON_POSITIVE_MESSAGE = "The segment size must be a positive integer"
 OVERLAP_LARGE_MESSAGE = "The segment size must be greater than " \
-        "the overlap of words between chunks"
+    "the overlap of words between chunks"
 PROP_NEGATIVE_MESSAGE = "The proportional size of the last segment must be " \
-                        "zero or greater"
+    "zero or greater"
 OVERLAP_NEGATIVE_MESSAGE = "The overlap between chunks must be zero or greater"
