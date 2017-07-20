a608134096dc20a1c7d3bcbf0402add8018b7701
xliu <liu_xinru@wheatoncollege.edu>
2017-07-20 11:16:36 -0400

    give change to the file test_cut_milestone2.py

--- test/unit_test/test_cut_milestone2.py
+++ test/unit_test/test_cut_milestone2.py
@@ -37,10 +37,3 @@ class TestMileStone:
         milestone = "BOBCAT"
         assert cut_by_milestone(text_content, milestone) == ["The bobcat "
                                                              "slept all day."]
-
-    def test_milestone_multiple(self):
-        text_content = "the bobcat slept all day the bobcat slept."
-        milestone = "the"
-        assert cut_by_milestone(text_content, milestone) == [" bobcat slept "
-                                                             "all day ",
-                                                             " bobcat slept."]
