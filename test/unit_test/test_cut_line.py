bd8df528c70e79eca85f8ab2add8e6b2b7708fbf
Shi26 <darias0826@gmail.com>
2017-07-19 20:00:31 -0400

    adding new function test more scenario with different line ending

--- test/unit_test/test_cut_line.py
+++ test/unit_test/test_cut_line.py
@@ -14,3 +14,12 @@ def test_cut_by_lines_regular():
     assert cut_by_lines(text="test\ntest\ntest", chunk_size=1,
                         overlap=0, last_prop=200) == ["test\n",
                                                       "test\ntest"]
+
+
+def test_cut_by_lines_line_ending():
+    assert cut_by_lines(text="test\rtest", chunk_size=1,
+                        overlap=0, last_prop=0) == ["test\r", "test"]
+    assert cut_by_lines(text="test\rtest\ntest", chunk_size=1,
+                        overlap=0, last_prop=0) == ["test\r",
+                                                    "test\n", "test"]
+
