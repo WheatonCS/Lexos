db91e4d31a38333d1220255f12711e4fe91a5f41
Weiqi97 <weltch1997@gmail.com>
2017-07-20 16:09:06 -0400

    Add error messages

---
+++ lexos/helpers/error_messages.py
@@ -0,0 +1,3 @@
+OVERLAP_SIZE_ERROR = "Overlap has to be smaller than segment size"
+NON_NEGATIVE_ERROR = "Parameter has to be bigger than or equal to zero"
+LAGER_THAN_ZERO_ERROR = "Parameter has to be bigger than zero"
