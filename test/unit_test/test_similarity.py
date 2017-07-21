7bc471d4f9dd3883107e0e4068dfbd2bf9eb44de
xliu <liu_xinru@wheatoncollege.edu>
2017-07-21 14:03:28 -0400

    add content to test_similarity.py

--- test/unit_test/test_similarity.py
+++ test/unit_test/test_similarity.py
@@ -2,6 +2,25 @@ from lexos.processors.analyze.similarity import similarity_maker
 
 
 class TestSimilarity:
+    def test_regular(self):
+        count_matrix = [['', '12', 'I', 'The', 'all', 'at', 'bed', 'bobcat',
+                         'cat', 'caterpillar', 'day.', 'every', 'go',
+                         'morning', 'night', 'nine', 'slept', 'to', 'up',
+                         'wake'],
+                        ['catBobcat', 0.0, 0.0, 9.0, 9.0, 0.0, 0.0, 5.0, 4.0,
+                         0.0, 9.0, 0.0, 0.0, 0.0, 0.0, 0.0, 9.0, 0.0, 0.0,
+                         0.0],
+                        ['catCaterpillar', 0.0, 0.0, 9.0, 9.0, 0.0, 0.0, 0.0,
+                         4.0, 5.0, 9.0, 0.0, 0.0, 0.0, 0.0, 0.0, 9.0, 0.0, 0.0,
+                         0.0],
+                        ['wake', 5.0, 10.0, 0.0, 0.0, 10.0, 5.0, 0.0, 0.0, 0.0,
+                         0.0, 10.0, 5.0, 5.0, 5.0, 5.0, 0.0, 5.0, 5.0, 5.0]]
+        comp_file_index = 2
+        temp_labels = ['catBobcat', 'catCaterpillar']
+        assert similarity_maker(count_matrix, comp_file_index, temp_labels
+                                ) == ([1.0, 1.0], ['catBobcat',
+                                                   'catCaterpillar'])
+
     def test_with_two_same_content_file(self):
         count_matrix = [['', 'The', 'all', 'bobcat', 'cat', 'caterpillar',
                          'day.', 'slept'],
@@ -24,4 +43,5 @@ class TestSimilarity:
         assert similarity_maker(count_matrix, comp_file_index, temp_labels
                                 ) == ([1.0, 1.0],
                                       ['catBobcat', 'catCaterpillar'])
+
 
