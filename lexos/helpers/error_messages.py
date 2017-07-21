d269f9909fe3380971d8ecb72525aecd54ebba6b
xliu <liu_xinru@wheatoncollege.edu>
2017-07-21 14:23:06 -0400

    add one error message

--- lexos/helpers/error_messages.py
+++ lexos/helpers/error_messages.py
@@ -3,3 +3,4 @@ NEG_NUM_MESSAGE = "the overlap or last segment proportion should " \
     "not be negative"
 LARGER_CHUNK_SIZE_MESSAGE = "the segment size should be larger " \
     "than the overlap size"
+NON_POSITIVE_INDEX_MESSAGE = "the index should be larger than or equal to zero"
