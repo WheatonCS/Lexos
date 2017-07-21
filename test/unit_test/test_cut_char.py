43a41e9744f732865ae4be92793366f6bc50a043
Weiqi97 <weltch1997@gmail.com>
2017-07-21 10:31:18 -0400

    Change error message names in unit test

--- test/unit_test/test_cut_char.py
+++ test/unit_test/test_cut_char.py
@@ -1,5 +1,5 @@
-from lexos.helpers.error_messages import NON_NEGATIVE_ERROR, \
-    OVERLAP_SIZE_ERROR, LAGER_THAN_ZERO_ERROR
+from lexos.helpers.error_messages import NON_POSITIVE_NUM_MESSAGE, \
+    NEG_NUM_MESSAGE, LARGER_CHUNK_SIZE_MESSAGE
 from lexos.processors.prepare.cutter import cut_by_characters
 
 
@@ -45,25 +45,25 @@ class TestCutByCharacters:
                                   last_prop=0)
             raise AssertionError("Larger than zero error did not raise")
         except AssertionError as error:
-            assert str(error) == LAGER_THAN_ZERO_ERROR
+            assert str(error) == NON_POSITIVE_NUM_MESSAGE
 
         try:
             _ = cut_by_characters(text="ABAB", chunk_size=2, overlap=-1,
                                   last_prop=0)
             raise AssertionError("None negative error did not raise")
         except AssertionError as error:
-            assert str(error) == NON_NEGATIVE_ERROR
+            assert str(error) == NEG_NUM_MESSAGE
 
         try:
             _ = cut_by_characters(text="ABAB", chunk_size=2, overlap=0,
                                   last_prop=-1)
             raise AssertionError("None negative error did not raise")
         except AssertionError as error:
-            assert str(error) == NON_NEGATIVE_ERROR
+            assert str(error) == NEG_NUM_MESSAGE
 
         try:
             _ = cut_by_characters(text="ABAB", chunk_size=2, overlap=2,
                                   last_prop=0)
             raise AssertionError("Overlap size error did not raise")
         except AssertionError as error:
-            assert str(error) == OVERLAP_SIZE_ERROR
+            assert str(error) == LARGER_CHUNK_SIZE_MESSAGE
