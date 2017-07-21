9ec1a40eec16ee90ac3aa06ce7cfb20f208f4ea3
xliu <liu_xinru@wheatoncollege.edu>
2017-07-21 14:41:25 -0400

    change one error message

--- lexos/helpers/error_messages.py
+++ lexos/helpers/error_messages.py
@@ -4,4 +4,5 @@ NEG_NUM_MESSAGE = "the overlap or last segment proportion should " \
 LARGER_CHUNK_SIZE_MESSAGE = "the segment size should be larger " \
     "than the overlap size"
 NON_POSITIVE_INDEX_MESSAGE = "the index should be larger than or equal to zero"
-MATRIX_DIMENSION_UNEQUAL_MESSAGE = "the dimension of the matrix is not equal"
+MATRIX_DIMENSION_UNEQUAL_MESSAGE = "the dimension of the matrix " \
+    "should be equal"
