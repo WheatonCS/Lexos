5944944299c2a9ae4b9bdf82cd75f68598707169
xliu <liu_xinru@wheatoncollege.edu>
2017-07-24 11:33:21 -0400

    modify one function and delete one function

--- test/unit_test/test_similarity.py
+++ test/unit_test/test_similarity.py
@@ -21,28 +21,16 @@ class TestSimilarity:
                                 ) == ([1.0, 1.0], ['catBobcat',
                                                    'catCaterpillar'])
 
-    def test_with_two_same_content_file(self):
+    def test_with_all_same_content_file(self):
         count_matrix = [['', 'The', 'all', 'bobcat', 'cat', 'caterpillar',
                          'day.', 'slept'],
                         ['catBobcat', 9.0, 9.0, 5.0, 4.0, 0.0, 9.0, 9.0],
-                        ['catCaterpillar', 9.0, 9.0, 0.0, 4.0, 5.0, 9.0, 9.0],
+                        ['catCaterpillar', 9.0, 9.0, 5.0, 4.0, 0.0, 9.0, 9.0],
                         ['test', 9.0, 9.0, 5.0, 4.0, 0.0, 9.0, 9.0]]
         comp_file_index = 1
         temp_labels = ['catBobcat', 'test']
         assert similarity_maker(count_matrix, comp_file_index, temp_labels
-                                ) == ([0.0685, 0.0685], ['catBobcat', 'test'])
-
-    def test_with_one_file_without_content(self):
-        count_matrix = [['', 'The', 'all', 'bobcat', 'cat', 'caterpillar',
-                         'day.', 'slept'],
-                        ['catBobcat', 9.0, 9.0, 5.0, 4.0, 0.0, 9.0, 9.0],
-                        ['catCaterpillar', 9.0, 9.0, 0.0, 4.0, 5.0, 9.0, 9.0],
-                        ['empty', 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0]]
-        comp_file_index = 2
-        temp_labels = ['catBobcat', 'catCaterpillar']
-        assert similarity_maker(count_matrix, comp_file_index, temp_labels
-                                ) == ([1.0, 1.0],
-                                      ['catBobcat', 'catCaterpillar'])
+                                ) == ([0.0, 0.0], ['catBobcat', 'test'])
 
     def test_with_two_dimension(self):
         count_matrix = [['', 'cat', 'the'], ['file1', 0.0, 1.0],
