f475a081d1ed30a271db1d7840c9f1a01ed84bea
Weiqi97 <weltch1997@gmail.com>
2017-07-24 14:42:16 -0400

    Fix coding style

--- test/unit_test/test_utility_stats/test_utility_stats.py
+++ test/unit_test/test_utility_stats/test_utility_stats.py
@@ -11,49 +11,43 @@ for i in range(len(file_list)):
     file_info_list.append((file_list[i], file_information.return_statistics()))
 
 
-def test_basic_info():
-    assert file_info_list[0][1]["name"] == file_list[0]
-    assert file_info_list[1][1]["name"] == file_list[1]
+class TestFileInfo:
+    def test_basic_info(self):
+        assert file_info_list[0][1]["name"] == file_list[0]
+        assert file_info_list[1][1]["name"] == file_list[1]
 
+    def test_unique_words(self):
+        assert file_info_list[0][1]["numUniqueWords"] == len(word_lists[0])
+        assert file_info_list[1][1]["numUniqueWords"] == len(word_lists[1])
 
-def test_unique_words():
-    assert file_info_list[0][1]["numUniqueWords"] == len(word_lists[0])
-    assert file_info_list[1][1]["numUniqueWords"] == len(word_lists[1])
+    def test_total_words(self):
+        assert file_info_list[0][1]["totalWordCount"] == \
+            sum(word_lists[0].values())
+        assert file_info_list[1][1]["totalWordCount"] == \
+            sum(word_lists[1].values())
 
+    def test_median(self):
+        assert file_info_list[0][1]["median"] == (15 + 20) / 2
+        assert file_info_list[1][1]["median"] == 3
 
-def test_total_words():
-    assert file_info_list[0][1]["totalWordCount"] == \
-        sum(word_lists[0].values())
-    assert file_info_list[1][1]["totalWordCount"] == \
-        sum(word_lists[1].values())
+    def test_quartiles(self):
+        assert file_info_list[0][1]["Q1"] == 10
+        assert file_info_list[0][1]["Q3"] == 30
+        assert file_info_list[1][1]["Q1"] == 2
+        assert file_info_list[1][1]["Q3"] == 4
+        assert file_info_list[0][1]["IQR"] == \
+            file_info_list[0][1]["Q3"] - file_info_list[0][1]["Q1"]
 
+    def test_average(self):
+        assert file_info_list[0][1]["average"] == \
+            sum(word_lists[0].values()) / len(word_lists[0])
+        assert file_info_list[1][1]["average"] == \
+            sum(word_lists[1].values()) / len(word_lists[1])
 
-def test_median():
-    assert file_info_list[0][1]["median"] == (15+20) / 2
-    assert file_info_list[1][1]["median"] == 3
+    def test_std(self):
+        assert round(file_info_list[0][1]["std"], 4) == 12.7475
+        assert round(file_info_list[1][1]["std"], 4) == 1.4142
 
-
-def test_quartiles():
-    assert file_info_list[0][1]["Q1"] == 10
-    assert file_info_list[0][1]["Q3"] == 30
-    assert file_info_list[1][1]["Q1"] == 2
-    assert file_info_list[1][1]["Q3"] == 4
-    assert file_info_list[0][1]["IQR"] == \
-        file_info_list[0][1]["Q3"] - file_info_list[0][1]["Q1"]
-
-
-def test_average():
-    assert file_info_list[0][1]["average"] == \
-        sum(word_lists[0].values())/len(word_lists[0])
-    assert file_info_list[1][1]["average"] == \
-        sum(word_lists[1].values())/len(word_lists[1])
-
-
-def test_std():
-    assert round(file_info_list[0][1]["std"], 4) == 12.7475
-    assert round(file_info_list[1][1]["std"], 4) == 1.4142
-
-
-def test_hapax():
-    assert file_info_list[0][1]["Hapax"] == 0
-    assert file_info_list[1][1]["Hapax"] == 1
+    def test_hapax(self):
+        assert file_info_list[0][1]["Hapax"] == 0
+        assert file_info_list[1][1]["Hapax"] == 1
