77bbd1391854d1962a20c6a5caf41b58cc4800b8
chantisnake <13501393281@163.com>
2017-07-26 15:38:18 -0400

    remove unused import

--- lexos_core.py
+++ lexos_core.py
@@ -1,13 +1,6 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
-
-import lexos.helpers.constants as constants
 from lexos.application import app
-# force matplotlib to use antigrain (Agg) rendering
-if constants.IS_SERVER:
-    import matplotlib
-
-    matplotlib.use('Agg')
 
 
 if __name__ == "__main__":
