6f8c7829737a267f4706dc27715105c6f458f0a6
xliu <liu_xinru@wheatoncollege.edu>
2017-07-27 15:54:49 -0400

    change the assertion in test_similarity

--- test/unit_test/test_similarity.py
+++ test/unit_test/test_similarity.py
@@ -6,11 +6,7 @@ import numpy as np
 
 class TestSimilarity:
     def test_with_similarity_equals_one(self):
-        count_matrix = np.array([['12', 'I', 'The', 'all', 'at', 'bed',
-                                  'bobcat', 'cat', 'caterpillar', 'day.',
-                                  'every', 'go', 'morning', 'night', 'nine',
-                                  'slept', 'to', 'up', 'wake'],
-                                 [0.0, 0.0, 9.0, 9.0, 0.0, 0.0, 5.0, 4.0,
+        count_matrix = np.array([[0.0, 0.0, 9.0, 9.0, 0.0, 0.0, 5.0, 4.0,
                                   0.0, 9.0, 0.0, 0.0, 0.0, 0.0, 0.0, 9.0, 0.0,
                                   0.0, 0.0],
                                  [0.0, 0.0, 9.0, 9.0, 0.0, 0.0, 0.0, 4.0, 5.0,
@@ -18,45 +14,49 @@ class TestSimilarity:
                                   0.0],
                                  [5.0, 10.0, 0.0, 0.0, 10.0, 5.0, 0.0, 0.0,
                                   0.0, 0.0, 10.0, 5.0, 5.0, 5.0, 5.0, 0.0, 5.0,
-                                  5.0, 5.0]])
+                                  5.0, 5.0]], dtype=object)
         comp_file_index = 2
         temp_labels = np.array(['catBobcat', 'catCaterpillar'])
-        assert similarity_maker(count_matrix, comp_file_index, temp_labels
-                                ) == ([1.0, 1.0], ['catBobcat',
-                                                   'catCaterpillar'])
+        np.testing.assert_equal(
+            similarity_maker(count_matrix, comp_file_index, temp_labels),
+            (np.array([1.0, 1.0]), np.array(['catBobcat', 'catCaterpillar']))
+        )
 
     def test_with_all_same_content_file(self):
-        count_matrix = np.array([['The', 'all', 'bobcat', 'cat',
-                                  'caterpillar', 'day.', 'slept'],
+        count_matrix = np.array([[9.0, 9.0, 5.0, 4.0, 0.0, 9.0, 9.0],
                                 [9.0, 9.0, 5.0, 4.0, 0.0, 9.0, 9.0],
-                                [9.0, 9.0, 5.0, 4.0, 0.0, 9.0, 9.0],
-                                [9.0, 9.0, 5.0, 4.0, 0.0, 9.0, 9.0]])
+                                [9.0, 9.0, 5.0, 4.0, 0.0, 9.0, 9.0]],
+                                dtype=object)
         comp_file_index = 1
         temp_labels = np.array(['catBobcat', 'test'])
-        assert similarity_maker(count_matrix, comp_file_index, temp_labels
-                                ) == ([0.0, 0.0], ['catBobcat', 'test'])
+        np.testing.assert_equal(
+            similarity_maker(count_matrix, comp_file_index, temp_labels),
+            (np.array([0.0, 0.0]), np.array(['catBobcat', 'test']))
+        )
 
     def test_with_two_dimension(self):
-        count_matrix = np.array([['cat', 'the'], [0.0, 1.0], [1.0, 2.0],
-                                 [2.0, 1.0]])
+        count_matrix = np.array([[0.0, 1.0], [1.0, 2.0],
+                                 [2.0, 1.0]], dtype=object)
         comp_file_index = 0
         temp_labels = np.array(['file2', 'file3'])
-        assert similarity_maker(count_matrix, comp_file_index, temp_labels
-                                ) == ([0.1056, 0.5528], ['file2', 'file3'])
+        np.testing.assert_equal(
+            similarity_maker(count_matrix, comp_file_index, temp_labels),
+            (np.array([0.1056, 0.5528]), np.array(['file2', 'file3']))
+        )
 
     def test_with_three_dimension(self):
-        count_matrix = np.array([['I', 'dogs', 'love'],
-                                 [1.0, 1.0, 1.0],
+        count_matrix = np.array([[1.0, 1.0, 1.0],
                                  [1.0, 0.0, 0.0],
-                                 [0.0, 2.0, 1.0]])
+                                 [0.0, 2.0, 1.0]], dtype=object)
         comp_file_index = 1
         temp_labels = np.array(['file_1', 'file_3'])
-        assert similarity_maker(count_matrix, comp_file_index, temp_labels
-                                ) == ([0.4226, 1.0], ['file_1', 'file_3'])
+        np.testing.assert_equal(
+            similarity_maker(count_matrix, comp_file_index, temp_labels),
+            (np.array([0.4226, 1.0]), np.array(['file_1', 'file_3'])))
 
     def test_similarity_maker_non_neg_index_precondition(self):
         try:
-            count_matrix = np.array([['test'], [1.0], [1.0]])
+            count_matrix = np.array([[1.0], [1.0]], dtype=object)
             _ = similarity_maker(count_matrix, comp_file_index=-1,
                                  temp_labels=np.array(['test_1']))
             raise AssertionError("negative index error did not raise.")
@@ -65,7 +65,7 @@ class TestSimilarity:
 
     def test_similarity_maker_empty_temp_labels_precondition(self):
         try:
-            count_matrix = np.array([['test'], [1.0], [1.0]])
+            count_matrix = np.array([[1.0], [1.0]], dtype=object)
             _ = similarity_maker(count_matrix, comp_file_index=1,
                                  temp_labels=np.array([]))
             raise AssertionError("empty list error did not raise.")
