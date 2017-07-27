6fbb5793f7ac5a6f21c8ffda088ad1c936525633
ariannaalfiero <alfiero_arianna@wheatoncollege.edu>
2017-07-27 10:59:41 -0400

    added more basic unit testing

--- test/unit_test/test_rw_analyzer.py
+++ test/unit_test/test_rw_analyzer.py
@@ -57,11 +57,63 @@ def test_r_word_line():
                        second_word="test", window_size=1) == [0.5, 1.0, 0]
 
 
-# def test_rw_analyze():
-#     assert rw_analyze(file_string=["testt", "testt"], count_type='ratio',
-#                       token_type='string', window_type='word', key_word='t',
-#                       second_key_word='s',
-#                       window_size_str='1') == (
-#         [0.75, 0.75],
-#         "Ratio of t's to (number of t's + number of s's) in a window of words",
-#         "First word in a window", "Ratio")
+def test_rw_analyze():
+    assert rw_analyze(file_string="test", count_type='average',
+                      token_type='string', window_type='letter', key_word='t',
+                      second_key_word='',
+                      window_size_str='1') == (
+               [[1.0, 0, 0, 1.0]],
+               "Average number of t's in a window of 1 characters.",
+               "First character in window", "Average")
+    assert rw_analyze(file_string="test test", count_type='average',
+                      token_type='string', window_type='word', key_word='t',
+                      second_key_word='',
+                      window_size_str='1') == (
+               [[2.0, 2.0]],
+               "Average number of t's in a window of 1 words.",
+               "First word in window", "Average")
+    assert rw_analyze(file_string="test test", count_type='average',
+                      token_type='word', window_type='word', key_word='test',
+                      second_key_word='',
+                      window_size_str='1') == (
+               [[1.0, 1.0]],
+               "Average number of test's in a window of 1 words.",
+               "First word in window", "Average")
+    # leave this space for test on a_word_line
+    assert rw_analyze(file_string="test", count_type='ratio',
+                      token_type='string', window_type='letter', key_word='t',
+                      second_key_word='s',
+                      window_size_str='1') == (
+               [[1.0, 0, 0, 1.0]],
+               "Ratio of t's to (number of t's + number of s's) in a window"
+               " of 1 characters.", "First character in window", "Ratio")
+    assert rw_analyze(file_string="testt testt", count_type='ratio',
+                      token_type='string', window_type='word', key_word='t',
+                      second_key_word='s',
+                      window_size_str='1') == (
+        [[0.75, 0.75]],
+        "Ratio of t's to (number of t's + number of s's) in a window"
+        " of 1 words.", "First word in window", "Ratio")
+    assert rw_analyze(file_string="test hello", count_type='ratio',
+                      token_type='word', window_type='word', key_word='test',
+                      second_key_word='hello',
+                      window_size_str='1') == (
+               [[1.0, 0]],
+               "Ratio of test's to (number of test's + number of hello's) in a"
+               " window of 1 words.", "First word in window", "Ratio")
+    assert rw_analyze(file_string="hello test\r hello world\r"
+                                  "this is a test", count_type='ratio',
+                      token_type='word', window_type='line', key_word='test',
+                      second_key_word='hello',
+                      window_size_str='1') == (
+               [[0.5, 0, 1.0]],
+               "Ratio of test's to (number of test's + number of hello's) in a"
+               " window of 1 lines.", "First line in window", "Ratio")
+    assert rw_analyze(file_string="hello test\n hello world\n"
+                                  "this is a test", count_type='ratio',
+                      token_type='word', window_type='line', key_word='test',
+                      second_key_word='hello',
+                      window_size_str='1') == (
+               [[0.5, 0, 1.0]],
+               "Ratio of test's to (number of test's + number of hello's) in a"
+               " window of 1 lines.", "First line in window", "Ratio")
