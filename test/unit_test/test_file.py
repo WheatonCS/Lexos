ab5dcf1ebd030e99cc6a258d9088cfd09664a08e
xliu <liu_xinru@wheatoncollege.edu>
2017-07-27 12:18:40 -0400

    modify test_file

--- test/unit_test/test_file.py
+++ test/unit_test/test_file.py
@@ -2,13 +2,12 @@ import numpy as np
 from sklearn.metrics.pairwise import cosine_similarity
 
 
-final_matrix = [[1, 2, 3], [4, 5, 6], [7, 8, 9], [10, 11, 12], [13, 14, 15]]
-raw_matrix = np.delete(np.delete(final_matrix, 0, 0), 0, 1)
-dist = 1 - cosine_similarity(raw_matrix)
+final_matrix = np.array([[1, 2, 3], [4, 5, 6], [7, 8, 9], [10, 11, 12], [13, 14, 15]])
+dist = 1 - cosine_similarity(final_matrix)
 comp_file_index = 1
 temp_labels = np.array(['one', 'two', 'three', 'four'])
 other_file_indexes = np.asarray([file_index for file_index in range(
-        raw_matrix.shape[0]) if file_index != comp_file_index])
+        final_matrix.shape[0]) if file_index != comp_file_index])
 docs_np_score = np.asarray([dist[file_index, comp_file_index]
                            for file_index in other_file_indexes])
 docs_np_name = np.asarray([temp_labels[i] for i in range(
@@ -19,4 +18,4 @@ sorted_docs_np = docs_np[docs_np[:, 1].astype(float).argsort()]
 docs_name = sorted_docs_np[:, 0]
 docs_score = np.round(sorted_docs_np[:, 1].astype(float), decimals=4)
 
-print(raw_matrix)
+print(final_matrix)
