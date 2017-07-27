fcd5718fe3aec5f4053a1e6346aff8ea1b04099d
chantisnake <13501393281@163.com>
2017-07-27 13:33:37 -0400

    add include data and test_suit change style

--- setup.py
+++ setup.py
@@ -1,25 +1,28 @@
 from setuptools import setup, find_packages
 
-setup(name="Lexos",
-      version="4.0rc1",
-      description="Python/Flask-based website for text analysis workflow.",
-      long_description="""TODO""",
-      author="WheatonCS/LexomicsResearch",
-      url="http://wheatoncollege.edu/lexomics/introduction-lexomics/",
-      download_url="TODO",
-      license="MIT",
-      install_requires=[
-          'flask',
-          'numpy',
-          'scikit-learn',
-          'scipy',
-          'beautifulsoup4',
-          'lxml',
-          'matplotlib',
-          'chardet',
-          'natsort',
-          'plotly',
-          'gensim'
-      ],
-      packages=find_packages('lexos')
-      )
+setup(
+    name="Lexos",
+    version="4.0rc1",
+    description="Python/Flask-based website for text analysis workflow.",
+    long_description="""TODO""",
+    author="WheatonCS/LexomicsResearch",
+    url="http://wheatoncollege.edu/lexomics/introduction-lexomics/",
+    download_url="TODO",
+    license="MIT",
+    include_package_data=True,
+    test_suite="tests",
+    install_requires=[
+        'flask',
+        'numpy',
+        'scikit-learn',
+        'scipy',
+        'beautifulsoup4',
+        'lxml',
+        'matplotlib',
+        'chardet',
+        'natsort',
+        'plotly',
+        'gensim'
+    ],
+    packages=find_packages(exclude="./test/*")
+)
