fe25d68f4dfe9ac36812401352da216007bc0a06
chantisnake <13501393281@163.com>
2017-07-27 12:16:13 -0400

    create basic setup.py

---
+++ setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup, find_packages
+
+setup(name="Lexos",
+      version="4.0rc1",
+      description="Python/Flask-based website for text analysis workflow.",
+      long_description="""TODO""",
+      author="WheatonCS/LexomicsResearch",
+      url="http://wheatoncollege.edu/lexomics/introduction-lexomics/",
+      download_url="TODO",
+      license="MIT",
+      requires=[
+          'flask',
+          'numpy',
+          'scikit-learn',
+          'scipy',
+          'beautifulsoup4',
+          'lxml',
+          'matplotlib',
+          'chardet',
+          'natsort',
+          'plotly',
+          'gensim'
+      ],
+      packages=find_packages('lexos')
+      )
