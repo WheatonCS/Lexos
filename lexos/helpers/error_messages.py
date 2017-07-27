91ec2c1837a7bb3889fc2b156b23cc5e86a4379d
Shi26 <darias0826@gmail.com>
2017-07-27 11:49:35 -0400

    fix error message naming

--- lexos/helpers/error_messages.py
+++ lexos/helpers/error_messages.py
@@ -5,8 +5,8 @@ PROP_NEGATIVE_MESSAGE = "The proportional size of the last segment must be " \
     "zero or greater"
 OVERLAP_NEGATIVE_MESSAGE = "The overlap between chunks must be zero or greater"
 NON_POSITIVE_SEGMENT_MESSAGE = "the segment size should be positive"
-NEG_NUM_MESSAGE = "the overlap or last segment proportion should " \
-    "not be negative"
+NEG_OVERLAP_LAST_PROP_MESSAGE = "the overlap or last segment proportion" \
+                                "should not be negative"
 LARGER_CHUNK_SIZE_MESSAGE = "the segment size should be larger " \
     "than the overlap size"
 NON_NEGATIVE_INDEX_MESSAGE = "the index should be larger than or equal to zero"
