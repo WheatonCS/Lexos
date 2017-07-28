9f488c4a33d6185f6a94514a2ca4132599ff7fbf
Weiqi97 <weltch1997@gmail.com>
2017-07-28 16:12:21 -0400

    Add more tests

--- test/unit_test/test_utility_topword.py
+++ test/unit_test/test_utility_topword.py
@@ -2,12 +2,11 @@ from lexos.processors.analyze.topword import _z_test_
 
 
 class TestZTest:
-    # def test_normal_case(self):
-        # assert _z_test_(p1=0.1, pt=0.3, n1=100, nt=10000) ==
+    def test_normal_case(self):
+        assert round(_z_test_(p1=0.1, pt=0.3, n1=100, nt=10000), 2) == -4.35
 
     def test_special_case(self):
-        try:
-            _ = _z_test_(p1=0.1, pt=0.3, n1=0, nt=0)
-            raise AssertionError("Division by zero message did not raise")
-        except ZeroDivisionError:
-            assert _ == "Insignificant"
+        assert _z_test_(p1=0.1, pt=0.3, n1=0, nt=0) == "Insignificant"
+        assert _z_test_(p1=0.1, pt=0.3, n1=100, nt=0) == "Insignificant"
+        assert _z_test_(p1=0.1, pt=0.3, n1=0, nt=100) == "Insignificant"
+
