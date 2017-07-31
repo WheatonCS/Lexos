ccfe1a055e918645d4eb158d878f3fa95dd9176f
alvaro <delandaluce_alvaro@wheatoncollege.edu>
2017-07-31 15:29:23 -0400

    fix indentation errors

--- test/unit_test/test_general_functions.py
+++ test/unit_test/test_general_functions.py
@@ -36,13 +36,12 @@ class TestGeneralFunctions(unittest.TestCase):
         assert generate_d3_object(
             {'a': 1, 'b': 2, 'c': 3, 'd': 4},
             "object", "word", "count") == {'name': 'object', 'children': [{
-            'word': 'a', 'count': 1}, {'word': 'b', 'count': 2}, {
-            'word': 'c', 'count': 3}, {'word': 'd', 'count': 4}]}
+                'word': 'a', 'count': 1}, {'word': 'b', 'count': 2}, {
+                'word': 'c', 'count': 3}, {'word': 'd', 'count': 4}]}
 
     def test_merge_list(self):
-        assert merge_list(
-            [{"a": 1, "b": 2}, {"c": 3, "d": 4}]) == {
-                   'a': 1, 'b': 2, 'c': 3, 'd': 4}
+        assert merge_list([{"a": 1, "b": 2}, {"c": 3, "d": 4}]) == {
+            'a': 1, 'b': 2, 'c': 3, 'd': 4}
 
     def test_load_stastic(self):
         assert load_stastic(
