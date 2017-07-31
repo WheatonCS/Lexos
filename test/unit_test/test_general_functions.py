2da46918f766bf8505d3f541fda4dc77ac1ddf03
alvaro <delandaluce_alvaro@wheatoncollege.edu>
2017-07-31 14:43:22 -0400

    fix unit test errors

--- test/unit_test/test_general_functions.py
+++ test/unit_test/test_general_functions.py
@@ -1,9 +1,7 @@
 from lexos.helpers.general_functions import get_encoding, make_preview_from, \
-    generate_d3_object, copy_dir, merge_list, load_stastic, matrix_to_dict, \
+    generate_d3_object, merge_list, load_stastic, matrix_to_dict, \
     dict_to_matrix, html_escape, apply_function_exclude_tags, decode_bytes
 import unittest
-import os
-import shutil
 
 
 class TestGeneralFunctions(unittest.TestCase):
@@ -29,7 +27,7 @@ class TestGeneralFunctions(unittest.TestCase):
         assert make_preview_from(str_500) == str_500
         assert make_preview_from(
             more_than_500_char_even) == \
-               str_250 + less_than_500_char + middle + less_than_500_char +\
+               str_250 + less_than_500_char + middle + less_than_500_char + \
                str_250
 
         assert make_preview_from(
@@ -40,21 +38,19 @@ class TestGeneralFunctions(unittest.TestCase):
     def test_generate_d3_object(self):
         assert generate_d3_object(
             {'a': 1, 'b': 2, 'c': 3, 'd': 4},
-            "object", "word", "count") == {'name': 'object', 'children':
-            [{'word': 'a', 'count': 1},
-             {'word': 'b', 'count': 2},
-             {'word': 'c', 'count': 3},
-             {'word': 'd', 'count': 4}]}
+            "object", "word", "count") == {'name': 'object', 'children': [{
+            'word': 'a', 'count': 1}, {'word': 'b', 'count': 2}, {
+            'word': 'c', 'count': 3}, {'word': 'd', 'count': 4}]}
 
     def test_merge_list(self):
         assert merge_list(
-            [{"a": 1, "b": 2}, {"c": 3, "d": 4}]) == \
-               {'a': 1, 'b': 2, 'c': 3, 'd': 4}
+            [{"a": 1, "b": 2}, {"c": 3, "d": 4}]) == {
+                   'a': 1, 'b': 2, 'c': 3, 'd': 4}
 
     def test_load_stastic(self):
         assert load_stastic(
-            "this is a string string") == \
-               {"this": 1, "is": 1, "a": 1, "string": 2}
+            "this is a string string") == {
+            "this": 1, "is": 1, "a": 1, "string": 2}
 
     def test_matrix_to_dict(self):
         assert matrix_to_dict(
@@ -63,9 +59,9 @@ class TestGeneralFunctions(unittest.TestCase):
 
     def test_dict_to_matrix(self):
         assert dict_to_matrix(
-            [{'a': 1, 'b': 2, 'c': 3, 'd': 4}]) == \
-               ([['', 'a', 'b', 'c', 'd'], [0, 1, 2, 3, 4]],
-                ['a', 'b', 'c', 'd'])
+            [{'a': 1, 'b': 2, 'c': 3, 'd': 4}]) == (
+            [['', 'a', 'b', 'c', 'd'], [0, 1, 2, 3, 4]],
+            ['a', 'b', 'c', 'd'])
 
     def test_html_escape(self):
         assert html_escape("&") == "&amp;"
