38a35f53704835ead64675673cf67bb0ee4b05d6
Weiqi97 <weltch1997@gmail.com>
2017-07-31 16:32:34 -0400

    Add more tests

--- test/unit_test/test_utility_topword.py
+++ test/unit_test/test_utility_topword.py
@@ -1,5 +1,6 @@
 from lexos.helpers.error_messages import EMPTY_LIST_MESSAGE
-from lexos.processors.analyze.topword import _z_test_, analyze_all_to_para
+from lexos.processors.analyze.topword import _z_test_, analyze_all_to_para, \
+    group_division, analyze_para_to_group
 
 
 class TestZTest:
@@ -12,7 +13,7 @@ class TestZTest:
         assert _z_test_(p1=0.1, pt=0.3, n1=100, nt=0) == "Insignificant"
         assert _z_test_(p1=0.1, pt=0.3, n1=0, nt=100) == "Insignificant"
 
-
+# Create test suite for next part of test
 word_lists_one = [{'C': 1.0, 'D': 1.0}, {'A': 1.0, 'B': 1.0}]
 word_lists_two = [{'C': 1.0, 'D': 10.0}, {'A': 1.0, 'B': 1.0}]
 word_lists_empty = []
@@ -30,3 +31,41 @@ class TestAnalyzeAllToPara:
             raise AssertionError("Empty input error message did not raise")
         except AssertionError as error:
             assert str(error) == EMPTY_LIST_MESSAGE
+
+# Create test suite for next part of test
+word_lists = [{'G': 1.0, 'H': 1.0}, {'E': 1.0, 'F': 1.0},
+              {'C': 1.0, 'D': 1.0}, {'A': 1.0, 'B': 1.0}]
+group_map = [[0, 1], [2, 3]]
+group_map_empty = []
+
+
+class TestGroupDivision:
+    def test_normal_case(self):
+        assert group_division(word_lists=word_lists, group_map=group_map) == \
+            [[{'G': 1.0, 'H': 1.0}, {'E': 1.0, 'F': 1.0}],
+             [{'C': 1.0, 'D': 1.0}, {'A': 1.0, 'B': 1.0}]]
+
+    def test_special_case(self):
+        try:
+            _ = group_division(word_lists=word_lists,
+                               group_map=group_map_empty)
+            raise AssertionError("Empty input error message did not raise")
+        except AssertionError as error:
+            assert str(error) == EMPTY_LIST_MESSAGE
+
+        try:
+            _ = group_division(word_lists=word_lists_empty,
+                               group_map=group_map_empty)
+            raise AssertionError("Empty input error message did not raise")
+        except AssertionError as error:
+            assert str(error) == EMPTY_LIST_MESSAGE
+
+
+group_para_list_one = [[{'G': 1.0, 'H': 1.0}, {'E': 1.0, 'F': 1.0}],
+                       [{'C': 1.0, 'D': 1.0}, {'A': 1.0, 'B': 1.0}]]
+
+
+class TestAnalyzeParaToGroup:
+    def test_normal_case(self):
+        assert analyze_para_to_group(group_para_lists=group_para_list_one) == \
+               [{}, {}]
