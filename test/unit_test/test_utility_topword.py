f68b2e0ea3206f340e7ee57f2160720102d8ae94
Weiqi97 <weltch1997@gmail.com>
2017-07-31 14:53:50 -0400

    Add more test

--- test/unit_test/test_utility_topword.py
+++ test/unit_test/test_utility_topword.py
@@ -4,6 +4,7 @@ from lexos.processors.analyze.topword import _z_test_
 class TestZTest:
     def test_normal_case(self):
         assert round(_z_test_(p1=0.1, pt=0.3, n1=100, nt=10000), 2) == -4.35
+        assert round(_z_test_(p1=0.3, pt=0.1, n1=100, nt=100), 2) == 3.54
 
     def test_special_case(self):
         assert _z_test_(p1=0.1, pt=0.3, n1=0, nt=0) == "Insignificant"
