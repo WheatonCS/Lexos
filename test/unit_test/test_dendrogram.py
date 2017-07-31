b0f48ff08a1d1ceb3364a9984107798bf18e93fb
xliu <liu_xinru@wheatoncollege.edu>
2017-07-31 15:38:06 -0400

    add content to test_dendrogram

--- test/unit_test/test_dendrogram.py
+++ test/unit_test/test_dendrogram.py
@@ -1,5 +1,8 @@
+from lexos.processors.analyze.dendrogrammer import get_dendro_distances
 class GetDendroDistance:
     def test_dist_euclidean_single(self):
         linkage_method = "Single"
         distance_metric = "Euclidean"
         dendro_matrix = [[0, 10], [10, 10], [20, 10]]
+        assert get_dendro_distances(linkage_method, distance_metric,
+                                    dendro_matrix) == [1, 0, 3]
