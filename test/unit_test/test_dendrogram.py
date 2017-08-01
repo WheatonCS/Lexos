180530cf88addd91ef981139518f97e4bc18cb01
xliu <liu_xinru@wheatoncollege.edu>
2017-08-01 11:41:13 -0400

    add third test in test_dendrogram.py

--- test/unit_test/test_dendrogram.py
+++ test/unit_test/test_dendrogram.py
@@ -19,3 +19,12 @@ class TestGetDendroDistance:
                          [0.66666666666666663, 0.33333333333333331]]
         assert get_dendro_distances(linkage_method, distance_metric,
                                     dendro_matrix) == [0.10557, 0.2]
+
+    def test_dist_hamming_complete(self):
+        linkage_method = "complete"
+        distance_metric = "hamming"
+        dendro_matrix = [[0.0, 1.0],
+                         [0.33333333333333331, 0.66666666666666663],
+                         [0.66666666666666663, 0.33333333333333331]]
+        assert get_dendro_distances(linkage_method, distance_metric,
+                                     dendro_matrix) == [1.0, 1.0]
