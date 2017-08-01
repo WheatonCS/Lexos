559f577e0850f8baf4b76bf9658131305370159e
alvaro <delandaluce_alvaro@wheatoncollege.edu>
2017-08-01 10:05:29 -0400

    fix test_decode_bytes

--- test/unit_test/test_general_functions.py
+++ test/unit_test/test_general_functions.py
@@ -1,10 +1,9 @@
 from lexos.helpers.general_functions import get_encoding, make_preview_from, \
     generate_d3_object, merge_list, load_stastic, matrix_to_dict, \
     dict_to_matrix, html_escape, apply_function_exclude_tags, decode_bytes
-import unittest
 
 
-class TestGeneralFunctions(unittest.TestCase):
+class TestGeneralFunctions:
     def test_get_encoding(self):
         assert get_encoding(b"asdf") == "ascii"
 
@@ -75,4 +74,4 @@ class TestGeneralFunctions(unittest.TestCase):
             input_str, [dummy_function]) == "<tag>asdfasdf</tag>"
 
     def test_decode_bytes(self):
-        assert decode_bytes(u'哈哈'.encode('gb2312')) == "¹þ¹þ"
+        assert decode_bytes(u'做戏之说做戏之'.encode('gb2312')) == "做戏之说做戏之"
