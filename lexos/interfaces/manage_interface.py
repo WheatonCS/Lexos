54971ab8959bba6a530d4ab6d899e505d779be25
alvaro <delandaluce_alvaro@wheatoncollege.edu>
2017-08-02 14:48:47 -0400

    fix documentation for manage_interface

--- lexos/interfaces/manage_interface.py
+++ lexos/interfaces/manage_interface.py
@@ -17,19 +17,16 @@ manage_view = Blueprint('manage', __name__)
 # Tells Flask to load this function when someone is at '/select'
 @manage_view.route("/manage", methods=["GET", "POST"])
 def manage():
-    """
-    Handles the functionality of the select page. Its primary role is to
+    """Handles the functionality of the select page. Its primary role is to
     activate/deactivate specific files depending on the user's input.
-    Note: Returns a response object (often a render_template call) to flask
+
+    :return: a response object (often a render_template call) to flask
     and eventually to the browser.
     """
-
     # Detect the number of active documents.
     num_active_docs = detect_active_docs()
-
     # Usual loading of the FileManager
     file_manager = utility.load_file_manager()
-
     if request.method == "GET":
         rows = file_manager.get_previews_of_all()
         for row in rows:
@@ -37,13 +34,11 @@ def manage():
                 row["state"] = "selected"
             else:
                 row["state"] = ""
-
         return render_template(
             'manage.html',
             rows=rows,
             itm="manage",
             numActiveDocs=num_active_docs)
-
     if 'previewTest' in request.headers:
         file_id = int(request.data)
         file_label = file_manager.files[file_id].label
@@ -52,59 +47,49 @@ def manage():
             "id": file_id,
             "label": file_label,
             "previewText": file_preview}
-
         return json.dumps(preview_vals)
-
     if 'toggleFile' in request.headers:
         # Catch-all for any POST request.
         # On the select page, POSTs come from JavaScript AJAX XHRequests.
         file_id = int(request.data)
-
         # Toggle the file from active to inactive or vice versa
         file_manager.toggle_file(file_id)
-
     elif 'toggliFy' in request.headers:
         file_ids = request.data
         file_ids = file_ids.split(",")
         file_manager.disable_all()
-
         # Toggle the file from active to inactive or vice versa
         file_manager.togglify(file_ids)
-
     elif 'setLabel' in request.headers:
         new_name = (request.headers['setLabel'])
         file_id = int(request.data)
-
         file_manager.files[file_id].set_name(new_name)
         file_manager.files[file_id].label = new_name
-
     elif 'setClass' in request.headers:
         new_class_label = (request.headers['setClass'])
         file_id = int(request.data)
         file_manager.files[file_id].set_class_label(new_class_label)
-
     elif 'disableAll' in request.headers:
         file_manager.disable_all()
-
     elif 'selectAll' in request.headers:
         file_manager.enable_all()
-
     elif 'applyClassLabel' in request.headers:
         file_manager.classify_active_files()
-
     elif 'deleteActive' in request.headers:
         file_manager.delete_active_files()
-
     elif 'deleteRow' in request.headers:
         # delete the file in request.form
         file_manager.delete_files(list(request.form.keys()))
-
     utility.save_file_manager(file_manager)
     return ''  # Return an empty string because you have to return something
 
 
 @manage_view.route("/selectAll", methods=["GET", "POST"])
 def select_all():
+    """selects all files
+
+    :return: string indicating that it has succeeded 
+    """
     file_manager = utility.load_file_manager()
     file_manager.enable_all()
     utility.save_file_manager(file_manager)
@@ -113,6 +98,10 @@ def select_all():
 
 @manage_view.route("/deselectAll", methods=["GET", "POST"])
 def deselect_all():
+    """deletes all files
+
+    :return: string indicating that it has succeeded 
+    """
     file_manager = utility.load_file_manager()
     file_manager.disable_all()
     utility.save_file_manager(file_manager)
@@ -121,6 +110,10 @@ def deselect_all():
 
 @manage_view.route("/downloadDocuments", methods=["GET", "POST"])
 def download_documents():
+    """downloads all selected files
+
+    :return: a .zip file congaing all selected files
+    """
     # The 'Download Selected Documents' button is clicked in manage.html.
     # Sends zipped files to downloads folder.
     file_manager = utility.load_file_manager()
@@ -155,7 +148,6 @@ def get_previews():
         "id": file_id,
         "label": file_label,
         "previewText": file_preview}
-
     return json.dumps(preview_vals)
 
 
