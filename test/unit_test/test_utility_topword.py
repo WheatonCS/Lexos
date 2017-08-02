1adfbb091fe3fef1ed1d743644f6b7b29661c45f
Weiqi97 <weltch1997@gmail.com>
2017-08-02 14:49:34 -0400

    Fix unit test

--- test/unit_test/test_utility_topword.py
+++ test/unit_test/test_utility_topword.py
@@ -1,8 +1,10 @@
 import numpy as np
 import pandas as pd
 
-from lexos.helpers.error_messages import EMPTY_LIST_MESSAGE
-from lexos.processors.analyze.topword import _z_test_, analyze_all_to_para
+from lexos.helpers.error_messages import EMPTY_NP_ARRAY_MESSAGE, \
+    EMPTY_LIST_MESSAGE
+from lexos.processors.analyze.topword import _z_test_, analyze_all_to_para, \
+    group_division, analyze_para_to_group, analyze_group_to_group
 
 
 class TestZTest:
@@ -41,74 +43,98 @@ class TestAnalyzeAllToPara:
                                     words=empty_data.columns.values)
             raise AssertionError("Empty input error message did not raise")
         except AssertionError as error:
-            assert str(error) == EMPTY_LIST_MESSAGE
+            assert str(error) == EMPTY_NP_ARRAY_MESSAGE
 
 
-"""
 # Create test suite for next part of test
-word_lists = [{'G': 1.0, 'H': 1.0}, {'E': 1.0, 'F': 1.0},
-              {'C': 1.0, 'D': 1.0}, {'A': 1.0, 'B': 1.0}]
-group_map = [[0, 1], [2, 3]]
-group_map_empty = []
+dtm_data_three = pd.DataFrame(data=np.array([(1, 0, 0, 0), (0, 1, 0, 0),
+                                             (0, 0, 1, 0), (0, 0, 0, 1)]),
+                              index=np.array(["F1", "F2", "F3", "F4"]),
+                              columns=np.array(["A", "B", "C", "D"]))
+group_map = np.array([(True, True, False, False), (False, False, True, True)])
+group_map_empty = np.array([])
 
 
 class TestGroupDivision:
     def test_normal_case(self):
-        assert group_division(word_lists=word_lists, group_map=group_map) == \
-            [[{'G': 1.0, 'H': 1.0}, {'E': 1.0, 'F': 1.0}],
-             [{'C': 1.0, 'D': 1.0}, {'A': 1.0, 'B': 1.0}]]
+        label_list, group_list = group_division(dtm=dtm_data_three,
+                                                division_map=group_map)
+        assert np.array_equal(label_list, [([[1, 0, 0, 0], [0, 1, 0, 0]]),
+                                           ([[0, 0, 1, 0], [0, 0, 0, 1]])])
+        assert np.array_equal(group_list, [(['F1', 'F2']), (['F3', 'F4'])])
 
     def test_special_case(self):
         try:
-            _ = group_division(word_lists=word_lists,
-                               group_map=group_map_empty)
+            _ = group_division(dtm=empty_data, division_map=group_map)
             raise AssertionError("Empty input error message did not raise")
         except AssertionError as error:
-            assert str(error) == EMPTY_LIST_MESSAGE
+            assert str(error) == EMPTY_NP_ARRAY_MESSAGE
 
         try:
-            _ = group_division(word_lists=empty_list,
-                               group_map=group_map_empty)
+            _ = group_division(dtm=dtm_data_one, division_map=group_map_empty)
             raise AssertionError("Empty input error message did not raise")
         except AssertionError as error:
-            assert str(error) == EMPTY_LIST_MESSAGE
+            assert str(error) == EMPTY_NP_ARRAY_MESSAGE
 
 
 # Create test suite for next part of test
-group_para_list_one = [[{'G': 1.0, 'H': 1.0}, {'E': 1.0, 'F': 1.0}],
-                       [{'C': 1.0, 'D': 1.0}, {'A': 1.0, 'B': 1.0}]]
-group_para_list_two = [[{'G': 10.0, 'H': 1.0}, {'E': 1.0, 'F': 1.0}],
-                       [{'C': 1.0, 'D': 1.0}, {'A': 1.0, 'B': 1.0}]]
+empty_group = []
+empty_words = np.array([])
+words = np.array(["A", "B", "C", "D", "E", "F", "G", "H"])
+group_para_list_one = \
+    [np.array([(1, 1, 0, 0, 0, 0, 0, 0), (0, 0, 1, 1, 0, 0, 0, 0)]),
+     np.array([(0, 0, 0, 0, 1, 1, 0, 0), (0, 0, 0, 0, 0, 0, 1, 1)])]
+group_para_list_two = \
+    [np.array([(1, 1, 0, 0, 0, 0, 0, 0), (0, 0, 1, 1, 0, 0, 0, 0)]),
+     np.array([(0, 0, 0, 0, 1, 1, 0, 0), (0, 0, 0, 0, 0, 0, 1, 10)])]
 
 
 class TestAnalyzeParaToGroup:
     def test_normal_case(self):
-        assert analyze_para_to_group(group_para_lists=group_para_list_one) == \
+        assert analyze_para_to_group(group_values=group_para_list_one,
+                                     words=words) == \
             {(0, 0, 1): [], (0, 1, 1): [], (1, 0, 0): [], (1, 1, 0): []}
-        assert analyze_para_to_group(group_para_lists=group_para_list_two) == \
-            {(0, 0, 1): [('G', 3.3029)], (0, 1, 1): [],
-             (1, 0, 0): [('C', 2.639), ('D', 2.639), ('G', -2.1483)],
-             (1, 1, 0): [('A', 2.639), ('B', 2.639), ('G', -2.1483)]}
+        assert analyze_para_to_group(group_values=group_para_list_two,
+                                     words=words) == \
+               {(0, 0, 1): [('A', 2.639), ('B', 2.639), ('H', -2.1483)],
+                (0, 1, 1): [('C', 2.639), ('D', 2.639), ('H', -2.1483)],
+                (1, 0, 0): [], (1, 1, 0): [('H', 3.3029)]}
 
     def test_special_case(self):
         try:
-            _ = analyze_para_to_group(group_para_lists=empty_list)
+            _ = analyze_para_to_group(group_values=empty_group,
+                                      words=words)
             raise AssertionError("Empty input error message did not raise")
         except AssertionError as error:
             assert str(error) == EMPTY_LIST_MESSAGE
+
+        try:
+            _ = analyze_para_to_group(group_values=group_para_list_one,
+                                      words=empty_words)
+            raise AssertionError("Empty input error message did not raise")
+        except AssertionError as error:
+            assert str(error) == EMPTY_NP_ARRAY_MESSAGE
 
 
 class TestGroupToGroup:
     def test_normal_case(self):
-        assert analyze_group_to_group(group_para_lists=group_para_list_one) \
-            == {(0, 1): []}
-        assert analyze_group_to_group(group_para_lists=group_para_list_two) \
-            == {(0, 1): [('G', 2.7336)]}
+        assert analyze_group_to_group(group_values=group_para_list_one,
+                                      words=words) == {(0, 1): []}
+        assert analyze_group_to_group(group_values=group_para_list_two,
+                                      words=words) == \
+            {(0, 1): [('H', -2.7336)]}
 
     def test_special_case(self):
         try:
-            _ = analyze_group_to_group(group_para_lists=empty_list)
+            _ = analyze_group_to_group(group_values=empty_group,
+                                       words=words)
             raise AssertionError("Empty input error message did not raise")
         except AssertionError as error:
             assert str(error) == EMPTY_LIST_MESSAGE
-"""
+
+        try:
+            _ = analyze_group_to_group(group_values=group_para_list_one,
+                                       words=empty_words)
+            raise AssertionError("Empty input error message did not raise")
+        except AssertionError as error:
+            assert str(error) == EMPTY_NP_ARRAY_MESSAGE
