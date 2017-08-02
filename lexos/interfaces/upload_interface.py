a6b82713647519898ea0b01829339f9d24ba72db
alvaro <delandaluce_alvaro@wheatoncollege.edu>
2017-08-02 15:21:16 -0400

    fix documentation for upload_interface

--- lexos/interfaces/upload_interface.py
+++ lexos/interfaces/upload_interface.py
@@ -20,27 +20,22 @@ upload_view = Blueprint('upload', __name__)
 
 @upload_view.route("/upload", methods=["GET", "POST"])
 def upload():
-    """
-    Handles the functionality of the upload page. It uploads files to be used
-    in the current session.
-    Note: Returns a response object (often a render_template call) to flask and
+    """Handles the functionality of the upload page. It uploads files to be
+    used in the current session.
+
+    :return: a response object (often a render_template call) to flask and
      eventually to the browser.
     """
-
     # Detect the number of active documents.
     num_active_docs = detect_active_docs()
-
     if request.method == "GET":
-
         print("About to fix session in case of browser caching")
         # fix the session in case the browser is caching the old session
         session_manager.fix()
         print("Session fixed. Rendering template.")
-
         if 'generalsettings' not in session:
             session['generalsettings'] = \
                 constants.DEFAULT_GENERALSETTINGS_OPTIONS
-
         return render_template(
             'upload.html',
             MAX_FILE_SIZE=constants.MAX_FILE_SIZE,
@@ -48,13 +43,10 @@ def upload():
             MAX_FILE_SIZE_UNITS=constants.MAX_FILE_SIZE_UNITS,
             itm="upload-tool",
             numActiveDocs=num_active_docs)
-
     # X_FILENAME is the flag to signify a file upload
     if 'X_FILENAME' in request.headers:
-
         # File upload through javascript
         file_manager = utility.load_file_manager()
-
         # --- check file name ---
         # Grab the filename, which will be UTF-8 percent-encoded (e.g. '%E7'
         # instead of python's '\xe7')
@@ -63,17 +55,13 @@ def upload():
         # '\xe7')
         file_name = unquote(file_name)
         # --- end check file name ---
-
         if file_name.endswith('.lexos'):
             file_manager.handle_upload_workspace()
-
             # update filemanager
             file_manager = utility.load_file_manager()
             file_manager.update_workspace()
-
         else:
             file_manager.add_upload_file(request.data, file_name)
-
         utility.save_file_manager(file_manager)
         return 'success'
 
@@ -82,10 +70,8 @@ def upload():
 def scrape():
     # Detect the number of active documents.
     num_active_docs = detect_active_docs()
-
     if request.method == "GET":
         return render_template('scrape.html', numActiveDocs=num_active_docs)
-
     if request.method == "POST":
         import requests
         urls = request.json["urls"]
