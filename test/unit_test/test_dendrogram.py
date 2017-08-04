e015ca1efeee64cf3a67ad73fc1e61546483dde5
xliu <liu_xinru@wheatoncollege.edu>
2017-08-04 11:58:53 -0400

    add precondition tests to test_dendrogram.py

--- test/unit_test/test_dendrogram.py
+++ test/unit_test/test_dendrogram.py
@@ -1,11 +1,12 @@
-from lexos.processors.analyze.dendrogrammer import get_dendro_distances, \
-    get_silhouette_score
+from lexos.helpers.error_messages import EMPTY_STRING_MESSAGE, \
+    EMPTY_LIST_MESSAGE
+from lexos.processors.analyze.dendrogrammer import get_dendro_distances
 
 
 class TestGetDendroDistance:
     def test_dist_euclidean_average(self):
-        linkage_method = "average"
-        distance_metric = "euclidean"
+        linkage_method = 'average'
+        distance_metric = 'euclidean'
         dendro_matrix = [[0.0, 1.0],
                          [0.33333333333333331, 0.66666666666666663],
                          [0.66666666666666663, 0.33333333333333331]]
@@ -13,8 +14,8 @@ class TestGetDendroDistance:
                                     dendro_matrix) == [0.47140, 0.70711]
 
     def test_dist_cosine_single(self):
-        linkage_method = "single"
-        distance_metric = "cosine"
+        linkage_method = 'single'
+        distance_metric = 'cosine'
         dendro_matrix = [[0.0, 1.0],
                          [0.33333333333333331, 0.66666666666666663],
                          [0.66666666666666663, 0.33333333333333331]]
@@ -22,44 +23,45 @@ class TestGetDendroDistance:
                                     dendro_matrix) == [0.10557, 0.2]
 
     def test_dist_hamming_complete(self):
-        linkage_method = "complete"
-        distance_metric = "hamming"
+        linkage_method = 'complete'
+        distance_metric = 'hamming'
         dendro_matrix = [[0.0, 1.0],
                          [0.33333333333333331, 0.66666666666666663],
                          [0.66666666666666663, 0.33333333333333331]]
         assert get_dendro_distances(linkage_method, distance_metric,
                                     dendro_matrix) == [1.0, 1.0]
 
+    def test_dist_empty_lm_str_precondition(self):
+        try:
+            _ = get_dendro_distances(linkage_method='',
+                                     distance_metric='cosine',
+                                     dendro_matrix=[[0.0, 1.0],
+                                                    [0.33333333333333331,
+                                                     0.66666666666666663],
+                                                    [0.66666666666666663,
+                                                     0.33333333333333331]])
+            raise AssertionError("empty string error did not raise.")
+        except AssertionError as error:
+            assert str(error) == EMPTY_STRING_MESSAGE
 
-class TestSilhouetteScore:
-    def test_score_euclidean_average(self):
-        linkage_method = "average"
-        distance_metric = "euclidean"
-        dendro_matrix = [[0.0, 0.0, 0.20000000000000001, 0.20000000000000001,
-                         0.0, 0.0, 0.1111111111111111, 0.088888888888888892,
-                         0.0, 0.20000000000000001, 0.0, 0.0, 0.0, 0.0, 0.0,
-                         0.0, 0.0, 0.20000000000000001, 0.0, 0.0, 0.0],
-                         [0.0, 0.0, 0.20000000000000001, 0.20000000000000001,
-                         0.0, 0.0, 0.0, 0.088888888888888892,
-                         0.1111111111111111, 0.20000000000000001, 0.0, 0.0,
-                         0.0, 0.0, 0.0, 0.0, 0.0, 0.20000000000000001, 0.0,
-                         0.0, 0.0],
-                         [0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0,
-                         0.66666666666666663, 0.0, 0.0, 0.33333333333333331,
-                         0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0],
-                         [0.066666666666666666, 0.13333333333333333, 0.0,
-                         0.0, 0.13333333333333333, 0.066666666666666666,
-                         0.0, 0.0, 0.0, 0.0, 0.0, 0.13333333333333333,
-                         0.066666666666666666, 0.0, 0.066666666666666666,
-                         0.066666666666666666, 0.066666666666666666, 0.0,
-                         0.066666666666666666, 0.066666666666666666,
-                         0.066666666666666666]]
-        labels = ['catBobcat', 'catCaterpillar', 'file_3', 'wake']
-        assert get_silhouette_score(dendro_matrix, distance_metric,
-                                    linkage_method, labels) == \
-            ('Silhouette Score: 0.3894\n(-1 ≤ Silhouette Score ≤ 1)',
-                'The best value is 1 and the worst value is -1. Values near 0 '
-                'indicate overlapping clusters. Negative values generally '
-                'indicate that a sample has been assigned to the wrong '
-                'cluster, as a different cluster is more similar.', 0.3894,
-             0.7, 3, 1.36, 0.23, 1.09, 0.23, 3)
+    def test_dist_empty_dm_str_precondition(self):
+        try:
+            _ = get_dendro_distances(linkage_method='average',
+                                     distance_metric='',
+                                     dendro_matrix=[[0.0, 1.0],
+                                                    [0.33333333333333331,
+                                                     0.66666666666666663],
+                                                    [0.66666666666666663,
+                                                     0.33333333333333331]])
+            raise AssertionError("empty string error did not raise.")
+        except AssertionError as error:
+            assert str(error) == EMPTY_STRING_MESSAGE
+
+    def test_dist_empty_matrix_precondition(self):
+        try:
+            _ = get_dendro_distances(linkage_method='average',
+                                     distance_metric='cosine',
+                                     dendro_matrix=[])
+            raise AssertionError("empty list error did not raise.")
+        except AssertionError as error:
+            assert str(error) == EMPTY_LIST_MESSAGE
