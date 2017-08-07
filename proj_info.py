2dce8b923fd4ea24806ec850a6525aec4490956a
chantisnake <13501393281@163.com>
2017-08-07 15:00:07 -0400

    add PROJECT_ROOT_PATH to infos

--- proj_info.py
+++ proj_info.py
@@ -1,3 +1,9 @@
+"""Used to store project information.
+
+This file need to be stored in the project root dir"""
+
+import os
+
 SHORT_NAME = 'Lexos'
 VERSION = '4.0rc1'
 ABOUT_URL = 'http://wheatoncollege.edu/lexomics/'
@@ -9,4 +15,4 @@ SHORT_DESCRIPTION = 'Python/Flask-based website for text analysis workflow. ' \
                     'Previous (stable) release is live at: ' \
                     'http://lexos.wheatoncollege.edu '
 LONG_DESCRIPTION = """TODO"""
-
+PROJECT_ROOT_PATH = os.path.dirname(__file__)
