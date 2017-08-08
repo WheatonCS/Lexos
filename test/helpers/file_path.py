9f48cf50e008297e81be1d99aa2949903f1e484b
chantisnake <13501393281@163.com>
2017-08-08 16:32:54 -0400

    add the chinese encoding file

--- test/helpers/file_path.py
+++ test/helpers/file_path.py
@@ -5,3 +5,8 @@ __current_file_dir__ = dir_path = os.path.dirname(os.path.realpath(__file__))
 
 TEST_SUITE_PATH = path_join(__current_file_dir__, '..', 'test_suite')
 
+
+UPLOAD_TEST_SUITE_PATH = path_join(TEST_SUITE_PATH, 'Upload')
+
+ENCODING_TEST_SUITE_PATH = path_join(UPLOAD_TEST_SUITE_PATH, 'encoding')
+CHINESE_ENCODING_TEST_FILE = path_join(ENCODING_TEST_SUITE_PATH, 'chinese')
