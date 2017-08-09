af6d5e5b02bb4f3e347d25087325ab30d3c22f49
Shi26 <darias0826@gmail.com>
2017-08-09 14:35:33 -0400

    change all the word "chunk" into "segment"

--- lexos/helpers/error_messages.py
+++ lexos/helpers/error_messages.py
@@ -3,11 +3,12 @@ OVERLAP_LARGE_MESSAGE = "The segment size must be greater than " \
     "the overlap of words between chunks"
 PROP_NEGATIVE_MESSAGE = "The proportional size of the last segment must be " \
     "zero or greater"
-OVERLAP_NEGATIVE_MESSAGE = "The overlap between chunks must be zero or greater"
+OVERLAP_NEGATIVE_MESSAGE = "The overlap between segments must be zero or " \
+                           "greater"
 NON_POSITIVE_SEGMENT_MESSAGE = "the segment size should be positive"
 NEG_OVERLAP_LAST_PROP_MESSAGE = "the overlap or last segment proportion" \
                                 "should not be negative"
-LARGER_CHUNK_SIZE_MESSAGE = "the segment size should be larger " \
+LARGER_SEG_SIZE_MESSAGE = "the segment size should be larger " \
     "than the overlap size"
 NON_NEGATIVE_INDEX_MESSAGE = "the index should be larger than or equal to zero"
 MATRIX_DIMENSION_UNEQUAL_MESSAGE = "the dimension of the matrix " \
