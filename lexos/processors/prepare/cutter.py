d8c335d06e88ae8e84397bff677aadbc4e216530
Shi26 <darias0826@gmail.com>
2017-08-14 14:58:40 -0400

    fix last_prop calculation bug fix last_prop handling minor renaming

--- lexos/processors/prepare/cutter.py
+++ lexos/processors/prepare/cutter.py
@@ -7,39 +7,47 @@ from lexos.helpers.error_messages import NON_POSITIVE_SEGMENT_MESSAGE, \
     EMPTY_MILESTONE_MESSAGE, INVALID_CUTTING_TYPE_MESSAGE
 
 
-def cut_list_with_overlap(input_list: list, seg_size: int, overlap: int,
+def cut_list_with_overlap(input_list: list, norm_seg_size: int, overlap: int,
                           last_prop: float) -> List[list]:
     """Helper to get all the segments of input_list.
 
+
     :param last_prop: the last segment size / other segment size.
     :param input_list: the segment list that split the contents of the file.
-    :param seg_size: the size of the segment.
+    :param norm_seg_size: the size of the segment.
     :param overlap: the min proportional size that the last segment has to be.
     :return a list of list(segment) that the text has been cut into, which has
     not go through the last proportion size calculation.
     """
 
-    start_point_distance = seg_size - overlap
+    start_point_distance = norm_seg_size - overlap
 
     input_list_length = len(input_list)
     num_stop_point = \
-        (input_list_length - seg_size*last_prop) / start_point_distance
+        (input_list_length - norm_seg_size * last_prop) / start_point_distance
 
     if num_stop_point > 0:
         num_segment = int(math.ceil(num_stop_point) + 1)
     else:
         num_segment = 1
 
-    def get_single_seg(index):
+    def get_single_seg(index: int, is_last_prop: bool) -> List[list]:
         """Helper to get one single segment with index: index.
 
+        :param is_last_prop:
         :param index: the index of the segment in the final segment list.
         :return single segment in the input_list based on index.
         """
-        return input_list[start_point_distance * index:
-                          start_point_distance * index + seg_size]
+        if is_last_prop:
+            cur_seg_size = int(norm_seg_size * last_prop)
+        else:
+            cur_seg_size = norm_seg_size
 
-    return [get_single_seg(index) for index in range(num_segment)]
+        return input_list[start_point_distance * index:
+                          start_point_distance * index + cur_seg_size]
+
+    return [get_single_seg(index, True if index == num_segment - 1 else False)
+            for index in range(num_segment)]
 
 
 def join_sublist_element(input_list: list) -> List[str]:
@@ -77,7 +85,7 @@ def cut_by_characters(text: str, seg_size: int, overlap: int,
 
     # add sub-lists(segment) to final list
     final_seg_list = cut_list_with_overlap(input_list=seg_list,
-                                           seg_size=seg_size,
+                                           norm_seg_size=seg_size,
                                            overlap=overlap,
                                            last_prop=last_prop)
 
@@ -111,7 +119,7 @@ def cut_by_words(text: str, seg_size: int, overlap: int, last_prop: float) \
 
     # add sub-lists(segment) to final list
     final_seg_list = cut_list_with_overlap(input_list=seg_list,
-                                           seg_size=seg_size,
+                                           norm_seg_size=seg_size,
                                            overlap=overlap,
                                            last_prop=last_prop)
 
@@ -145,7 +153,7 @@ def cut_by_lines(text: str, seg_size: int, overlap: int, last_prop: float) \
 
     # add sub-lists(segment) to final list
     final_seg_list = cut_list_with_overlap(input_list=seg_list,
-                                           seg_size=seg_size,
+                                           norm_seg_size=seg_size,
                                            overlap=overlap,
                                            last_prop=last_prop)
 
@@ -177,7 +185,8 @@ def cut_by_number(text: str, num_segment: int) -> List[str]:
     # add sub-lists(chunks) to final list
     if seg_size != 0:
         final_seg_list = cut_list_with_overlap(
-            input_list=seg_list, seg_size=int_seg_size, overlap=0, last_prop=0)
+            input_list=seg_list, norm_seg_size=int_seg_size, overlap=0,
+            last_prop=0)
     else:
         final_seg_list = seg_list
 
@@ -209,7 +218,7 @@ def cut_by_milestone(text: str, milestone: str) -> List[str]:
 
 
 def cut(text: str, cutting_value: str, cutting_type: str, overlap: str,
-        last_prop: str) -> List[str]:
+        last_prop_percent: str) -> List[str]:
     """Cuts each text string into various segments.
 
     Cutting according to the options chosen by the user.
@@ -218,7 +227,7 @@ def cut(text: str, cutting_value: str, cutting_type: str, overlap: str,
     :param cutting_type: A string representing which cutting method to use.
     :param overlap: A unicode string representing the number of words to be
            overlapped between each text segment.
-    :param last_prop: A unicode string representing the minimum proportion
+    :param last_prop_percent: A unicode string representing the minimum proportion
            percentage the last segment has to be to not get assimilated by
            the previous.
     :return A list of strings, each representing a segment of the original.
@@ -232,7 +241,7 @@ def cut(text: str, cutting_value: str, cutting_type: str, overlap: str,
     # standardize parameters
     cutting_type = str(cutting_type)
     overlap = int(overlap)
-    last_prop = float(last_prop.strip('%')) / 100
+    last_prop_percent = float(last_prop_percent.rstrip('%')) / 100
 
     # distribute cutting method by input cutting value
     if cutting_type != 'milestone':
@@ -240,13 +249,16 @@ def cut(text: str, cutting_value: str, cutting_type: str, overlap: str,
 
     if cutting_type == 'letters':
         string_list = cut_by_characters(text=text, seg_size=cutting_value,
-                                        overlap=overlap, last_prop=last_prop)
+                                        overlap=overlap,
+                                        last_prop=last_prop_percent)
     elif cutting_type == 'words':
         string_list = cut_by_words(text=text, seg_size=cutting_value,
-                                   overlap=overlap, last_prop=last_prop)
+                                   overlap=overlap,
+                                   last_prop=last_prop_percent)
     elif cutting_type == 'lines':
         string_list = cut_by_lines(text=text, seg_size=cutting_value,
-                                   overlap=overlap, last_prop=last_prop)
+                                   overlap=overlap,
+                                   last_prop=last_prop_percent)
     elif cutting_type == 'milestone':
         string_list = cut_by_milestone(text=text, milestone=cutting_value)
     elif cutting_type == 'number':
