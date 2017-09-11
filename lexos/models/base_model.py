45b57e0387dd3a9c06e4233969381f5346581c61
chantisnake <13501393281@163.com>
2017-09-11 09:29:56 -0400

    add a new function to base model to check for whether a data is in request

--- lexos/models/base_model.py
+++ lexos/models/base_model.py
@@ -17,3 +17,14 @@ class BaseModel:
     @property
     def front_end_data(self) -> dict:
         return self._option
+
+    def check_data_exists(self, data_key: str) -> bool:
+        """check whether the data key exists in the data sent from front end
+
+        :param data_key: the key of the data
+        :return: whether that data key is in the request parameter.
+        """
+        if not self._option:
+            raise KeyError(NO_DATA_SENT_ERROR_MESSAGE)
+
+        return data_key in self._option
