cf9aa79637e21fdf79382c0f19881f0d7681f072
Weiqi97 <weltch1997@gmail.com>
2017-09-12 00:06:40 -0400

    Add more unit test

--- test/unit_test/test_utility_kmeans.py
+++ test/unit_test/test_utility_kmeans.py
@@ -3,6 +3,7 @@ import numpy as np
 from lexos.helpers import constants
 from lexos.processors.analyze import KMeans
 
+# Set up the global testing values
 init_method = "k-means++"
 n_init = constants.N_INIT
 max_iter = constants.MAX_ITER
@@ -14,16 +15,6 @@ count_matrix = np.array([(1, 0, 0, 0, 0, 200, 0, 0),
                          (0, 0, 1, 0, 0, 0, 0, 0),
                          (0, 0, 0, 100, 0, 0, 0, 0)])
 
-k_means_pca_data = KMeans.GetKMeansPca(count_matrix=count_matrix,
-                                       labels=labels,
-                                       n_init=n_init,
-                                       k_value=int(np.size(labels) / 2),
-                                       max_iter=max_iter,
-                                       tolerance=tolerance,
-                                       init_method=init_method,
-                                       folder_path="",
-                                       metric_dist="precomputed")
-
 new_labels = np.array(["F1", "F2", "F3", "F4", "F5", "F6"])
 new_count_matrix = np.array([(1, 10, 100, 0, 0, 0, 0, 0),
                              (2, 20, 200, 0, 0, 0, 0, 0),
@@ -32,7 +23,63 @@ new_count_matrix = np.array([(1, 10, 100, 0, 0, 0, 0, 0),
                              (0, 0, 0, 0, 1000, 1000, 0, 0),
                              (0, 0, 0, 0, 0, 0, 1, 1)])
 
+# Set up the pca testing values
+k_means_pca_data = KMeans.GetKMeansPca(
+    labels=labels,
+    n_init=n_init,
+    folder_path="",
+    max_iter=max_iter,
+    tolerance=tolerance,
+    init_method=init_method,
+    metric_dist="euclidean",
+    count_matrix=count_matrix,
+    k_value=int(np.size(labels) / 2))
+
 new_k_means_pca_data = KMeans.GetKMeansPca(
+    labels=new_labels,
+    n_init=n_init,
+    folder_path="",
+    max_iter=max_iter,
+    tolerance=tolerance,
+    init_method=init_method,
+    metric_dist="euclidean",
+    count_matrix=new_count_matrix,
+    k_value=int(np.size(new_labels) / 2))
+
+
+class TestPCA:
+    def test_index(self):
+        assert set(k_means_pca_data.best_index) == {0, 1}
+        assert set(new_k_means_pca_data.best_index) == {0, 1, 2}
+
+    def test_color(self):
+        assert k_means_pca_data.color_chart == \
+            'rgb(27, 158, 119, 255)#rgb(102, 102, 102, 255)#'
+        assert new_k_means_pca_data.color_chart == \
+            'rgb(27, 158, 119, 255)#rgb(102, 166, 30, 255)' \
+            '#rgb(102, 102, 102, 255)#'
+
+    def test_name(self):
+        assert k_means_pca_data.file_name_str == 'F1#F2#F3#F4'
+        assert new_k_means_pca_data.file_name_str == 'F1#F2#F3#F4#F5#F6'
+
+    def test_score(self):
+        assert k_means_pca_data.silhouette_score == \
+            'N/A [Not available for K ≤ 2]'
+        assert new_k_means_pca_data.silhouette_score == 0.283
+
+
+k_means_vor_data = KMeans.GetKMeansVoronoi(
+    count_matrix=count_matrix,
+    labels=labels,
+    n_init=n_init,
+    k_value=int(np.size(labels) / 2),
+    max_iter=max_iter,
+    tolerance=tolerance,
+    init_method=init_method,
+    metric_dist="euclidean")
+
+new_k_means_vor_data = KMeans.GetKMeansVoronoi(
     count_matrix=new_count_matrix,
     labels=new_labels,
     n_init=n_init,
@@ -40,26 +87,31 @@ new_k_means_pca_data = KMeans.GetKMeansPca(
     max_iter=max_iter,
     tolerance=tolerance,
     init_method=init_method,
-    folder_path="",
     metric_dist="euclidean")
-
-INDEX = set(k_means_pca_data.best_index)
 
 print("DONE")
 
 
-class TestPCA:
-    def test_normal_index(self):
-        assert set(k_means_pca_data.best_index) == {0, 1}
-        assert set(new_k_means_pca_data.best_index) == {0, 1, 2}
+class TestVor:
+    def test_index(self):
+        assert set(k_means_vor_data.best_index) == {0, 1}
+        assert set(new_k_means_vor_data.best_index) == {0, 1, 2}
 
-    def test_normal_color(self):
-        assert k_means_pca_data.color_chart == \
-            'rgb(27, 158, 119, 255)#rgb(102, 102, 102, 255)#'
-        assert new_k_means_pca_data.color_chart == \
-            'rgb(27, 158, 119, 255)#rgb(102, 166, 30, 255)' \
-            '#rgb(102, 102, 102, 255)#'
+    def test_color(self):
+        assert k_means_vor_data.color_chart == \
+            'rgb(27, 158, 119)#rgb(102, 102, 102)#'
+        assert new_k_means_vor_data.color_chart == \
+            'rgb(27, 158, 119)#rgb(102, 166, 30)#rgb(102, 102, 102)#'
 
-    def test_normal_name(self):
-        assert k_means_pca_data.file_name_str == 'F1#F2#F3#F4'
-        assert new_k_means_pca_data.file_name_str == 'F1#F2#F3#F4#F5#F6'
+    def test_labels(self):
+        assert k_means_vor_data.labels_str == 'F1#F2#F3#F4'
+        assert new_k_means_vor_data.labels_str == 'F1#F2#F3#F4#F5#F6'
+
+    def test_max(self):
+        assert round(k_means_vor_data.max_x, 3) == 311.416
+        assert round(new_k_means_vor_data.max_x, 3) == 1520.832
+
+    def test_score(self):
+        assert k_means_vor_data.silhouette_score == \
+            'N/A [Not available for K ≤ 2]'
+        assert new_k_means_vor_data.silhouette_score == 0.283
