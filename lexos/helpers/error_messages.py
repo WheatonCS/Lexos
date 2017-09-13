ae50d95fae6c26bdacd00642ab2c485c4be30a7d
Weiqi97 <weltch1997@gmail.com>
2017-09-12 21:19:04 -0400

    Make structure changes

--- lexos/helpers/error_messages.py
+++ lexos/helpers/error_messages.py
@@ -23,4 +23,3 @@ EMPTY_NP_ARRAY_MESSAGE = "The input numpy array should not be empty, since" \
     "the input file should not be empty."
 EMPTY_STRING_MESSAGE = "the string should not be empty"
 WINDOW_NON_POSITIVE_MESSAGE = "The window size must be a positive integer"
-EMPTY_FOLDER_PATH = "The given folder path should not be empty"
