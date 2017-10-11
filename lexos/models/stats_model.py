253f83e3576cfa2f3289b1e9f3f38e9b23d2bf5c
Weiqi97 <weltch1997@gmail.com>
2017-10-10 21:33:21 -0400

    Finish model part

--- lexos/models/stats_model.py
+++ lexos/models/stats_model.py
@@ -1,21 +1,20 @@
-from typing import Optional
+from typing import Optional, List
 
 import numpy as np
 import pandas as pd
+
+from lexos.helpers.error_messages import EMPTY_LIST_MESSAGE
 from lexos.models.base_model import BaseModel
 from lexos.models.matrix_model import MatrixModel
 
 
 class CorpusInfo:
-    """This is a structure that holds all corpus information"""
+    """This is a structure that holds corpus information"""
     def __init__(self, q1: float, q3: float, iqr: float, median: float,
                  average: float, num_file: int, file_sizes: int,
                  file_names: np.ndarray, std_deviation: float,
                  anomaly_iqr: dict, anomaly_std_err: dict):
-        self._q1 = q1
-        self._q3 = q3
-        self._iqr = iqr
-        self._median = median
+
         self._average = average
         self._num_file = num_file
         self._file_sizes = file_sizes
@@ -26,6 +25,21 @@ class CorpusInfo:
 
 
 class FileInfo:
+    """This is a structure that holds each file information"""
+    def __init__(self, q1: float, q3: float, iqr: float, hapax: int,
+                 median: float, average: float, num_word: int, file_name: str,
+                 word_count: int, std_deviation: float, total_word_count:int):
+        self._q1 = q1
+        self._q3 = q3
+        self._iqr = iqr
+        self._hapax = hapax
+        self._median = median
+        self._average = average
+        self._num_word = num_word
+        self._file_name = file_name
+        self._word_count = word_count
+        self._std_deviation = std_deviation
+        self._total_word_count = total_word_count
 
 
 class StatsModel(BaseModel):
@@ -44,7 +58,7 @@ class StatsModel(BaseModel):
         return self._test_dtm if self._test_dtm is not None \
             else MatrixModel().get_matrix()
 
-    def _get_corpus_info(self):
+    def _get_corpus_info(self) -> CorpusInfo:
         """Converts word lists completely to statistic."""
         # initialize
         num_file = np.size(self._doc_term_matrix.index.values)
@@ -86,6 +100,50 @@ class StatsModel(BaseModel):
                           anomaly_iqr=file_anomaly_iqr,
                           anomaly_std_err=file_anomaly_std_err)
 
+    @staticmethod
+    def _get_file_info(count_list: np.ndarray, file_name: str) -> FileInfo:
+        """Gives statistics of a particular file in a given file list
 
+        :param count_list: a list contains words count of a particular file
+        :param file_name: the file name of that file
+        """
+        assert count_list.size > 0, EMPTY_LIST_MESSAGE
 
+        # initialize remove all zeros from count_list
+        nonzero_count_list = count_list[count_list != 0]
+        num_word = np.size(nonzero_count_list)
+        total_word_count = sum(nonzero_count_list).item()
+        # 1 standard error analysis
+        average_word_count = round(total_word_count / num_word, 3)
+        # calculate the standard deviation
+        std_word_count = np.std(nonzero_count_list).item()
 
+        # 2 iqr analysis
+        mid = np.median(nonzero_count_list).item()
+        q1 = np.percentile(nonzero_count_list, 25, interpolation="midpoint")
+        q3 = np.percentile(nonzero_count_list, 75, interpolation="midpoint")
+        iqr = q3 - q1
+        hapax = ((count_list == 1).sum()).item()
+
+        return FileInfo(q1=q1,
+                        q3=q3,
+                        iqr=iqr,
+                        hapax=hapax,
+                        median=mid,
+                        average=average_word_count,
+                        num_word=num_word,
+                        file_name=file_name,
+                        word_count=nonzero_count_list,
+                        std_deviation=std_word_count,
+                        total_word_count=total_word_count)
+
+    def _get_all_file_result(self) -> List[FileInfo]:
+        file_info_list = \
+            [self._get_file_info(
+                count_list=self._doc_term_matrix.values[index, :],
+                file_name=label)
+             for index, label in enumerate(self._doc_term_matrix.index.values)]
+        return file_info_list
+
+    def get_result(self):
+        return self._get_all_file_result(), self._get_corpus_info()
