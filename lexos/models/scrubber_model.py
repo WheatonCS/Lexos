ebff5ee68044a823baa0796ad149a04b33616ed5
emma_grace <steffens_emma@wheatoncollege.edu>
2017-10-12 13:11:25 -0400

    try template file again

---
+++ lexos/models/scrubber_model.py
@@ -0,0 +1,19 @@
+
+class BasicOptions:
+    def __init__(self):
+        pass
+
+
+class AdditionalOptions:
+    def __init__(self):
+        pass
+
+
+class ScrubbingOptions:
+    def __init__(self):
+        pass
+
+
+class ScrubberModel:
+    def __init__(self):
+        pass
