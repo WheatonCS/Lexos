f4da8ef2c16380392c4e6444c25bb135ca449136
alvaro <delandaluce_alvaro@wheatoncollege.edu>
2017-11-01 15:37:31 -0400

    make members private

--- lexos/models/content_analysis_model.py
+++ lexos/models/content_analysis_model.py
@@ -16,12 +16,12 @@ class ContentAnalysisModel(object):
         scores: List of formula/total word count of each file
         averages: Lis of averages count of each dictionary
         """
-        self.dictionaries = []
-        self.corpus = []
-        self.counters = []
-        self.formulas = []
-        self.scores = []
-        self.averages = []
+        self._dictionaries = []
+        self._corpus = []
+        self._counters = []
+        self._formulas = []
+        self._scores = []
+        self._averages = []
 
     def add_corpus(self, file_name: str, label: str, content: str):
         """Adds a file to the corpus
@@ -31,10 +31,10 @@ class ContentAnalysisModel(object):
         :param label: file label
         """
         total_word_counts = len(str(content).split(" "))
-        self.corpus.append(File(content=content,
-                                file_name=file_name,
-                                label=label,
-                                total_word_counts=total_word_counts))
+        self._corpus.append(File(content=content,
+                                 file_name=file_name,
+                                 label=label,
+                                 total_word_counts=total_word_counts))
 
     def add_dictionary(self, file_name: str, content: str):
         """Adds a dictionary
@@ -46,18 +46,18 @@ class ContentAnalysisModel(object):
         new_list.sort(key=lambda x: len(x.split()), reverse=True)
         import os
         label = os.path.splitext(file_name)[0]
-        self.dictionaries.append(Dictionary(content=new_list,
-                                            file_name=file_name,
-                                            label=label))
+        self._dictionaries.append(Dictionary(content=new_list,
+                                             file_name=file_name,
+                                             label=label))
 
     def delete_dictionary(self, label: str):
         """deletes a dictionary
 
         :param label: label of dictionary to delete
         """
-        for i in range(len(self.dictionaries)):
-            if self.dictionaries[i].label == label:
-                del self.dictionaries[i]
+        for i in range(len(self._dictionaries)):
+            if self._dictionaries[i].label == label:
+                del self._dictionaries[i]
                 break
 
     def toggle_dictionary(self, label: str):
@@ -65,7 +65,7 @@ class ContentAnalysisModel(object):
 
         :param label: filename of dictionary to toggle
         """
-        for dictionary in self.dictionaries:
+        for dictionary in self._dictionaries:
             if dictionary.label == label:
                 dictionary.active = not dictionary.active
 
@@ -75,14 +75,14 @@ class ContentAnalysisModel(object):
         :return: a list containing all active dictionaries
         """
         active_dicts = []
-        for dictionary in self.dictionaries:
+        for dictionary in self._dictionaries:
             if dictionary.active:
                 active_dicts.append(dictionary)
         return active_dicts
 
     def detect_active_dicts(self) -> int:
         num_active_dicts = 0
-        for dictionary in self.dictionaries:
+        for dictionary in self._dictionaries:
             if dictionary.active:
                 num_active_dicts += 1
         return num_active_dicts
@@ -92,13 +92,13 @@ class ContentAnalysisModel(object):
 
         """
         # delete previous results
-        self.counters = []
-        for file in self.corpus:
+        self._counters = []
+        for file in self._corpus:
             counts = []
-            for i in range(len(self.dictionaries)):
-                if self.dictionaries[i].active:
+            for i in range(len(self._dictionaries)):
+                if self._dictionaries[i].active:
                     count = 0
-                    for word in self.dictionaries[i].content:
+                    for word in self._dictionaries[i].content:
                         if file.content.startswith(word + " "):
                             count += 1
                         if file.content.endswith(" " + word + "\n") or \
@@ -110,7 +110,7 @@ class ContentAnalysisModel(object):
                         if ' ' in word:
                             file.content = file.content.replace(word, " ")
                     counts.append(count)
-            self.counters.append(counts)
+            self._counters.append(counts)
 
     def generate_scores(self, formula: str):
         """calculate the formula and scores=formula/total_word_count for each
@@ -119,31 +119,31 @@ class ContentAnalysisModel(object):
         :param formula: a string containing a mathematical equation with
         dictionary names between brackets
         """
-        self.scores = []
-        self.formulas = []
+        self._scores = []
+        self._formulas = []
         active_dicts = self.get_active_dicts()
         result = 0
-        for i in range(len(self.corpus)):
+        for i in range(len(self._corpus)):
             new_formula = formula
             for j in range(len(active_dicts)):
                 new_formula = new_formula.replace(
                     "[" + active_dicts[j].label + "]",
-                    str(self.counters[i][j]))
+                    str(self._counters[i][j]))
             new_formula = new_formula.replace("()", "")
             try:
                 result = eval(new_formula)
             except (ValueError, SyntaxError):
                 pass
-            self.scores.append(round(
-                float(result) / self.corpus[i].total_word_counts, 3))
-            self.formulas.append(result)
+            self._scores.append(round(
+                float(result) / self._corpus[i].total_word_counts, 3))
+            self._formulas.append(result)
 
     def generate_averages(self):
         """Calculates the averages of eachm dictionary count, formula,
         total_word_count, and score
 
         """
-        self.averages = []
+        self._averages = []
         scores_sum = 0
         total_word_counts_sum = 0
         formulas_sum = 0
@@ -151,36 +151,36 @@ class ContentAnalysisModel(object):
         for i in range(len(self.scores)):
             scores_sum += self.scores[i]
             total_word_counts_sum += \
-                self.corpus[i].total_word_counts
-            formulas_sum += self.formulas[i]
+                self._corpus[i].total_word_counts
+            formulas_sum += self._formulas[i]
         if len(self.scores) != 0:
             scores_avg = round(
                 (float(scores_sum) / len(self.scores)), 3)
         else:
             scores_avg = 0
-        if len(self.corpus) != 0:
+        if len(self._corpus) != 0:
             total_word_counts_avg = round((float(total_word_counts_sum) /
-                                           (len(self.corpus))), 1)
+                                           (len(self._corpus))), 1)
         else:
             total_word_counts_avg = 0
-        if len(self.formulas) != 0:
-            sums_avg = round((float(formulas_sum) / len(self.formulas)), 1)
+        if len(self._formulas) != 0:
+            sums_avg = round((float(formulas_sum) / len(self._formulas)), 1)
         else:
             sums_avg = 0
         cat_count = 0
-        self.averages.append("Averages")
+        self._averages.append("Averages")
         for x in range(len(active_dicts)):
-            for i in range(len(self.counters)):
-                cat_count += self.counters[i][x]
-            if len(self.counters) != 0:
-                self.averages.append(round(
-                    float(cat_count) / len(self.counters), 1))
+            for i in range(len(self._counters)):
+                cat_count += self._counters[i][x]
+            if len(self._counters) != 0:
+                self._averages.append(round(
+                    float(cat_count) / len(self._counters), 1))
             else:
-                self.averages.append(0)
+                self._averages.append(0)
             cat_count = 0
-        self.averages.append(sums_avg)
-        self.averages.append(total_word_counts_avg)
-        self.averages.append(scores_avg)
+        self._averages.append(sums_avg)
+        self._averages.append(total_word_counts_avg)
+        self._averages.append(scores_avg)
 
     def to_html(self) -> str:
         df = self.to_data_frame()
@@ -200,20 +200,20 @@ class ContentAnalysisModel(object):
             columns.append(dictionary.label)
         columns += ['formula', 'total word count', 'score']
         indices = []
-        for file in self.corpus:
+        for file in self._corpus:
             indices.append(file.label)
         indices += ['averages']
         df = pd.DataFrame(columns=range(len(columns)),
                           index=range(len(indices)))
-        for i in range(len(self.corpus)):
-            df.xs(i)[0] = self.corpus[i].label
-            for j in range(len(self.counters[i])):
-                df.xs(i)[j + 1] = self.counters[i][j]
-            df.xs(i)[j + 2] = self.formulas[i]
-            df.xs(i)[j + 3] = self.corpus[i].total_word_counts
-            df.xs(i)[j + 4] = self.scores[i]
-        for j in range(len(self.averages)):
-            df.xs(i + 1)[j] = self.averages[j]
+        for i in range(len(self._corpus)):
+            df.xs(i)[0] = self._corpus[i].label
+            for j in range(len(self._counters[i])):
+                df.xs(i)[j + 1] = self._counters[i][j]
+            df.xs(i)[j + 2] = self._formulas[i]
+            df.xs(i)[j + 3] = self._corpus[i].total_word_counts
+            df.xs(i)[j + 4] = self._scores[i]
+        for j in range(len(self._averages)):
+            df.xs(i + 1)[j] = self._averages[j]
         df.columns = columns
         return df
 
@@ -230,6 +230,30 @@ class ContentAnalysisModel(object):
         if len(formula) == 0:
             return True
         return False
+
+    @property
+    def dictionaries(self) -> list:
+        return self._dictionaries
+
+    @dictionaries.setter
+    def dictionaries(self, dictionaries: list):
+        self._dictionaries = dictionaries
+
+    @property
+    def corpus(self) -> list:
+        return self._corpus
+
+    @property
+    def counters(self) -> list:
+        return self._counters
+
+    @property
+    def scores(self) -> list:
+        return self._scores
+
+    @property
+    def averages(self) -> list:
+        return self._averages
 
 
 class Document(object):
