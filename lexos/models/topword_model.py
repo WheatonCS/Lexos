b104e3866c3fb35a4d942d759ad240e7551d75ee
Weiqi97 <weltch1997@gmail.com>
2017-11-06 00:08:01 -0500

    Add static method for z_test

--- lexos/models/topword_model.py
+++ lexos/models/topword_model.py
@@ -41,3 +41,32 @@ class TopwordModel(BaseModel):
         return self._test_option if self._test_option is not None \
             else TopwordReceiver().options_from_front_end()
 
+    @staticmethod
+    def _z_test_(p1, pt, n1, nt):
+        """Examines if a particular word is an anomaly.
+
+        while examining, this function compares the probability of a word's
+        occurrence in one particular segment to the probability of the same
+        word's occurrence in the rest of the segments. Usually we report a
+        word as an anomaly if the return value is smaller than -1.96 or
+        bigger than 1.96.
+        :param p1: the probability of a word's occurrence in a particular
+                   segment: Number of word occurrence in the
+                   segment/total word count in the segment
+        :param pt: the probability of a word's occurrence in all the segments
+                   (or the whole passage)
+                   Number of word occurrence in all the segment/
+                   total word count in all the segment
+        :param n1: the number of total words in the segment we care about.
+        :param nt: the number of total words in all the segment selected.
+        :return: the probability that the particular word in a particular
+                 segment is NOT an anomaly.
+        """
+        assert n1 > 0, SEG_NON_POSITIVE_MESSAGE
+        assert nt > 0, SEG_NON_POSITIVE_MESSAGE
+        p = (p1 * n1 + pt * nt) / (n1 + nt)
+        standard_error = (p * (1 - p) * ((1 / n1) + (1 / nt))) ** 0.5
+        if np.isclose([standard_error], [0]):
+            return 0
+        else:
+            return round((p1 - pt) / standard_error, 4)
