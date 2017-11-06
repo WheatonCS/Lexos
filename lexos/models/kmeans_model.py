dd78bc0488df057ac9a8ce1f5e4402ce56e2c013
Weiqi97 <weltch1997@gmail.com>
2017-11-06 01:11:25 -0500

    complete model

--- lexos/models/kmeans_model.py
+++ lexos/models/kmeans_model.py
@@ -4,7 +4,7 @@
 # for more details
 
 from os.path import join as path_join
-from typing import Union, Optional, NamedTuple
+from typing import Union, Optional, NamedTuple, Iterable
 
 import matplotlib.pyplot as plt
 import numpy as np
@@ -33,10 +33,22 @@ class KmeansPCAResult(NamedTuple):
     folder_path: str
     reduced_data: list
     colored_points: list
-    silhouette_score: Union[str, float]):
+    silhouette_score: Union[str, float]
 
 
-class KmeansPCAModel(BaseModel):
+class KmeansVORResult(NamedTuple):
+    max_x: int
+    k_value: int
+    text_data: list
+    best_index: list
+    labels_str: str
+    color_chart: str
+    silhouette_score: Union[str, float]
+    final_points_list: Iterable
+    final_centroids_list: list
+
+
+class KmeansModel(BaseModel):
     def __init__(self, test_dtm: Optional[pd.DataFrame] = None,
                  test_option: Optional[KmeansOption] = None):
         """This is the class to generate kmeans.
@@ -192,7 +204,7 @@ class KmeansPCAModel(BaseModel):
         html_file.write(lg_html)
         html_file.close()
 
-    def get_result(self) -> KmeansPCAResult:
+    def get_pca_result(self) -> KmeansPCAResult:
         self._get_pca_data()
         self._draw_graph()
         return KmeansPCAResult(labels=self._doc_term_matrix.index.values,
@@ -204,73 +216,49 @@ class KmeansPCAModel(BaseModel):
                                colored_points=self.colored_points,
                                silhouette_score=self.silhouette_score)
 
+    @staticmethod
+    def _get_voronoi_plot_data_(data: np.ndarray,
+                                group_index: np.ndarray) -> np.ndarray:
+        """Generates the data needed to be plotted in voronoi analysis method.
 
-
-
-
-def _get_voronoi_plot_data_(data: np.ndarray,
-                            group_index: np.ndarray) -> np.ndarray:
-    """Generates the data needed to be plotted in voronoi analysis method.
-
-    :param data: the reduced data analyzed by the k-means method.
-    :param group_index: index for the results that are in the same group.
-    :return: the centroid analysis data.
-    """
-    temp_data = [data[item] for _, item in enumerate(group_index)]
-    result = np.average(temp_data[0].transpose(), axis=1)
-    return result
-
-
-class GetKMeansVoronoi:
-    def __init__(self,
-                 count_matrix: np.ndarray,
-                 n_init: int,
-                 k_value: int,
-                 max_iter: int,
-                 tolerance: float,
-                 init_method: str,
-                 metric_dist: str,
-                 labels: np.ndarray):
-        """Generates an array of centroid index based on the active files.
-
-        This function also finds a list of points for the centroids, and a list
-        of points for the segments.
-        :param count_matrix: a 2D numpy matrix contains the word counts.
-        :param n_init: number of iterations with different centroids.
-        :param k_value: k value-for k-means analysis.
-        :param max_iter: maximum number of iterations.
-        :param tolerance: relative tolerance, inertia to declare convergence.
-        :param init_method: method of initialization: "K++" or "random".
-        :param metric_dist: method of the distance metrics.
-        :param labels: file names of active files.
+        :param data: the reduced data analyzed by the k-means method.
+        :param group_index: index for the results that are in the same group.
+        :return: the centroid analysis data.
         """
-        assert np.size(count_matrix) > 0, EMPTY_NP_ARRAY_MESSAGE
-        assert np.size(labels) > 0, EMPTY_NP_ARRAY_MESSAGE
+        temp_data = [data[item] for _, item in enumerate(group_index)]
+        result = np.average(temp_data[0].transpose(), axis=1)
+        return result
 
-        # finds xy coordinates for each segment
-        reduced_data = PCA(n_components=2).fit_transform(count_matrix)
+    def _get_voronoi_data(self) -> KmeansVORResult:
+        """Generates an array of centroid index based on the active files."""
 
-        # TODO: n_init probably should be determined based on number of files
-        # performs the kmeans analysis
-        k_means = KMeans(tol=tolerance,
-                         n_init=n_init,
-                         init=init_method,
-                         max_iter=max_iter,
-                         n_clusters=k_value)
-        kmeans_index = k_means.fit_predict(reduced_data)
-        best_index = kmeans_index.tolist()
+        # Test if get empty input
+        assert np.size(self._doc_term_matrix.values) > 0, \
+            EMPTY_NP_ARRAY_MESSAGE
+
+        # Get Kmeans
+        self.reduced_data = \
+            PCA(n_components=2).fit_transform(self._doc_term_matrix.values)
+        self.k_means = KMeans(tol=self._kmeans_option.tolerance,
+                              n_init=self._kmeans_option.n_init,
+                              init=self._kmeans_option.init_method,
+                              max_iter=self._kmeans_option.max_iter,
+                              n_clusters=self._kmeans_option.k_value)
+        kmeans_index = self.k_means.fit_predict(self.reduced_data)
+        self.best_index = kmeans_index.tolist()
 
         # calculates the centroid points list
-        centroid_coordinates = [_get_voronoi_plot_data_(
-            data=reduced_data, group_index=np.where(kmeans_index == index))
+        centroid_coordinates = [self._get_voronoi_plot_data_(
+            data=self.reduced_data,
+            group_index=np.where(kmeans_index == index))
             for index in np.unique(kmeans_index)]
 
         # generates values to translate x, y coordinates
-        translate_x = abs(min(reduced_data[:, 0])) + 100
-        translate_y = abs(min(reduced_data[:, 1])) + 100
+        translate_x = abs(min(self.reduced_data[:, 0])) + 100
+        translate_y = abs(min(self.reduced_data[:, 1])) + 100
 
         # create final points coordinates list to plot
-        final_points_list = np.copy(reduced_data)
+        final_points_list = np.copy(self.reduced_data)
         final_points_list[:, 0] += translate_x
         final_points_list[:, 1] += translate_y
         max_x = max(final_points_list[:, 0])
@@ -284,39 +272,42 @@ class GetKMeansVoronoi:
         final_centroids_list.insert(0, [-500, -500])
 
         # create text data
-        text_data = [{"x": item[0], "y": item[1], "title": labels[index]}
+        text_data = [{"x": item[0], "y": item[1],
+                      "title": self._doc_term_matrix.index.values[index]}
                      for index, item in enumerate(final_points_list)]
 
         # create a color gradient with k colors
-        color_list = plt.cm.Dark2(np.linspace(0, 1, k_value))
+        color_list = \
+            plt.cm.Dark2(np.linspace(0, 1, self._kmeans_option.k_value))
 
         # create list of rgb tuples to let the website to plot
         rgb_tuples = [tuple([int(rgb_value * 255) for rgb_value in color[:-1]])
                       for color in color_list]
 
         # puts rgb values in right order to match the table
-        rgb_index = sorted(set(best_index), key=lambda x: best_index.index(x))
-        ordered_color_list1 = [None] * k_value
+        rgb_index = sorted(set(self.best_index),
+                           key=lambda x: self.best_index.index(x))
+        ordered_color_list1 = [None] * self._kmeans_option.k_value
         for index, item in enumerate(rgb_index):
             ordered_color_list1[item] = rgb_tuples[index]
         color_chart = "rgb" + "#rgb".join(map(str, rgb_tuples)) + "#"
 
         # create a file label string
-        labels_str = "#".join(labels)
+        labels_str = "#".join(self._doc_term_matrix.index.values)
 
         # calculate silhouette score
-        silhouette_score = _get_silhouette_score_(k=k_value,
-                                                  k_means=k_means,
-                                                  matrix=count_matrix,
-                                                  metric_dist=metric_dist)
+        self.silhouette_score = self._get_silhouette_score()
 
-        # pack all the data
-        self.max_x = max_x
-        self.k_value = k_value
-        self.text_data = text_data
-        self.best_index = best_index
-        self.labels_str = labels_str
-        self.color_chart = color_chart
-        self.silhouette_score = silhouette_score
-        self.final_points_list = final_points_list
-        self.final_centroids_list = final_centroids_list
+        return KmeansVORResult(max_x=max_x,
+                               k_value=self._kmeans_option.k_value,
+                               text_data=text_data,
+                               best_index=self.best_index,
+                               labels_str=labels_str,
+                               color_chart=color_chart,
+                               silhouette_score=self.silhouette_score,
+                               final_points_list=final_points_list,
+                               final_centroids_list=final_centroids_list)
+
+    def get_vor_result(self) -> KmeansVORResult:
+
+        return self._get_voronoi_data()
