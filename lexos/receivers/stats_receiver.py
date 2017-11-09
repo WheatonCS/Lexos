d49ad227722b0987f978c35a8f66e8fe5bc5fe5f
Weiqi97 <weltch1997@gmail.com>
2017-11-08 23:08:57 -0500

    Complete receiver

--- lexos/receivers/stats_receiver.py
+++ lexos/receivers/stats_receiver.py
@@ -5,3 +5,7 @@ class StatsReceiver(BaseReceiver):
     def __init__(self):
         """So far there is no frontend option for statistics analysis"""
         super().__init__()
+
+    def options_from_front_end(self):
+        """So far there is no frontend option for statistics analysis"""
+        pass
