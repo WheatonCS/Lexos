bf5552f12dd0432bea3b90f5836a29a66992c9b3
Weiqi97 <weltch1997@gmail.com>
2017-11-12 20:10:38 -0500

    update test

--- test/unit_test/test_similarity_model.py
+++ test/unit_test/test_similarity_model.py
@@ -1,41 +1,49 @@
 import pandas as pd
-from pandas.util.testing import assert_frame_equal
-
-from lexos.helpers.error_messages import NON_NEGATIVE_INDEX_MESSAGE
-from lexos.processors.analyze.similarity import similarity_maker
+from lexos.models.similarity_model import SimilarityModel
+from lexos.receivers.similarity_receiver import SimilarityOption
 
 
 # --------------------- test with similarity equals one --------------------
-from lexos.receivers.similarity_receiver import SimilarityOption
-
-test_dtm = pd.DataFrame([[0.0, 0.0, 9.0, 9.0, 0.0, 0.0, 5.0, 4.0,0.0, 9.0, 0.0,
-                          0.0, 0.0, 0.0, 0.0, 9.0, 0.0, 0.0, 0.0],
-                         [0.0, 0.0, 9.0, 9.0, 0.0, 0.0, 0.0, 4.0,5.0, 9.0, 0.0,
-                          0.0, 0.0, 0.0, 0.0, 9.0, 0.0, 0.0, 0.0],
-                         [5.0, 10.0, 0.0, 0.0, 10.0, 5.0, 0.0, 0.0, 0.0, 0.0,
-                          10.0, 5.0, 5.0, 5.0, 5.0, 0.0, 5.0, 5.0, 5.0]],
-                        index=["F1", "F2", "F3"])
-SimilarityOption.comp_file_id = 3
-
+def test_with_similarity_equal_one():
+    test_dtm = pd.DataFrame([[0.0, 0.0, 9.0, 9.0, 0.0, 0.0, 5.0, 4.0, 0.0, 9.0,
+                              0.0, 0.0, 0.0, 0.0, 0.0, 9.0, 0.0, 0.0, 0.0],
+                             [0.0, 0.0, 9.0, 9.0, 0.0, 0.0, 0.0, 4.0, 5.0, 9.0,
+                              0.0, 0.0, 0.0, 0.0, 0.0, 9.0, 0.0, 0.0, 0.0],
+                             [5.0, 10.0, 0.0, 0.0, 10.0, 5.0, 0.0, 0.0, 0.0,
+                              0.0,
+                              10.0, 5.0, 5.0, 5.0, 5.0, 0.0, 5.0, 5.0, 5.0]],
+                            index=[0, 1, 2])
+    test_option = SimilarityOption(comp_file_id=2)
+    test_id_table = {0: "F1.txt", 1: "F2.txt", 2: "F3.txt"}
+    similarity_model = SimilarityModel(test_dtm=test_dtm,
+                                       test_option=test_option,
+                                       test_id_temp_label_map=test_id_table)
+    scores = similarity_model.get_similarity_score()
+    labels = similarity_model.get_similarity_label()
+    assert scores == '1.0***1.0***'
+    assert labels == "F1.txt***F2.txt***"
 # --------------------------------------------------------------------------
-    def test_with_similarity_equals_one(self):
 
-        comp_file_index = 2
-        assert_frame_equal(similarity_maker(dtm_data_frame, comp_file_index),
-                           pd.DataFrame([[1.0], [1.0]],
-                                        index=['catBobcat', 'catCaterpillar'],
-                                        columns=["Cosine similarity"]))
 
-    def test_with_all_same_content_file(self):
-        dtm_data_frame = pd.DataFrame([[9.0, 9.0, 5.0, 4.0, 0.0, 9.0, 9.0],
-                                       [9.0, 9.0, 5.0, 4.0, 0.0, 9.0, 9.0],
-                                       [9.0, 9.0, 5.0, 4.0, 0.0, 9.0, 9.0]],
-                                      index=['file_1', 'file_2', 'file_3'])
-        comp_file_index = 1
-        assert_frame_equal(similarity_maker(dtm_data_frame, comp_file_index),
-                           pd.DataFrame([[0.0], [0.0]],
-                                        index=['file_1', 'file_3'],
-                                        columns=["Cosine similarity"]))
+# --------------------- test with all same content -------------------------
+def test_with_all_same_content_file():
+    test_dtm = pd.DataFrame([[9.0, 9.0, 5.0, 4.0, 0.0, 9.0, 9.0],
+                             [9.0, 9.0, 5.0, 4.0, 0.0, 9.0, 9.0],
+                             [9.0, 9.0, 5.0, 4.0, 0.0, 9.0, 9.0]],
+                            index=[0, 1, 2])
+    test_option = SimilarityOption(comp_file_id=1)
+    test_id_table = {0: "F1.txt", 1: "F2.txt", 2: "F3.txt"}
+    similarity_model = SimilarityModel(test_dtm=test_dtm,
+                                       test_option=test_option,
+                                       test_id_temp_label_map=test_id_table)
+    scores = similarity_model.get_similarity_score()
+    labels = similarity_model.get_similarity_label()
+    assert scores == '0.0***0.0***'
+    assert labels == "F1.txt***F3.txt***"
+# --------------------------------------------------------------------------
+
+"""
+
 
     def test_with_two_dimension(self):
         dtm_data_frame = pd.DataFrame([[0.0, 1.0], [1.0, 2.0], [2.0, 1.0]],
@@ -65,3 +73,4 @@ SimilarityOption.comp_file_id = 3
             raise AssertionError("negative index error did not raise.")
         except AssertionError as error:
             assert str(error) == NON_NEGATIVE_INDEX_MESSAGE
+"""
