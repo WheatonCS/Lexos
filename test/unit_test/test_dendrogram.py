0ff966f452cbd031a1323054006d26ee5fbeb9b4
chantisnake <13501393281@163.com>
2017-11-12 20:13:33 -0500

    remove old test

--- test/unit_test/test_dendrogram.py
+++ test/unit_test/test_dendrogram.py
@@ -1,6 +1,6 @@
 import pandas as pd
 
-from lexos.models.dendro_model import DendrogramModel, DendroOption
+from lexos.models.dendro_model import DendroOption
 
 dtm_data_frame = pd.DataFrame([[0.0, 0.0, 9.0, 9.0, 0.0, 0.0, 5.0, 4.0],
                                [0.0, 0.0, 9.0, 9.0, 0.0, 0.0, 0.0, 4.0],
@@ -14,6 +14,3 @@ options = DendroOption(orientation='left',
                        dist_metric='euclidean')
 
 
-def test_regular():
-    model = DendrogramModel(test_dtm=dtm_data_frame,
-                            test_option=options)
