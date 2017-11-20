1a1479d91b8f362fedee4b125b0b0bda878b350e
chantisnake <13501393281@163.com>
2017-11-19 22:46:21 -0500

    split the view function into 3 web page

--- lexos/views/similarity_query_view.py
+++ lexos/views/similarity_query_view.py
@@ -1,7 +1,8 @@
-from flask import request, session, render_template, send_file, Blueprint
+from flask import session, render_template, send_file, Blueprint
 
 from lexos.helpers import constants as constants
-from lexos.managers import utility, session_manager as session_manager
+from lexos.managers import session_manager as session_manager
+from lexos.models.filemanager_model import FileManagerModel
 from lexos.models.similarity_model import SimilarityModel
 from lexos.views.base_view import detect_active_docs
 
@@ -24,49 +25,34 @@ def similarity():
     """
     # Detect the number of active documents.
     num_active_docs = detect_active_docs()
-    file_manager = utility.load_file_manager()
-    encoded_labels = {}
-    labels = file_manager.get_active_labels_with_id()
-    for i in labels:
-        encoded_labels[str(i)] = labels[i]
+    id_label_map = \
+        FileManagerModel().load_file_manager().get_active_labels_with_id()
 
-    if request.method == 'GET':
-        # 'GET' request occurs when the page is first loaded
-        if 'analyoption' not in session:
-            session['analyoption'] = constants.DEFAULT_ANALYZE_OPTIONS
-        if 'similarities' not in session:
-            session['similarities'] = constants.DEFAULT_SIM_OPTIONS
-        return render_template(
-            'similarity.html',
-            labels=labels,
-            encodedLabels=encoded_labels,
-            docsListScore="",
-            docsListName="",
-            similaritiesgenerated=False,
-            itm="similarity-query",
-            numActiveDocs=num_active_docs)
+    # 'GET' request occurs when the page is first loaded
+    if 'analyoption' not in session:
+        session['analyoption'] = constants.DEFAULT_ANALYZE_OPTIONS
+    if 'similarities' not in session:
+        session['similarities'] = constants.DEFAULT_SIM_OPTIONS
+    return render_template(
+        'similarity.html',
+        labels=id_label_map,
+        similaritiesgenerated=False,
+        numActiveDocs=num_active_docs)
 
-    if 'gen-sims' in request.form:
-        # 'POST' request occur when html form is submitted
-        # (i.e. 'Get Graphs', 'Download...')
-        session_manager.cache_analysis_option()
-        session_manager.cache_sim_options()
-        return render_template(
-            'similarity.html',
-            labels=labels,
-            encodedLabels=encoded_labels,
-            docsListScore=docs_score,
-            docsListName=docs_label,
-            similaritiesgenerated=True,
-            itm="similarity-query",
-            numActiveDocs=num_active_docs)
 
-    if 'get-sims' in request.form:
-        # The 'Download Matrix' button is clicked on similarity.html.
-        session_manager.cache_analysis_option()
-        session_manager.cache_sim_options()
-        save_path = SimilarityModel().generate_sims_csv()
-        return send_file(
-            save_path,
-            as_attachment=True,
-            attachment_filename="similarity-query.csv")
+@sim_blueprint.route("/similarityTable", method=['POST'])
+def sim_html():
+    session_manager.cache_analysis_option()
+    session_manager.cache_sim_options()
+    return SimilarityModel().generate_sims_html()
+
+
+@sim_blueprint.route("/similarityCSV", method=['POST'])
+def sim_csv():
+    session_manager.cache_analysis_option()
+    session_manager.cache_sim_options()
+    save_path = SimilarityModel().generate_sims_csv()
+    return send_file(
+        save_path,
+        as_attachment=True,
+        attachment_filename="similarity-query.csv")
