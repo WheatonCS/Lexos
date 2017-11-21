d33f6e323ce2d56b37d499a32fc49ce8483ad575
Weiqi97 <weltch1997@gmail.com>
2017-11-21 02:27:13 -0500

    Change name again...

--- lexos/receivers/topword_receiver.py
+++ lexos/receivers/topword_receiver.py
@@ -3,21 +3,21 @@ from typing import NamedTuple
 from lexos.receivers.base_receiver import BaseReceiver
 
 
-class TopwordOption(NamedTuple):
+class TopwordFrontEndOption(NamedTuple):
     """The typed tuple to implement topword options."""
     analysis_option: str
 
 
-class TopwordFrontEndReceiver(BaseReceiver):
+class TopwordReceiver(BaseReceiver):
     def __init__(self):
         """The Receiver to get all the topword options from front end."""
         super().__init__()
 
-    def options_from_front_end(self) -> TopwordOption:
+    def options_from_front_end(self) -> TopwordFrontEndOption:
         """Get the topword option from front end.
 
         :return: a TopwordFrontEndOption object to hold the analysis option.
         """
         analysis_option = self._front_end_data['testInput']
 
-        return TopwordOption(analysis_option=analysis_option)
+        return TopwordFrontEndOption(analysis_option=analysis_option)
