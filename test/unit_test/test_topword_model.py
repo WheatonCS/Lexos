95ad31dbf423912ff59f9df6551045876ed9adf7
Weiqi97 <weltch1997@gmail.com>
2017-11-25 22:26:18 -0500

    More tests

--- test/unit_test/test_topword_model.py
+++ test/unit_test/test_topword_model.py
@@ -8,3 +8,11 @@ class TestZTest:
         assert round(TopwordModel._z_test(p1=0.1, pt=0.3, n1=100, nt=10000), 2) == -4.35
         assert round(TopwordModel._z_test(p1=0.3, pt=0.1, n1=100, nt=100), 2) == 3.54
         assert TopwordModel._z_test(p1=1, pt=1, n1=100, nt=100) == 0
+
+    def test_special_case(self):
+        try:
+            _ = TopwordModel._z_test(p1=0.1, pt=0.3, n1=100, nt=0)
+            raise AssertionError("Error message did not raise")
+        except AssertionError as error:
+            assert str(error) == SEG_NON_POSITIVE_MESSAGE
+
