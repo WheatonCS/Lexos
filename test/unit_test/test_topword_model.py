9f72b8402b908b2d42966fe691e61681f8922091
Weiqi97 <weltch1997@gmail.com>
2017-11-25 22:28:28 -0500

    Fix indentation errors

--- test/unit_test/test_topword_model.py
+++ test/unit_test/test_topword_model.py
@@ -2,11 +2,12 @@ from lexos.helpers.error_messages import SEG_NON_POSITIVE_MESSAGE
 from lexos.models.topword_model import TopwordModel
 
 
-#
 class TestZTest:
     def test_normal_case(self):
-        assert round(TopwordModel._z_test(p1=0.1, pt=0.3, n1=100, nt=10000), 2) == -4.35
-        assert round(TopwordModel._z_test(p1=0.3, pt=0.1, n1=100, nt=100), 2) == 3.54
+        assert round(TopwordModel._z_test(p1=0.1, pt=0.3, n1=10, nt=1000), 2) \
+            == -1.38
+        assert round(TopwordModel._z_test(p1=0.3, pt=0.1, n1=100, nt=100), 2) \
+            == 3.54
         assert TopwordModel._z_test(p1=1, pt=1, n1=100, nt=100) == 0
 
     def test_special_case(self):
