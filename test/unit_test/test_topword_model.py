1bd9879d09a706ed9b71f8e9d3f45f28eecf7760
Weiqi97 <weltch1997@gmail.com>
2017-11-25 23:43:47 -0500

    Refactor

--- test/unit_test/test_topword_model.py
+++ test/unit_test/test_topword_model.py
@@ -8,21 +8,21 @@ from lexos.receivers.topword_receiver import TopwordFrontEndOption
 # ---------------------------- Test for z-test ------------------------------
 class TestZTest:
     def test_normal_case(self):
-        assert round(TopwordModel._z_test(p1=0.1, pt=0.3, n1=10, nt=1000), 2) \
+        assert round(TopwordModel.z_test(p1=0.1, pt=0.3, n1=10, nt=1000), 2) \
             == -1.38
-        assert round(TopwordModel._z_test(p1=0.3, pt=0.1, n1=100, nt=100), 2) \
+        assert round(TopwordModel.z_test(p1=0.3, pt=0.1, n1=100, nt=100), 2) \
             == 3.54
-        assert TopwordModel._z_test(p1=1, pt=1, n1=100, nt=100) == 0
+        assert TopwordModel.z_test(p1=1, pt=1, n1=100, nt=100) == 0
 
     def test_special_case(self):
         try:
-            _ = TopwordModel._z_test(p1=0.1, pt=0.3, n1=100, nt=0)
+            _ = TopwordModel.z_test(p1=0.1, pt=0.3, n1=100, nt=0)
             raise AssertionError("Error message did not raise")
         except AssertionError as error:
             assert str(error) == SEG_NON_POSITIVE_MESSAGE
 
         try:
-            _ = TopwordModel._z_test(p1=0.1, pt=0.3, n1=0, nt=100)
+            _ = TopwordModel.z_test(p1=0.1, pt=0.3, n1=0, nt=100)
             raise AssertionError("Error message did not raise")
         except AssertionError as error:
             assert str(error) == SEG_NON_POSITIVE_MESSAGE
@@ -32,8 +32,8 @@ class TestZTest:
 # -------------------- Test method analyze all to para ----------------------
 # Create test suits for normal cases
 test_dtm = pd.DataFrame(data=np.array([(1, 1, 0, 0), (0, 0, 1, 10)]),
-                            index=np.array([0, 1]),
-                            columns=np.array(["A", "B", "C", "D"]))
+                        index=np.array([0, 1]),
+                        columns=np.array(["A", "B", "C", "D"]))
 test_id_temp_label_map = {0: "F1", 1: "F2"}
 test_front_end_option = TopwordFrontEndOption(analysis_option="allToPara")
 test_option = TopwordTestOptions(doc_term_matrix=test_dtm,
