4ff5439b043f860da141c949ca40172e69ec4d81
Weiqi97 <weltch1997@gmail.com>
2017-11-25 22:26:04 -0500

    More tests

--- test/unit_test/test_topword_model.py
+++ test/unit_test/test_topword_model.py
@@ -6,3 +6,5 @@ from lexos.models.topword_model import TopwordModel
 class TestZTest:
     def test_normal_case(self):
         assert round(TopwordModel._z_test(p1=0.1, pt=0.3, n1=100, nt=10000), 2) == -4.35
+        assert round(TopwordModel._z_test(p1=0.3, pt=0.1, n1=100, nt=100), 2) == 3.54
+        assert TopwordModel._z_test(p1=1, pt=1, n1=100, nt=100) == 0
