6aa9d42ea71d646f80c1162469ea7600415cbc2b
chantisnake <13501393281@163.com>
2017-11-29 19:48:55 -0500

    add some documentation and better naming

--- lexos/helpers/definitions.py
+++ lexos/helpers/definitions.py
@@ -1,8 +1,13 @@
 # ====== Host all the text definition =======
 import re
 
+# the word is a sequence (more than one) of non-space character
 WORD_REGEX_STR = r'\S+'
-WORD_RIGHT_BOUNDARY_REGEX_STR = r'^|\s'
-WORD_LEFT_BOUNDARY_REGEX_STR = r'\s|$'
 
+# the left boundary of a word is either the start of the passage or a space
+SINGLE_LEFT_WORD_BOUNDARY_REGEX_STR = r'^|\s'
+# the right boundary of a word is either the end of the passage or a space
+SINGLE_RIGHT_WORD_BOUNDARY_REGEX_STR = r'\s|$'
+
+# ============== compiled version ==================
 WORD_REGEX = re.compile(WORD_REGEX_STR, re.UNICODE)
