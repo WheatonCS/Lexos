a4c294dec3b49554ca45387d14486c8951bbcb2b
chantisnake <13501393281@163.com>
2017-12-04 11:24:03 -0500

    add the get preview function and the main function

--- lexos/models/scrubber_model.py
+++ lexos/models/scrubber_model.py
@@ -580,10 +580,54 @@ class ScrubberModel(BaseModel):
         return {file_id: self._scrub(file_id)
                 for file_id, content in self._file_id_content_map.items()}
 
-    def save_scrub_changes(self):
-        """Perform side effect to save scrubbed the file on the disk"""
+    @staticmethod
+    def _save_scrub_changes(id_scrubbed_content_map: FileIDContentMap):
+        """Perform side effect to save scrubbed the file on the disk
+
+        This function is not tested, because it performs side-effects.
+        For those looking at this function, beware that performing
+        performing side effect is dangerous and untestable.
+        make as few side-effect as possible and do not mix them with testable
+        functions.
+        :param id_scrubbed_content_map: a dictionary maps file id to
+                scrubbed content
+        """
 
         file_manager = FileManagerModel().load_file_manager()
         scrubbed_file_manager = file_manager.mass_update_content(
-            self._get_all_scrub_text())
+            id_content_map=id_scrubbed_content_map)
         FileManagerModel().save_file_manager(scrubbed_file_manager)
+
+    @staticmethod
+    def _get_preview_of_scrubbed(id_scrubbed_content_map: FileIDContentMap) \
+            -> FileIDContentMap:
+        """Make a dictionary maps file id to preview of the scrubbed content.
+
+        :param id_scrubbed_content_map: a dictionary maps file id to the
+            scrubbed content
+        :return: a dictionary map file id to preview of the scrubbed content
+        """
+        return {file_id: general_functions.make_preview_from(content)
+                for file_id, content in id_scrubbed_content_map.items()}
+
+    def scrub_active_file_and_return_preview(self, save_changes: bool) \
+            -> FileIDContentMap:
+        """scrubs the file and returns the preview
+
+        :param save_changes: if true then we save the scrubbed file onto the
+            disk, else we only generate the previews.
+        :return: a dictionary maps file id to preview of scrubbed content
+        """
+        id_scrubbed_content_map = self._get_all_scrub_text()
+
+        # saves the changes to the disk
+        if save_changes:
+            ScrubberModel._save_scrub_changes(
+                id_scrubbed_content_map=id_scrubbed_content_map)
+
+        # return
+        return ScrubberModel._get_preview_of_scrubbed(
+            id_scrubbed_content_map=id_scrubbed_content_map)
+
+
+
