a844ff9f4e276253262dc9f9a4b09a4472f241d6
Weiqi97 <weltch1997@gmail.com>
2017-12-27 12:36:55 +0800

    Update view

--- lexos/views/k_means_view.py
+++ lexos/views/k_means_view.py
@@ -17,7 +17,7 @@ k_means_blueprint = Blueprint('k_means', __name__)
 
 
 # Tells Flask to load this function when someone is at '/kmeans'
-@k_means_blueprint.route("/kmeans", methods=["GET", "POST"])
+@k_means_blueprint.route("/kmeans", methods=["GET"])
 def k_means():
     """Handles the functionality on the kmeans page.
 
@@ -32,113 +32,28 @@ def k_means():
     for key in labels:
         labels[key] = labels[key]
     default_k = int(len(labels) / 2)
-    if request.method == 'GET':
-        # 'GET' request occurs when the page is first loaded
-        if 'analyoption' not in session:
-            session['analyoption'] = constants.DEFAULT_ANALYZE_OPTIONS
-        if 'kmeanoption' not in session:
-            session['kmeanoption'] = constants.DEFAULT_KMEAN_OPTIONS
-        return render_template(
-            'kmeans.html',
-            labels=labels,
-            silhouettescore='',
-            kmeansIndex=[],
-            fileNameStr='',
-            fileNumber=len(labels),
-            KValue=0,
-            defaultK=default_k,
-            colorChartStr='',
-            kmeansdatagenerated=False,
-            itm="kmeans",
-            numActiveDocs=num_active_docs)
-    if request.method == "POST":
-        # 'POST' request occur when html form is submitted
-        # (i.e. 'Get Graphs', 'Download...')
-        session_manager.cache_analysis_option()
-        session_manager.cache_k_mean_option()
-        utility.save_file_manager(file_manager)
-        if request.form['viz'] == 'PCA':
-            PCA_result = KmeansModel().get_pca_result()
-            return render_template(
-                'kmeans.html',
-                labels=PCA_result.labels,
-                silhouettescore=PCA_result.silhouette_score,
-                kmeansIndex=PCA_result.best_index,
-                fileNameStr="",
-                fileNumber=len(labels),
-                KValue=PCA_result.k_value,
-                defaultK=default_k,
-                colorChartStr=PCA_result.color_chart,
-                kmeansdatagenerated=True,
-                itm="kmeans",
-                numActiveDocs=num_active_docs)
-        elif request.form['viz'] == 'Voronoi':
-            kmeans_index, silhouette_score, file_name_str, k_value, \
-                color_chart_str, final_points_list, final_centroids_list, \
-                text_data, max_x = \
-                utility.generate_k_means_voronoi(file_manager)
-            return render_template(
-                'kmeans.html',
-                labels=labels,
-                silhouettescore=silhouette_score,
-                kmeansIndex=kmeans_index,
-                fileNameStr=file_name_str,
-                fileNumber=len(labels),
-                KValue=k_value,
-                defaultK=default_k,
-                colorChartStr=color_chart_str,
-                finalPointsList=final_points_list,
-                finalCentroidsList=final_centroids_list,
-                textData=text_data,
-                maxX=max_x,
-                kmeansdatagenerated=True,
-                itm="kmeans",
-                numActiveDocs=num_active_docs)
+    # 'GET' request occurs when the page is first loaded
+    if 'analyoption' not in session:
+        session['analyoption'] = constants.DEFAULT_ANALYZE_OPTIONS
+    if 'kmeanoption' not in session:
+        session['kmeanoption'] = constants.DEFAULT_KMEAN_OPTIONS
+    return render_template(
+        'kmeans.html',
+        labels=labels,
+        silhouettescore='',
+        kmeansIndex=[],
+        fileNameStr='',
+        fileNumber=len(labels),
+        KValue=0,
+        defaultK=default_k,
+        colorChartStr='',
+        kmeansdatagenerated=False,
+        itm="kmeans",
+        numActiveDocs=num_active_docs)
 
 
-# Tells Flask to load this function when someone is at '/kmeansimage'
-@k_means_blueprint.route("/kmeansimage", methods=["GET", "POST"])
-def k_means_image():
-    """Reads the png image of the kmeans and displays it on the web browser.
-
-    *kmeansimage() linked to in analysis.html, displaying the kmeansimage.png
-    :return: a response object with the kmeansimage png to flask and
-     eventually to the browser.
-    """
-    # kmeansimage() is called in kmeans.html, displaying the
-    # KMEANS_GRAPH_FILENAME (if session['kmeansdatagenerated'] != False).
-    image_path = path_join(
-        session_manager.session_folder(),
-        constants.RESULTS_FOLDER,
-        constants.KMEANS_GRAPH_FILENAME)
-    return send_file(image_path)
-
-
-@k_means_blueprint.route("/small_PCA", methods=["GET", "POST"])
-def small_pca():
-    """Reads the small image of the PCA and displays it on the web browser.
-
-    :return: a response object with the small PCA as a png to flask and
-    eventually to the browser.
-    """
-    if constants.PCA_SMALL_GRAPH_FILENAME:
-        folder = path_join(
-            session_manager.session_folder(),
-            constants.RESULTS_FOLDER)
-        plotly_url = os.path.join(folder, constants.PCA_SMALL_GRAPH_FILENAME)
-        return send_file(plotly_url)
-
-
-@k_means_blueprint.route("/big_PCA", methods=["GET", "POST"])
-def big_pca():
-    """Reads the big image of the PCA and displays it on the web browser.
-
-    :return: a response object with the big PCA as a png to flask and
-    eventually to the browser.
-    """
-    if constants.PCA_BIG_GRAPH_FILENAME:
-        folder = path_join(
-            session_manager.session_folder(),
-            constants.RESULTS_FOLDER)
-        plotly_url = os.path.join(folder, constants.PCA_BIG_GRAPH_FILENAME)
-        return send_file(plotly_url)
+@k_means_blueprint.route("/kmeansDiv", methods=['POST'])
+def kmeans_div():
+    session_manager.cache_analysis_option()
+    session_manager.cache_k_mean_option()
+    return KmeansModel().get_pca_result()
