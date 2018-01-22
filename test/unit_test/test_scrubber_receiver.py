5d98d3cd14dc005dad9fc8322e2b2cb82d2fe928
Emma-sama <steffens_emma@wheatoncollege.edu>
2018-01-22 04:20:56 -0500

    tested create dict without merge

--- test/unit_test/test_scrubber_receiver.py
+++ test/unit_test/test_scrubber_receiver.py
@@ -1,5 +1,9 @@
+from lexos.helpers.error_messages import NOT_ONE_REPLACEMENT_COLON_MESSAGE, \
+    REPLACEMENT_RIGHT_OPERAND_MESSAGE, REPLACEMENT_NO_LEFTHAND_MESSAGE
+from lexos.helpers.exceptions import LexosException
 from lexos.receivers.scrubber_receiver import ScrubbingReceiver
 from test.helpers import special_chars_and_punct as chars
+
 
 # _load_character_deletion_map
 
@@ -94,7 +98,173 @@ class TestHandleFileAndManualStrings:
             == string1 + "\n" + string2
 
 
-# create_replacements_dict
+class TestCreateReplacementsDict:
+
+    def test_single_char_normal(self):
+        assert ScrubbingReceiver().create_replacements_dict(
+            replacer_string="s:f") == {"s": "f"}
+        assert ScrubbingReceiver().create_replacements_dict(
+            replacer_string="i,e:a") == {"i": "a", "e": "a"}
+        assert ScrubbingReceiver().create_replacements_dict(
+            replacer_string="t:t") == {"t": "t"}
+        assert ScrubbingReceiver().create_replacements_dict(
+            replacer_string=" s : f ") == \
+            ScrubbingReceiver().create_replacements_dict(
+                replacer_string="s:f")
+        assert ScrubbingReceiver().create_replacements_dict(
+            replacer_string="e:b \n i:o ") == {"e": "b", "i": "o"}
+        assert ScrubbingReceiver().create_replacements_dict(
+            replacer_string="e:b\nE:u ") == {"e": "b", "E": "u"}
+        assert ScrubbingReceiver().create_replacements_dict(
+            replacer_string="n:t\nt:x") == {"n": "t", "t": "x"}
+
+    def test_single_char_incomplete_replacer(self):
+        assert ScrubbingReceiver().create_replacements_dict(
+            replacer_string="g:") == {"g": ""}
+        assert ScrubbingReceiver().create_replacements_dict(
+            replacer_string="g:p\np:") == {"g": "p", "p": ""}
+        assert ScrubbingReceiver().create_replacements_dict(
+            replacer_string="") == {}
+        assert ScrubbingReceiver().create_replacements_dict(
+            replacer_string=" ") == \
+            ScrubbingReceiver().create_replacements_dict(replacer_string="")
+        assert ScrubbingReceiver().create_replacements_dict(
+            replacer_string="\n") == \
+            ScrubbingReceiver().create_replacements_dict(replacer_string="")
+        # Missing/too many colons
+        try:
+            ScrubbingReceiver().create_replacements_dict(
+                replacer_string="s,f")
+        except LexosException as excep:
+            assert str(excep) == NOT_ONE_REPLACEMENT_COLON_MESSAGE + "s,f"
+        try:
+            ScrubbingReceiver().create_replacements_dict(
+                replacer_string=",")
+        except LexosException as excep:
+            assert str(excep) == NOT_ONE_REPLACEMENT_COLON_MESSAGE + ","
+        try:
+            ScrubbingReceiver().create_replacements_dict(
+                replacer_string="k")
+        except LexosException as excep:
+            assert str(excep) == NOT_ONE_REPLACEMENT_COLON_MESSAGE + "k"
+        try:
+            ScrubbingReceiver().create_replacements_dict(
+                replacer_string="t:u:w")
+        except LexosException as excep:
+            assert str(excep) == NOT_ONE_REPLACEMENT_COLON_MESSAGE + "t:u:w"
+        try:
+            ScrubbingReceiver().create_replacements_dict(
+                replacer_string="t::w")
+        except LexosException as excep:
+            assert str(excep) == NOT_ONE_REPLACEMENT_COLON_MESSAGE + "t::w"
+        # Too many arguments on right of colon
+        try:
+            ScrubbingReceiver().create_replacements_dict(
+                replacer_string="a:i,e")
+        except LexosException as excep:
+            assert str(excep) == REPLACEMENT_RIGHT_OPERAND_MESSAGE + "a:i,e"
+        try:
+            ScrubbingReceiver().create_replacements_dict(
+                replacer_string="s,t:u,v")
+        except LexosException as excep:
+            assert str(excep) == REPLACEMENT_RIGHT_OPERAND_MESSAGE + "s,t:u,v"
+        # No argument on left of colon
+        try:
+            ScrubbingReceiver().create_replacements_dict(
+               replacer_string=":k")
+        except LexosException as excep:
+            assert str(excep) == REPLACEMENT_NO_LEFTHAND_MESSAGE + ":k"
+        try:
+            ScrubbingReceiver().create_replacements_dict(
+                replacer_string=":")
+        except LexosException as excep:
+            assert str(excep) == REPLACEMENT_NO_LEFTHAND_MESSAGE + ":"
+
+    def test_multichar_is_same(self):
+        assert ScrubbingReceiver().create_replacements_dict(
+            replacer_string="string:thread") == {"string": "thread"}
+        assert ScrubbingReceiver().create_replacements_dict(
+            replacer_string="string,rope:thread") == \
+            {"string": "thread", "rope": "thread"}
+        assert ScrubbingReceiver().create_replacements_dict(
+            replacer_string="is,testing:working\nfoo:bar") == \
+            {"is": "working", "testing": "working", "foo": "bar"}
+        assert ScrubbingReceiver().create_replacements_dict(
+            replacer_string="String:thread\nstring:rope") == \
+            {"String": "thread", "string": "rope"}
+
+    def test_multichar_incomplete_replacer(self):
+        assert ScrubbingReceiver().create_replacements_dict(
+            replacer_string="is:") == {"is": ""}
+        # Missing/too many colons
+        try:
+            ScrubbingReceiver().create_replacements_dict(
+                replacer_string="Test,testing,working")
+        except LexosException as excep:
+            assert str(excep) == NOT_ONE_REPLACEMENT_COLON_MESSAGE + \
+                "Test,testing,working"
+        try:
+            ScrubbingReceiver().create_replacements_dict(
+                replacer_string="word")
+        except LexosException as excep:
+            assert str(excep) == NOT_ONE_REPLACEMENT_COLON_MESSAGE + "word"
+        try:
+            ScrubbingReceiver().create_replacements_dict(
+                replacer_string="is::word")
+        except LexosException as excep:
+            assert str(excep) == NOT_ONE_REPLACEMENT_COLON_MESSAGE + "is::word"
+        # Too many arguments on right of colon
+        try:
+            ScrubbingReceiver().create_replacements_dict(
+                replacer_string="working:Test,testing")
+        except LexosException as excep:
+            assert str(excep) == REPLACEMENT_RIGHT_OPERAND_MESSAGE + \
+                "working:Test,testing"
+        try:
+            ScrubbingReceiver().create_replacements_dict(
+                replacer_string="is,string:how,what")
+        except LexosException as excep:
+            assert str(excep) == REPLACEMENT_RIGHT_OPERAND_MESSAGE + \
+                "is,string:how,what"
+        # No argument on left of colon
+        try:
+            ScrubbingReceiver().create_replacements_dict(
+                replacer_string=":word")
+        except LexosException as excep:
+            assert str(excep) == REPLACEMENT_NO_LEFTHAND_MESSAGE + ":word"
+        try:
+            ScrubbingReceiver().create_replacements_dict(
+                replacer_string=":")
+        except LexosException as excep:
+            assert str(excep) == REPLACEMENT_NO_LEFTHAND_MESSAGE + ":"
+        # testing multiple error conditions
+        try:
+            ScrubbingReceiver().create_replacements_dict(
+                replacer_string="string:word\ntesting::working\n:yay")
+        except LexosException as excep:
+            assert str(excep) == NOT_ONE_REPLACEMENT_COLON_MESSAGE + \
+                "testing::working"
+        try:
+            ScrubbingReceiver().create_replacements_dict(
+                replacer_string=":yay\ntesting,working\nstring:word")
+        except LexosException as excep:
+            assert str(excep) == REPLACEMENT_NO_LEFTHAND_MESSAGE + ":yay"
+        try:
+            ScrubbingReceiver().create_replacements_dict(
+                replacer_string="string:word,thing\ntesting,working\n:yay")
+        except LexosException as excep:
+            assert str(excep) == REPLACEMENT_RIGHT_OPERAND_MESSAGE + \
+                "string:word,thing"
+
+    def test_replacements_dict_punct(self):
+        assert ScrubbingReceiver().create_replacements_dict(
+            replacer_string="^words:things\nwords$:junk\nword.:stuff"
+            "\nwords+:text") == {"^words": "things", "words$": "junk",
+                                 "word.": "stuff", "words+": "text"}
+        assert ScrubbingReceiver().create_replacements_dict(
+            replacer_string=".,l:!\n") == {".": "!", "l": "!"}
+        assert ScrubbingReceiver().create_replacements_dict(
+            replacer_string="^:>\n$:%\n?:&") == {"^": ">", "$": "%", "?": "&"}
 
 
 class TestGetSpecialCharDictFromFile:
