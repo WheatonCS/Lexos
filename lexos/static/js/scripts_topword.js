cc462fc17b9e417ca79cc458fbfce9ab7dc08590
danielmullen <dpmullen98@gmail.com>
2018-05-23 12:09:12 -0400

    An error message now appears when a user has less than 2 files and get topwords is clicked AND when download results is clicked with less than 2 files.

--- lexos/static/js/scripts_topword.js
+++ lexos/static/js/scripts_topword.js
@@ -1,11 +1,30 @@
 $(function () {
+  // define error message
   var msg = 'You do not have enough active documents. Please activate at least two documents using the <a href="./manage">Manage</a> tool or <a href="./upload">upload</a> a new document.'
-  console.log($('#num_active_files').val())
+  // When get topwords button is clicked
   $('#gettopword').click(function () {
+      // Check number of active files
       if ($('#num_active_files').val() <  2) {
+          //change button type to button so no request is made
+          $("input[name='gen-topword']").prop("type", "button");
+          // display error
           $('#error-modal .modal-body').html(msg)
           $('#error-modal').modal()
       }
+
+  })
+
+  //when download results is clicked
+  $('#topworddownload').click(function () {
+      // Check number of active files
+      if ($('#num_active_files').val() <  2) {
+          //change button type to button so no request is made
+          $("input[name='gen-topword']").prop("type", "button");
+          // display error
+          $('#error-modal .modal-body').html(msg)
+          $('#error-modal').modal()
+      }
+
   })
 
   // Hide unnecessary div for DTM
