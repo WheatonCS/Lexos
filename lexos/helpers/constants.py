903bc5b9633819b9a9b54bf832da8b6f948dabc2
Weiqi97 <weltch1997@gmail.com>
2018-05-31 01:57:35 -0400

    Remove useless constants

--- lexos/helpers/constants.py
+++ lexos/helpers/constants.py
@@ -218,8 +218,8 @@ DEFAULT_CUT_OPTIONS = {
     'cutType': 'words', 'cutValue': '', 'cutOverlap': '0', 'cutLastProp': '50'
 }
 
-DEFAULT_CSV_OPTIONS = {
-    'csvdata': 'count', 'csvorientation': 'filecolumn', 'csvdelimiter': 'comma'
+DEFAULT_TOKENIZER_OPTIONS = {
+    'tokenizeroption': 'fileColumn'
 }
 
 DEFAULT_ROLLINGWINDOW_OPTIONS = {
