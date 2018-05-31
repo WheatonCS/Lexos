3ef2e7c1f8f79f4b42b9c0f389a542e2d58b4a6a
Weiqi97 <weltch1997@gmail.com>
2018-05-31 01:58:13 -0400

    Update receiver

---
+++ lexos/receivers/tokenizer_receiver.py
@@ -0,0 +1,25 @@
+from enum import Enum
+from lexos.receivers.base_receiver import BaseReceiver
+
+
+class TokenizerTableOrientation(Enum):
+    FILE_COLUMN = "fileColumn"
+    FILE_ROW = "fileRow"
+
+
+class TokenizerReceiver(BaseReceiver):
+    """Get the tokenizer table orientation from front end."""
+    def __init__(self):
+        super().__init__()
+
+    def options_from_front_end(self) -> TokenizerTableOrientation:
+        """Get the tokenizer orientation from front end.
+
+        :return: a TokenizerTableOrientation object that holds the orientation.
+        """
+        if self._front_end_data["tableorientation"] == "fileColumn":
+            return TokenizerTableOrientation.FILE_COLUMN
+        elif self._front_end_data["tableorientation"] == "fileRow":
+            return TokenizerTableOrientation.FILE_ROW
+        else:
+            raise ValueError("Invalid tokenizer orientation from front end.")
