145594c72703ef8392051fa07020b845ba5fb8fe
Krissa Cusanelli <cusanelli_krisandra@wheatoncollege.edu>
2018-06-05 15:06:44 -0400

    unit test file as rows for tokenizer

--- test/unit_test/test_tokenizer_model.py
+++ test/unit_test/test_tokenizer_model.py
@@ -19,32 +19,10 @@ test_option_col = TokenizerTestOption(
     id_temp_label_map=test_id_temp_table_col)
 test_tokenizer_model_col = TokenizerModel(test_options=test_option_col)
 test_file_result_col = pd.read_html(test_tokenizer_model_col.get_dtm())[0]
-print("DONE")
-# ------------------------------------------------------------------
-
-# ------------------------ Test file as rows ------------------------
-test_dtm_row = pd.DataFrame(data=np.array
-                            ([(40, 20, 15, 5, 0, 0, 0, 0, 0, 0, 0, 0),
-                             (0, 0, 0, 0, 1, 2, 3, 4, 5, 0, 0, 0),
-                             (0, 0, 0, 0, 0, 0, 0, 0, 10, 11, 12, 13)]),
-                            index=np.array([0, 1, 2]),
-                            columns=np.array(["A", "B", "C", "D", "E", "F",
-                                              "G", "H", "I", "J", "K", "L"]))
-test_id_temp_table_row = {0: "F1.txt", 1: "F2.txt", 2: "F3.txt"}
-test_token_type_str_row = "Characters"
-test_front_end_option_row = TokenizerTableOrientation.FILE_ROW
-test_option_row = TokenizerTestOption(
-    token_type_str=test_token_type_str_row,
-    doc_term_matrix=test_dtm_row,
-    front_end_option=test_front_end_option_row,
-    id_temp_label_map=test_id_temp_table_row)
-test_tokenizer_model_row = TokenizerModel(test_options=test_option_row)
-test_file_result_row = pd.read_html(test_tokenizer_model_row.get_dtm())[0]
-print("DONE")
 # ------------------------------------------------------------------
 
 
-class TestFileColResultOne:
+class TestFileColResult:
     def test_column_names(self):
         assert test_file_result_col.columns.values[0] == "word"
         assert test_file_result_col.columns.values[1] == "Total"
@@ -60,17 +38,38 @@ class TestFileColResultOne:
         assert test_file_result_col["F2.txt"][0] == 0
 
 
-class TestFileRowResultTwo:
-    def test_column_names(self):
-        assert test_file_result_row.rows.values[0] == "word"
-        assert test_file_result_row.rows.values[1] == "Total"
-        assert test_file_col_result_one.columns.values[2] == "Average"
-        assert test_file_col_result_one.columns.values[3] == "F1.txt"
-        assert test_file_col_result_one.columns.values[4] == "F2.txt"
+# ------------------------ Test file as rows ------------------------
+test_dtm_row = pd.DataFrame(data=np.array([(40, 20, 15, 5, 0, 0, 0, 0, 0),
+                                           (0, 0, 0, 0, 1, 2, 3, 4, 5)]),
+                            index=np.array([0, 1]),
+                            columns=np.array(["A", "B", "C", "D", "E", "F",
+                                              "G", "H", "I"]))
+test_id_temp_table_row = {0: "F1.txt", 1: "F2.txt"}
+test_token_type_str_row = "word"
+test_front_end_option_row = TokenizerTableOrientation.FILE_ROW
+test_option_row = TokenizerTestOption(
+    token_type_str=test_token_type_str_row,
+    doc_term_matrix=test_dtm_row,
+    front_end_option=test_front_end_option_row,
+    id_temp_label_map=test_id_temp_table_row)
+test_tokenizer_model_row = TokenizerModel(test_options=test_option_row)
+test_file_result_row = pd.read_html(test_tokenizer_model_row.get_dtm())[0]
+# ------------------------------------------------------------------
 
-    def test_column_values(self):
-        assert test_file_col_result_one["word"][0] == "A"
-        assert test_file_col_result_one["Total"][0] == 40
-        assert test_file_col_result_one["Average"][0] == 20
-        assert test_file_col_result_one["F1.txt"][0] == 40
-        assert test_file_col_result_one["F2.txt"][0] == 0
+
+class TestFileRowResult:
+    def test_row_names(self):
+        assert test_file_result_row.loc[0][0] == "F1.txt"
+        assert test_file_result_row.loc[1][0] == "F2.txt"
+
+    def test_row_values(self):
+        assert test_file_result_row.loc[0][1] == 40
+        assert test_file_result_row.loc[0][2] == 20
+        assert test_file_result_row.loc[0][3] == 15
+        assert test_file_result_row.loc[0][4] == 5
+
+        assert test_file_result_row.loc[1][5] == 1
+        assert test_file_result_row.loc[1][6] == 2
+        assert test_file_result_row.loc[1][7] == 3
+        assert test_file_result_row.loc[1][8] == 4
+        assert test_file_result_row.loc[1][9] == 5
