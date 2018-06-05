4acae917b1025132dc471bf2bb03141cf2ff0227
Weiqi97 <weltch1997@gmail.com>
2018-06-05 15:09:34 -0400

    Finish documenting.. Finish adding stuff.. I promise

--- lexos/models/tokenizer_model.py
+++ lexos/models/tokenizer_model.py
@@ -220,3 +220,16 @@ class TokenizerModel(BaseModel):
 
         # Return where the file is.
         return file_path
+
+    def get_dtm_size(self) -> int:
+        """Return the size of the dtm.
+
+        :return: An integer which represents the number of data contained in
+                 the generated dtm.
+        """
+        if self._front_end_option == TokenizerTableOrientation.FILE_COLUMN:
+            return self._get_file_col_dtm().size
+        elif self._front_end_option == TokenizerTableOrientation.FILE_ROW:
+            return self._get_file_row_dtm().size
+        else:
+            raise ValueError("Invalid tokenizer orientation from front end.")
