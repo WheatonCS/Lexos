f25eb1c3fd10ebb280f3e4666514022cddd3a767
Weiqi97 <weltch1997@gmail.com>
2018-06-05 12:25:11 -0400

    Get download working

--- lexos/views/tokenizer_view.py
+++ lexos/views/tokenizer_view.py
@@ -1,4 +1,4 @@
-from flask import session, Blueprint, render_template, send_file
+from flask import session, Blueprint, render_template, send_file, request
 from lexos.managers import session_manager
 from lexos.helpers import constants as constants
 from lexos.views.base_view import detect_active_docs
@@ -14,27 +14,34 @@ tokenizer_blueprint = Blueprint('tokenizer', __name__)
 
 
 # Tells Flask to load this function when someone is at '/tokenizer'
-@tokenizer_blueprint.route("/tokenizer", methods=["GET"])
+@tokenizer_blueprint.route("/tokenizer", methods=["GET", "POST"])
 def tokenizer():
     # Detect the number of active documents.
     num_active_docs = detect_active_docs()
     id_label_map = \
         FileManagerModel().load_file_manager().get_active_labels_with_id()
 
-    # Fill session with default options.
-    session['analyoption'] = constants.DEFAULT_ANALYZE_OPTIONS
-    session['tokenizerOption'] = constants.DEFAULT_TOKENIZER_OPTIONS
+    if request.method == "GET":
+        # Fill session with default options.
+        session['analyoption'] = constants.DEFAULT_ANALYZE_OPTIONS
+        session['tokenizerOption'] = constants.DEFAULT_TOKENIZER_OPTIONS
 
-    return render_template('tokenizer.html',
-                           labels=id_label_map,
-                           numActiveDocs=num_active_docs)
+        return render_template('tokenizer.html',
+                               labels=id_label_map,
+                               numActiveDocs=num_active_docs)
+    else:
+        file_path = TokenizerModel().download_dtm()
+        return send_file(file_path,
+                         as_attachment=True,
+                         attachment_filename="tokenizer_result.csv")
 
 
 @tokenizer_blueprint.route("/tokenizeTable", methods=["POST"])
 def tokenizer_result():
-    # Cache front end result and return table.
+    # Cache the front options for matrix model and tokenizer model.
     session_manager.cache_analysis_option()
     session_manager.cache_tokenizer_option()
+    # Return the generated DTM to ajax call.
     return TokenizerModel().get_dtm()
 
 
