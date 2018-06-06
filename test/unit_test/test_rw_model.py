a7e55f2509f371a3442e6194e0250622bb23f0b0
Krissa Cusanelli <cusanelli_krisandra@wheatoncollege.edu>
2018-06-06 11:59:16 -0400

    unit testing for rolling window model

---
+++ test/unit_test/test_rw_model.py
@@ -0,0 +1,31 @@
+import re
+from typing import NamedTuple, Optional, List, Iterator, Callable
+
+import numpy as np
+import pandas as pd
+import plotly.graph_objs as go
+from plotly.offline import plot
+
+from lexos.helpers.definitions import get_words_with_right_boundary, \
+    get_single_word_count_in_text
+from lexos.models.base_model import BaseModel
+from lexos.models.filemanager_model import FileManagerModel
+from lexos.models.matrix_model import FileIDContentMap
+from lexos.receivers.rolling_windows_receiver import RWAFrontEndOptions, \
+    RollingWindowsReceiver, WindowUnitType, RWATokenType, RWARatioTokenOptions,\
+    RWAAverageTokenOptions
+from lexos.models.rolling_windows_model import RollingWindowsModel, \
+    RWATestOptions
+
+
+test_option_one = RWATestOptions(file_id_content_map=
+                                 {0: "ha ha ha ha la ta ha",
+                                  2: "la la ta ta da da ha",
+                                  3: "ta da ha"
+                                  },
+                                 rolling_windows_options=RWAFrontEndOptions
+                                 (ratio_token_options=RWARatioTokenOptions
+                                 (token_type=RWATokenType("string"),
+                                  numerator_token="ta", denominator_token="ha")
+                                  , average_token_options=
+                                  RWAAverageTokenOptions()))
