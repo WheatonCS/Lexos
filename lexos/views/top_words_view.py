14a68dbd9d08e0d098672c90a8d5cc15b9911bff
Weiqi97 <weltch1997@gmail.com>
2018-06-10 15:48:46 -0400

    Rewrite view

--- lexos/views/top_words_view.py
+++ lexos/views/top_words_view.py
@@ -1,9 +1,9 @@
-from flask import request, session, render_template, send_file, Blueprint
+from flask import session, render_template, send_file, Blueprint
 from lexos.helpers import constants as constants
 from lexos.models.topword_model import TopwordModel
 from lexos.views.base_view import detect_active_docs
 from lexos.models.filemanager_model import FileManagerModel
-from lexos.managers import utility, session_manager as session_manager
+from lexos.managers import session_manager as session_manager
 
 # this is a flask blue print
 # it helps us to manage groups of views
@@ -38,43 +38,22 @@ def top_words():
     return render_template(
         'topword.html',
         labels=labels,
-        numClass=class_division_map.shape[0],
         numActiveDocs=num_active_docs,
         classDivisionMap=class_division_map)
 
 
 @top_words_blueprint.route("/topword", methods=["POST"])
 def topword_download():
-    # 'POST' request occurs when html form is submitted
-    num_active_docs = detect_active_docs()
-    file_manager = utility.load_file_manager()
-    labels = file_manager.get_active_labels_with_id()
+    session_manager.cache_analysis_option()
+    session_manager.cache_top_word_options()
+    file_path = TopwordModel().get_topword_csv_path()
+    return send_file(file_path,
+                     as_attachment=True,
+                     attachment_filename=constants.TOPWORD_CSV_FILE_NAME)
 
-    # get the class division map and number of existing classes
-    class_division_map = FileManagerModel().load_file_manager(). \
-        get_class_division_map()
-    num_class = class_division_map.shape[0]
-    if 'get-topword' in request.form:  # download topword
-        path = TopwordModel().get_topword_csv_path(
-            class_division_map=class_division_map)
-        session_manager.cache_analysis_option()
-        session_manager.cache_top_word_options()
-        return send_file(
-            path,
-            attachment_filename=constants.TOPWORD_CSV_FILE_NAME,
-            as_attachment=True)
-    else:
-        session_manager.cache_analysis_option()
-        session_manager.cache_top_word_options()
-        topword_result = TopwordModel().get_readable_result(
-            class_division_map=class_division_map)
-        return render_template(
-            'topword.html',
-            result=topword_result.results,
-            labels=labels,
-            header=topword_result.header,
-            numclass=num_class,
-            topwordsgenerated='True',
-            classmap=[],
-            itm='topwords',
-            numActiveDocs=num_active_docs)
+
+@top_words_blueprint.route("/topwordResult", methods=["POST"])
+def topword_result():
+    session_manager.cache_analysis_option()
+    session_manager.cache_top_word_options()
+    return TopwordModel().get_readable_result()
