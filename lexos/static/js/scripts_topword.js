eb4dbd84b1d0afc970e0b839861c46e9d4fd8ee8
Weiqi97 <weltch1997@gmail.com>
2018-06-10 18:38:25 -0400

    Remove unused functions..

--- lexos/static/js/scripts_topword.js
+++ lexos/static/js/scripts_topword.js
@@ -1,15 +1,4 @@
-/**
- * display/hide expandable divs (define group div) here
- * @returns {void}
- */
-function updateGroupOptionDiv () {
-  const $choice = $('.show-options div').siblings('input')
-  $.each($choice, function () {
-    if ($(this).is(':checked')) {
-      $(this).siblings('div').show()
-    } else { $(this).siblings('div').hide() }
-  })
-}
+
 
 $(function () {
   // Hide unnecessary div for DTM
@@ -17,25 +6,4 @@ $(function () {
   // set the normalize option to raw count
   $('#normalizeTypeRaw').attr('checked', true)
 
-  $('#temp-label-div').css('position', 'relative').css('left', '-6px').css('top', '0px')
-
-  $('.groupOption-div').click(function () {
-    updateGroupOptionDiv()
-  })
-
-  // Dynamically change the upper and lower bounds based on user inputs (Proportional Counts)
-  $('#upperboundPC').click(function () {
-    $(this).context.min = $('#lowerboundPC').val()
-    $('#upperboundRC, #lowerboundRC').val(0)
-  })
-
-  $('#lowerboundPC').click(function () {
-    $(this).context.max = $('#upperboundPC').val()
-    $('#upperboundRC, #lowerboundRC').val(0)
-  })
-
-  // Reset proportional counts input fields while raw counts is chosed
-  $('#upperboundRC, #lowerboundRC').click(function () {
-    $('#upperboundPC, #lowerboundPC').val(0)
-  })
 })
