64224e3584dc60110e83d69573d5ccf6139a376b
Krissa Cusanelli <cusanelli_krisandra@wheatoncollege.edu>
2018-06-11 15:04:06 -0400

    unit testing topword model _get_result continued

--- test/unit_test/test_topword_model.py
+++ test/unit_test/test_topword_model.py
@@ -68,10 +68,6 @@ class TestParaToGroup:
 
         assert test_results[1][0]['D'] == -2.1483
 
-        pd.testing.assert_series_equal(
-            left=test_results[1][1],
-            right=pd.Series([], name='Document "F2" compares to the whole corpus',
-                      dtype=None), check_names=False)
     def test_normal_case_header(self):
         assert test_topword_model.get_displayable_result().header \
                == "Compare Each Document to All the Documents As a Whole."
