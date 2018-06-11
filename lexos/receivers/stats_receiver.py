6a824389a7c27f887110f2f290b9b7055a225044
Krissa Cusanelli <cusanelli_krisandra@wheatoncollege.edu>
2018-06-11 10:45:00 -0400

    pydocstyle for stats receiver

--- lexos/receivers/stats_receiver.py
+++ lexos/receivers/stats_receiver.py
@@ -1,22 +1,27 @@
+"""This is the receiver for the stats model."""
+
 from typing import List, NamedTuple
 from lexos.receivers.base_receiver import BaseReceiver
 
 
 class StatsFrontEndOption(NamedTuple):
     """The typed tuple to hold stats front end option."""
+
     # This is the list of active file ids.
     active_file_ids: List[int]
 
 
 class StatsReceiver(BaseReceiver):
+    """This is the class that gets front end options for the stats model."""
+
     def __init__(self):
-        """The receiver to get stats front end options."""
+        """Get stats front end options using the receiver."""
         super().__init__()
 
     def options_from_front_end(self) -> StatsFrontEndOption:
-        """Get the option from front end.
+        """Get the options from front end.
 
-        The only option is selected files ids.
+        The only option is selected file ids.
         """
         # Get active file ids from front end as a string.
         active_file_ids_string = self._front_end_data["active_file_ids"]
