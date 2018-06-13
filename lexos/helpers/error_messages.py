98ccc274d8e2b53336d6967fe87b1e168789f955
Krissa Cusanelli <cusanelli_krisandra@wheatoncollege.edu>
2018-06-13 10:33:56 -0400

    cleaned up a bit of error messages

--- lexos/helpers/error_messages.py
+++ lexos/helpers/error_messages.py
@@ -1,18 +1,19 @@
+# ----------------------Cutter Model Error Messages---------------------------
 SEG_NON_POSITIVE_MESSAGE = "The segment size must be a positive integer"
-OVERLAP_LARGE_MESSAGE = "The segment size must be greater than " \
-    "the overlap of words between chunks"
-PROP_NEGATIVE_MESSAGE = "The proportional size of the last segment must be " \
-    "zero or greater"
-OVERLAP_NEGATIVE_MESSAGE = "The overlap between segments must be zero or " \
-                           "greater"
 NON_POSITIVE_SEGMENT_MESSAGE = "the segment size should be positive"
 NEG_OVERLAP_LAST_PROP_MESSAGE = "the overlap or last segment proportion" \
                                 "should not be negative"
 LARGER_SEG_SIZE_MESSAGE = "the segment size should be larger " \
     "than the overlap size"
+# ----------------------------------------------------------------------------
+
+
+# ----------------------Similarity Model Error Messages-----------------------
 NON_NEGATIVE_INDEX_MESSAGE = "the index should be larger than or equal to zero"
-MATRIX_DIMENSION_UNEQUAL_MESSAGE = "the dimension of the matrix " \
-    "should be equal"
+# ----------------------------------------------------------------------------
+
+
+# ----------------------Topwords Model Error Messages-------------------------
 EMPTY_LIST_MESSAGE = "the list should not be empty"
 WINDOW_SIZE_LARGE_MESSAGE = "The window size must be less than or equal to" \
                             " the length of the given document"
