4108e507bcd54360a9763e204d876d37fc274967
Krissa Cusanelli <cusanelli_krisandra@wheatoncollege.edu>
2018-06-13 12:01:10 -0400

    working with color scale rw model

--- lexos/models/rolling_windows_model.py
+++ lexos/models/rolling_windows_model.py
@@ -398,13 +398,16 @@ class RollingWindowsModel(BaseModel):
             go.Scattergl(
                 x=np.arange(len(row)),
                 y=row,
-                mode="lines",
                 name=token,
                 marker=dict(
                     line=dict(
-                        colorscale=[[0, 'rgb(0,0,0)', [1, 'rgb(128,128,128)']]]
-                    )
-                )
+                        cmax=39,
+                        cmin=0,
+                        color=[0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39],
+                        colorscale="Greys",
+                        width=4)
+                ),
+                mode="lines",
             ) for token, row in token_average_data_frame.iterrows()
         ]
 
