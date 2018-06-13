6ffd847d3154bca250f7dc564d3dcb118ba0d9cd
danielmullen <dpmullen98@gmail.com>
2018-06-13 13:50:15 -0400

    Removed a redundant error message.

--- lexos/static/js/scripts_cut.js
+++ lexos/static/js/scripts_cut.js
@@ -412,21 +412,6 @@ $(document).on('click', '#downloadCutting', function () {
  * Document ready function.
  */
 $(function () {
-  /*
-  const msg = 'You have no active documents. Please activate \
-  at least one document using the <a href="./manage">Manage</a> tool or\
-  <a href="./upload">upload</a> a new document.'
-
-  // When download, preview, or apply is clicked
-  $('#action-buttons').click(function () {
-    // check number of files
-    if ($('#num_active_files').val() < 1) {
-      // display error
-      $('#error-modal-message').html(msg)
-      $('#error-modal').modal()
-    }
-  })
-  */
   $('#actions').addClass('actions-cut')
 
   // Toggle cutting options when radio buttons with different classes are clicked
