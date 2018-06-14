ff2a291a946858899fb12d8253642ad4795a3ac0
Weiqi97 <weltch1997@gmail.com>
2018-06-14 16:19:48 -0400

    Update receiver and add more comments

--- lexos/receivers/kmeans_receiver.py
+++ lexos/receivers/kmeans_receiver.py
@@ -1,10 +1,21 @@
+"""This is the receiver for K-Means model."""
+
+from enum import Enum
 from typing import NamedTuple
-from lexos.models.filemanager_model import FileManagerModel
 from lexos.receivers.base_receiver import BaseReceiver
 
 
+class KMeansVisualizationOption(Enum):
+    """The typed tuple to hold K-Means Visualization method."""
+
+    PCA = "PCA"
+    Voronoi = "Voronoi"
+
+
 class KMeansOption(NamedTuple):
-    """The typed tuple to hold k means options."""
+    """The typed tuple to hold K-Means options."""
+
+    viz: KMeansVisualizationOption  # method of visualization.
     n_init: int  # number of iterations with different centroids.
     k_value: int  # k value-for k-means analysis. (k groups)
     max_iter: int  # maximum number of iterations.
@@ -13,24 +24,24 @@ class KMeansOption(NamedTuple):
 
 
 class KMeansReceiver(BaseReceiver):
+    """Class to get K-Means front end option."""
     def options_from_front_end(self) -> KMeansOption:
-        """Get the K-means option from front end.
+        """Get the K-Means option from front end.
 
         :return: a KMeansOption object to hold all the options.
         """
+        # Get all front end data.
+        viz = KMeansVisualizationOption.PCA \
+            if self._front_end_data['viz'] == "PCA" \
+            else KMeansVisualizationOption.Voronoi
         n_init = int(self._front_end_data['n_init'])
+        k_value = int(self._front_end_data['nclusters'])
         max_iter = int(self._front_end_data['max_iter'])
         tolerance = float(self._front_end_data['tolerance'])
         init_method = self._front_end_data['init']
 
-        # Check if no input from front-end, use the default k value.
-        try:
-            k_value = int(self._front_end_data['nclusters'])
-        except ValueError:
-            k_value = int(len(FileManagerModel().load_file_manager().
-                              get_active_files()) / 2)
-
-        return KMeansOption(n_init=n_init,
+        return KMeansOption(viz=viz,
+                            n_init=n_init,
                             k_value=k_value,
                             max_iter=max_iter,
                             tolerance=tolerance,
