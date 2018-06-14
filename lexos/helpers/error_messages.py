f34bef6afe9f4ee48ac545c8f6ebf7bef4c92726
Weiqi97 <weltch1997@gmail.com>
2018-06-14 10:47:18 -0400

    Remove overlapping error message

--- lexos/helpers/error_messages.py
+++ lexos/helpers/error_messages.py
@@ -1,10 +1,8 @@
 # ----------------------Cutter Model Error Messages---------------------------
-SEG_NON_POSITIVE_MESSAGE = "The segment size must be a positive integer"
-NON_POSITIVE_SEGMENT_MESSAGE = "the segment size should be positive"
-NEG_OVERLAP_LAST_PROP_MESSAGE = "the overlap or last segment proportion" \
-                                "should not be negative"
-LARGER_SEG_SIZE_MESSAGE = "the segment size should be larger " \
-    "than the overlap size"
+NEG_OVERLAP_LAST_PROP_MESSAGE = \
+    "the overlap or last segment proportion should not be negative"
+LARGER_SEG_SIZE_MESSAGE = \
+    "the segment size should be larger than the overlap size"
 INVALID_CUTTING_TYPE_MESSAGE = "the cutting type should be letters, lines, " \
     "number, words or milestone"
 EMPTY_MILESTONE_MESSAGE = "the milestone should not be empty"
@@ -17,9 +15,6 @@ NON_NEGATIVE_INDEX_MESSAGE = "the index should be larger than or equal to zero"
 
 
 # ----------------------Topword Model Error Messages--------------------------
-EMPTY_LIST_MESSAGE = "the list should not be empty"
-EMPTY_NP_ARRAY_MESSAGE = "The input numpy array should not be empty, since" \
-    "the input file should not be empty."
 NOT_ENOUGH_CLASSES_MESSAGE = "Only one class given, cannot do Z-test by " \
                              "class, at least 2 classes needed."
 # ----------------------------------------------------------------------------
@@ -33,17 +28,19 @@ WINDOW_NON_POSITIVE_MESSAGE = "The window size must be a positive integer"
 
 
 # ----------------------Scrubber Error Messages-------------------------------
-NOT_ONE_REPLACEMENT_COLON_MESSAGE = "Invalid number of colons: "
-REPLACEMENT_RIGHT_OPERAND_MESSAGE = "Too many values on right side of " \
-                                    "replacement string: "
-REPLACEMENT_NO_LEFTHAND_MESSAGE = "Missing value on the left side of " \
-                                  "replacement string: "
+NOT_ONE_REPLACEMENT_COLON_MESSAGE = "Invalid number of colons."
+REPLACEMENT_RIGHT_OPERAND_MESSAGE = \
+    "Too many values on right side of replacement string."
+REPLACEMENT_NO_LEFT_HAND_MESSAGE = \
+    "Missing value on the left side of replacement string."
 # ----------------------------------------------------------------------------
 
 
 # ======================== General Errors ====================================
+SEG_NON_POSITIVE_MESSAGE = "The segment size must be a positive integer."
 EMPTY_DTM_MESSAGE = "Empty DTM received, please upload files."
-
+EMPTY_LIST_MESSAGE = "The list should not be empty."
+EMPTY_NP_ARRAY_MESSAGE = "The input numpy array should not be empty."
 
 # ======================== Base Model Errors =================================
 NO_DATA_SENT_ERROR_MESSAGE = 'Front end did not send data to backend'
