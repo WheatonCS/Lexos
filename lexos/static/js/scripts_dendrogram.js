bfd643f2180ff42cc36e60c8396f45e4d2fd8aa3
Weiqi97 <weltch1997@gmail.com>
2018-06-19 16:24:02 -0400

    use getting submission error from utility in dendrogram

--- lexos/static/js/scripts_dendrogram.js
+++ lexos/static/js/scripts_dendrogram.js
@@ -1,24 +1,6 @@
 import * as utility from './utility.js'
 
 /**
- * the error message for the submission
- * @returns {string | null} - if it is null, it means no error, else then the string is the error message
- */
-function submissionError () {
-  const manageUrl = $('#manage-url').data().url
-  const uploadUrl = $('#upload-url').data().url
-  const activeFileNumTooFewErr = `You do not have enough active documents. 
-  Please activate at least two documents using the <a href=${manageUrl}>Manage</a> tool 
-  or <a href=${uploadUrl}>Upload</a> a new document.`
-  const activeFiles = $('#num_active_files').val()
-  if (activeFiles < 2) {
-    return activeFileNumTooFewErr
-  } else {
-    return null
-  }
-}
-
-/**
  * the function to submit form via ajax in dendrogram
  * @returns {void} - nothing is returned from this function.
  */
@@ -55,7 +37,7 @@ $(function () {
    * the events after dendrogram is clicked
    */
   $('#getdendro').on('click', function () {
-    const error = submissionError() // the error happens during submission
+    const error = utility.submissionError(2) // the error happens during submission
 
     if (error === null) { // if there is no error
       generateDendrogram()
