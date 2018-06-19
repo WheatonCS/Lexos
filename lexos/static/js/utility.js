5943fa953360e860a0baa0370d0e76b0e6296a6e
Weiqi97 <weltch1997@gmail.com>
2018-06-19 16:23:14 -0400

    add generating error message tool to utility

--- lexos/static/js/utility.js
+++ lexos/static/js/utility.js
@@ -1,5 +1,5 @@
 /**
- * The function to run the error modal.
+ * The function to run the error modal with input string.
  * @param {string} htmlMsg: the message to display.
  * @returns {void}.
  */
@@ -33,4 +33,30 @@ export function sendAjaxRequest (url, form) {
     contentType: 'application/json; charset=utf-8',
     data: JSON.stringify(form)
   })
+}
+
+/**
+ * At least one document is required to run the stats.
+ * @returns {string | null}: the errors that is checked by JS, if no error the result will be null.
+ */
+export function submissionError (numFileRequired) {
+  // Get the url for upload and manage page.
+  const uploadUrl = $('#upload-url').data().url
+  const manageUrl = $('#manage-url').data().url
+
+  // Set the error message
+  const activeFileNumTooFewErr = `You do not have enough active documents. Please 
+                                  activate at least ${numFileRequired} documents 
+                                  using the <a href=${manageUrl}>Manage</a> tool 
+                                  or <a href=${uploadUrl}>Upload</a> a new document.`
+
+  // Get the number of active files.
+  const activeFiles = $('#num-active-files').data().number
+
+  // Check if minimum number of required files exists.
+  if (activeFiles < numFileRequired) {
+    return activeFileNumTooFewErr
+  } else {
+    return null
+  }
 }
