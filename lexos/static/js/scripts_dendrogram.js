9d7de279b388547a67b3760a30a6854fa776c0b1
Weiqi97 <weltch1997@gmail.com>
2018-06-19 15:47:07 -0400

    Remove redundant function and use imports in dendrogram

--- lexos/static/js/scripts_dendrogram.js
+++ lexos/static/js/scripts_dendrogram.js
@@ -1,67 +1,4 @@
-/**
- * the function to convert the from into json
- * @returns {{string: string}} - the from converted to json
- */
-function jsonifyForm () {
-  const form = {}
-  $.each($('form').serializeArray(), function (i, field) {
-    form[field.name] = field.value || ''
-  })
-  return form
-}
-
-/**
- * the function to run the error modal
- * @param {string} htmlMsg  - the message to display, you can put html in it
- * @returns {void} - this function returns nothing.
- */
-function runModal (htmlMsg) {
-  $('#error-modal-message').html(htmlMsg)
-  $('#error-modal').modal()
-}
-
-/**
- * The function to create the ajax object
- * @param {object.<string, string>} form the form converted into a json
- * @returns {jquery.Ajax} - the jquery ajax object (a deferred object)
- */
-function sendAjaxRequest (form) {
-  return $.ajax({
-    type: 'POST',
-    url: '/dendrogramDiv',
-    contentType: 'application/json; charset=utf-8',
-    data: JSON.stringify(form)
-  })
-}
-
-/**
- * the function to submit form via ajax in dendrogram
- * @returns {void} - nothing is returned from this function.
- */
-function submitForm () {
-  // show loading icon
-  $('#status-analyze').css({'visibility': 'visible'})
-
-  // convert form into an object map string to string
-  const form = jsonifyForm()
-
-  // send the ajax request
-  sendAjaxRequest(form)
-    .done(
-      function (response) {
-        $('#dendrogram-result').html(response)
-      })
-    .fail(
-      function (jqXHR, textStatus, errorThrown) {
-        console.log('textStatus: ' + textStatus)
-        console.log('errorThrown: ' + errorThrown)
-        runModal('error encountered while plotting the dendrogram.')
-      })
-    .always(
-      function () {
-        $('#status-analyze').css({'visibility': 'hidden'})
-      })
-}
+import * as utility from './utility.js'
 
 /**
  * the error message for the submission
@@ -82,6 +19,35 @@ function submissionError () {
 }
 
 /**
+ * the function to submit form via ajax in dendrogram
+ * @returns {void} - nothing is returned from this function.
+ */
+function generateDendrogram () {
+  // show loading icon
+  $('#status-analyze').css({'visibility': 'visible'})
+
+  // convert form into an object map string to string
+  const form = utility.jsonifyForm()
+
+  // send the ajax request
+  utility.sendAjaxRequest('/dendrogramDiv', form)
+    .done(
+      function (response) {
+        $('#dendrogram-result').html(response)
+      })
+    .fail(
+      function (jqXHR, textStatus, errorThrown) {
+        console.log('textStatus: ' + textStatus)
+        console.log('errorThrown: ' + errorThrown)
+        utility.runModal('error encountered while plotting the dendrogram.')
+      })
+    .always(
+      function () {
+        $('#status-analyze').css({'visibility': 'hidden'})
+      })
+}
+
+/**
  * When the HTML documents finish loading
  */
 $(function () {
@@ -92,9 +58,9 @@ $(function () {
     const error = submissionError() // the error happens during submission
 
     if (error === null) { // if there is no error
-      submitForm()
+      generateDendrogram()
     } else {
-      runModal(error)
+      utility.runModal(error)
     }
   })
 })
