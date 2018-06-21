c578cd41ffb04ba1d93d78a6383330d29d97fc38
Weiqi97 <weltch1997@gmail.com>
2018-06-21 16:04:55 -0400

    Add filed set for mile stone color

--- lexos/static/js/scripts_rwanalysis.js
+++ lexos/static/js/scripts_rwanalysis.js
@@ -11,6 +11,25 @@ function updateMSopt () {
   }
 }
 
+function getSubmissionError () {
+  // get the number of active document
+  const numActiveDoc = Number($('#num_active_files').val())
+
+  // if there is no active document
+  if (numActiveDoc === 0)
+    return 'Please select a document to analyze.'
+  // no search pattern and window size
+  else if ($('#rollingwindowsize').val() === '' || $('#rollingsearchword').val() === '')
+    return 'Please fill out the \'Search Pattern(s)\' and \'Size of Rolling Window\' fields.'
+  // cannot search term using window of chars
+  else if ($('#inputword').prop('checked') && $('#windowletter').prop('checked'))
+    return 'You cannot use tokens for search terms when analyzing a window of characters. ' +
+      'The window setting has been changed to a window of tokens.'
+  // no error found
+  else
+    return null
+}
+
 /**
  * the function to submit form via ajax in dendrogram
  */
@@ -39,23 +58,40 @@ function generateRollingWindow () {
       })
 }
 
-function getSubmissionError () {
-  // get the number of active document
-  const numActiveDoc = Number($('#num_active_files').val())
+/**
+ * the function to submit form via ajax in dendrogram
+ */
+function displayMileStone () {
+  // show loading icon
+  $('#status-visualize').css({'visibility': 'visible'})
 
-  // if there is no active document
-  if (numActiveDoc === 0)
-    return 'Please select a document to analyze.'
-  // no search pattern and window size
-  else if ($('#rollingwindowsize').val() === '' || $('#rollingsearchword').val() === '')
-    return 'Please fill out the \'Search Pattern(s)\' and \'Size of Rolling Window\' fields.'
-  // cannot search term using window of chars
-  else if ($('#inputword').prop('checked') && $('#windowletter').prop('checked'))
-    return 'You cannot use tokens for search terms when analyzing a window of characters. ' +
-      'The window setting has been changed to a window of tokens.'
-  // no error found
-  else
-    return null
+  // convert form into an object map string to string
+  const form = utility.jsonifyForm()
+
+  // send the ajax request
+  utility.sendAjaxRequest('/rollingWindowMileStone', form)
+    .done(
+      function (response) {
+        if (response !== "") {
+          const mile_stones = response.map(function (mile_stone) {
+            return `<div class="col-sm-1 col-md-1 col-lg-1">
+                        <p style="color: ${mile_stone['color']}; font-size: 20px">${mile_stone['mile_stone']}</p>
+                    </div>`
+          })
+          $('#mile-stones').html(mile_stones)
+          $('#mile-stone-field').css('display', 'block')
+        }
+      })
+    .fail(
+      function (jqXHR, textStatus, errorThrown) {
+        console.log('textStatus: ' + textStatus)
+        console.log('errorThrown: ' + errorThrown)
+        utility.runModal('Error encountered while plotting the rolling window analysis.')
+      })
+    .always(
+      function () {
+        $('#status-visualize').css({'visibility': 'hidden'})
+      })
 }
 
 /* document.ready() Functions */
@@ -68,8 +104,11 @@ $(function () {
   $('#getgraph').click(function () {
     /* Validation */
     const errorString = getSubmissionError()
-    if (errorString === null)  // no error found
+    if (errorString === null) {
+      displayMileStone()
       generateRollingWindow()
+    }
+
     else
       utility.runModal(errorString)
   })
