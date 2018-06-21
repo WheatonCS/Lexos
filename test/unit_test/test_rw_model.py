8eb589177d2fb9a5203c4142dbd801fc4b89a232
Krissa Cusanelli <cusanelli_krisandra@wheatoncollege.edu>
2018-06-21 16:18:56 -0400

    updated rw unit test

--- test/unit_test/test_rw_model.py
+++ test/unit_test/test_rw_model.py
@@ -1,37 +1,42 @@
 from lexos.receivers.rolling_windows_receiver import RWAFrontEndOptions, \
     WindowUnitType, RWATokenType, RWARatioTokenOptions, RWAWindowOptions, \
-    RWAAverageTokenOptions
+    RWAAverageTokenOptions, RWAPlotOptions
 from lexos.models.rolling_windows_model import RollingWindowsModel, \
     RWATestOptions
 import numpy as np
 import pandas as pd
 
-
 # --------------------test by ratio count-----------------------------------
-test_ratio_count_one = RWATestOptions(file_id_content_map=
-                                      {0: "ha ha ha ha la ta ha",
-                                       2: "la la ta ta da da ha",
-                                       3: "ta da ha"
-                                       },
-                                      rolling_windows_options=
-                                      RWAFrontEndOptions
-                                      (ratio_token_options=RWARatioTokenOptions
-                                      (token_type=RWATokenType("string"),
-                                       numerator_token="t",
-                                       denominator_token="a"),
-                                       average_token_options=None,
-                                       passage_file_id=0,
-                                       window_options=RWAWindowOptions
-                                       (window_size=3, window_unit=
-                                       WindowUnitType("letter")),
-                                       milestone="ta"))
+test_ratio_count_one = RWATestOptions(
+    file_id_content_map={0: "ha ha ha ha la ta ha",
+                         2: "la la ta ta da da ha",
+                         3: "ta da ha"},
+    rolling_windows_options=RWAFrontEndOptions(
+        ratio_token_options=RWARatioTokenOptions(
+            token_type=RWATokenType.string,
+            token_frame=pd.DataFrame(data={
+                "numerator": ["t"],
+                "denominator": ["a"]
+            })
+        ),
+        average_token_options=None,
+        passage_file_id=0,
+        window_options=RWAWindowOptions(
+            window_size=3,
+            window_unit=WindowUnitType.letter
+        ),
+        plot_options=RWAPlotOptions(
+            individual_points=False,
+            black_white=False
+        ),
+        milestone="ta"
+    )
+)
 
 rw_ratio_model_one = RollingWindowsModel(test_option=test_ratio_count_one)
 
 # noinspection PyProtectedMember
 rw_ratio_windows = rw_ratio_model_one._get_windows()
-# noinspection PyProtectedMember
-basic_fig_ratio = rw_ratio_model_one._get_rwa_plotly()
 # ---------------------------------------------------------------------------
 # noinspection PyProtectedMember
 
@@ -47,51 +52,57 @@ class TestRatioCount:
     def test_token_ratio_windows(self):
         pd.testing.assert_series_equal(
             left=rw_ratio_model_one._find_token_ratio_in_windows(
-                rw_ratio_windows),
+                numerator_token="t", denominator_token="a",
+                windows=rw_ratio_windows),
             right=pd.Series(
                 data=[0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0,
-                      0.0, 0.0, 0.0, 1.0, 1.0, 1.0, 0.0, 0.0],
+                      0.0, 0.0, 0.0, .5, .5, .5, 0.0, 0.0],
             ), check_names=False)
 
-    def test_get_token_ratio_graph(self):
-        assert rw_ratio_model_one._get_token_ratio_graph()['type'] == \
-               'scattergl'
-        assert (rw_ratio_model_one._get_token_ratio_graph()['x'] ==
+    def test_generate_rwa_graph(self):
+        assert rw_ratio_model_one._generate_rwa_graph()['data'][0][
+                   'type'] == 'scattergl'
+        assert (rw_ratio_model_one._generate_rwa_graph()['data'][0]['x'] ==
                 [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15,
                  16, 17]).all()
-        assert rw_ratio_model_one._get_token_ratio_graph()['mode'] == 'lines'
-        assert rw_ratio_model_one._get_token_ratio_graph()['name'] == 't / a'
+        assert (rw_ratio_model_one._generate_rwa_graph()['data'][0]['y'] ==
+                [0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0,
+                 0.0, 0.0, 0.0, .5, .5, .5, 0.0, 0.0]).all()
 
     def test_find_milestone(self):
         assert rw_ratio_model_one.\
-                   _find_mile_stone_windows_indexes_in_all_windows\
-                   (rw_ratio_windows) == [15]
+                   _find_mile_stone_windows_indexes_in_all_windows(
+                    rw_ratio_windows) == {'t': [15], 'a': [1, 4, 7, 10, 13,
+                                                           16]}
 
 
 # --------------------test by average count-----------------------------------
-test_average_count_one = RWATestOptions(file_id_content_map=
-                                        {0: "ha ha ha ha la ta ha",
-                                         2: "la la ta ta da da ha",
-                                         3: "ta da ha"
-                                         },
-                                        rolling_windows_options=
-                                        RWAFrontEndOptions
-                                        (ratio_token_options=None,
-                                         average_token_options=
-                                         RWAAverageTokenOptions
-                                         (token_type=RWATokenType("string"),
-                                          tokens=["h"]),
-                                         passage_file_id=0,
-                                         window_options=RWAWindowOptions
-                                         (window_size=3, window_unit=
-                                         WindowUnitType("letter")),
-                                         milestone="ta"))
-rw_average_count_model_one = RollingWindowsModel \
-    (test_option=test_average_count_one)
+test_average_count_one = RWATestOptions(
+    file_id_content_map={0: "ha ha ha ha la ta ha",
+                         2: "la la ta ta da da ha",
+                         3: "ta da ha"},
+    rolling_windows_options=RWAFrontEndOptions(
+        ratio_token_options=None,
+        average_token_options=RWAAverageTokenOptions(
+            token_type=RWATokenType.string,
+            tokens=["ta", "ha"]),
+        passage_file_id=0,
+        window_options=RWAWindowOptions(
+            window_size=3,
+            window_unit=WindowUnitType.letter
+        ),
+        plot_options=RWAPlotOptions(
+            individual_points=False,
+            black_white=False
+        ),
+        milestone="ta"
+    )
+)
+rw_average_count_model_one = RollingWindowsModel(
+    test_option=test_average_count_one)
 # noinspection PyProtectedMember
 rw_average_windows = rw_average_count_model_one._get_windows()
-# noinspection PyProtectedMember
-basic_fig_average = rw_average_count_model_one._get_rwa_plotly()
+
 # ---------------------------------------------------------------------------
 # noinspection PyProtectedMember
 
@@ -105,28 +116,37 @@ class TestAverageCount:
 
     def test_token_average_windows(self):
         assert (rw_average_count_model_one._find_tokens_average_in_windows(
-            rw_average_windows).loc['h', 0:17] == [1 / 3, 1 / 3, 1 / 3, 1 / 3,
-                                                   1 / 3, 1 / 3, 1 / 3, 1 / 3,
-                                                   1 / 3, 1 / 3, 0.0, 0.0, 0.0,
-                                                   0.0, 0.0, 0.0, 1 / 3,
-                                                   1 / 3]).all()
+            rw_average_windows).loc['ta', 0:17] == [0, 0, 0, 0,
+                                                    0, 0, 0, 0,
+                                                    0, 0, 0.0, 0.0, 0.0,
+                                                    0.0, 1 / 3, 1 / 3, 0,
+                                                    0]).all()
+        assert (rw_average_count_model_one._find_tokens_average_in_windows(
+            rw_average_windows).loc['ha', 0:17] == [1 / 3, 0, 1 / 3, 1 / 3,
+                                                    0, 1 / 3, 1 / 3, 0,
+                                                    1 / 3, 1 / 3, 0.0, 0.0,
+                                                    0.0, 0.0, 0.0, 0.0, 0,
+                                                    1 / 3]).all()
 
-    def test_get_token_average_graph(self):
-        assert rw_average_count_model_one._get_token_average_graph()[0][
+    def test_generate_rwa_graph(self):
+        assert rw_average_count_model_one._generate_rwa_graph()['data'][0][
                    'type'] == 'scattergl'
-        assert (rw_average_count_model_one._get_token_average_graph()[0][
+        assert (rw_average_count_model_one._generate_rwa_graph()['data'][0][
                     'x'] ==
                 [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15,
                  16, 17]).all()
-        assert rw_average_count_model_one._get_token_average_graph()[0][
+        assert rw_average_count_model_one._generate_rwa_graph()['data'][1][
                    'mode'] == 'lines'
-        assert rw_average_count_model_one._get_token_average_graph()[0][
-                   'name'] == 'h'
+        assert rw_average_count_model_one._generate_rwa_graph()['data'][1][
+                   'name'] == 'ha'
+        assert rw_average_count_model_one._generate_rwa_graph()['data'][1][
+                   'line'] == {'color': 'rgb(55,126,184)'}
 
     def test_find_milestone(self):
-        assert rw_average_count_model_one.\
-                   _find_mile_stone_windows_indexes_in_all_windows\
-                   (rw_average_windows) == [15]
+        assert rw_average_count_model_one. \
+                   _find_mile_stone_windows_indexes_in_all_windows \
+                   (rw_average_windows) == {'t': [15],
+                                            'a': [1, 4, 7, 10, 13, 16]}
 
 
 # --------------------test static methods-------------------------------
@@ -150,6 +170,8 @@ rw_test_find_word = RollingWindowsModel._find_word_in_window(
 # noinspection PyProtectedMember
 rw_test_find_string = RollingWindowsModel._find_string_in_window(
     window="hello this the test the test", string="the test")
+
+
 # ---------------------------------------------------------------------------
 
 
@@ -177,37 +199,3 @@ class TestStaticMethods:
 
     def test_find_string(self):
         assert rw_test_find_string == 2
-
-
-class TestRWPlotly:
-    def test_get_rwa_ratio_plotly(self):
-        assert basic_fig_ratio['type'] == 'scattergl'
-
-        assert (basic_fig_ratio['x'] == [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11,
-                                         12, 13, 14, 15, 16, 17]).all()
-
-        assert (basic_fig_ratio['y'] == [0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0,
-                                         0.0, 0.0, 0.0,
-                                         0.0, 0.0, 0.0, 1.0, 1.0, 1.0, 0.0,
-                                         0.0]).all()
-
-        assert basic_fig_ratio['mode'] == 'lines'
-
-        assert basic_fig_ratio['name'] == 't / a'
-
-    def test_get_rwa_average_plotly(self):
-        assert basic_fig_average[0]['type'] == 'scattergl'
-
-        assert (basic_fig_average[0]['x'] == [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10,
-                                              11,
-                                              12, 13, 14, 15, 16, 17]).all()
-
-        assert (basic_fig_average[0]['y'] == [1 / 3, 1 / 3, 1 / 3, 1 / 3,
-                                              1 / 3, 1 / 3, 1 / 3, 1 / 3,
-                                              1 / 3, 1 / 3, 0.0, 0.0, 0.0,
-                                              0.0, 0.0, 0.0, 1 / 3,
-                                              1 / 3]).all()
-
-        assert basic_fig_average[0]['mode'] == 'lines'
-
-        assert basic_fig_average[0]['name'] == 'h'
