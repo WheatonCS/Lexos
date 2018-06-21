91a0716827989f70c4459d88334dda26ab46325d
Weiqi97 <weltch1997@gmail.com>
2018-06-21 17:08:17 -0400

    a little formatting fix

--- test/unit_test/test_rw_model.py
+++ test/unit_test/test_rw_model.py
@@ -1,10 +1,10 @@
+import numpy as np
+import pandas as pd
+from lexos.models.rolling_windows_model import RollingWindowsModel, \
+    RWATestOptions
 from lexos.receivers.rolling_windows_receiver import RWAFrontEndOptions, \
     WindowUnitType, RWATokenType, RWARatioTokenOptions, RWAWindowOptions, \
     RWAAverageTokenOptions, RWAPlotOptions
-from lexos.models.rolling_windows_model import RollingWindowsModel, \
-    RWATestOptions
-import numpy as np
-import pandas as pd
 
 # --------------------test by ratio count-----------------------------------
 test_ratio_count_one = RWATestOptions(
@@ -37,43 +37,48 @@ rw_ratio_model_one = RollingWindowsModel(test_option=test_ratio_count_one)
 
 # noinspection PyProtectedMember
 rw_ratio_windows = rw_ratio_model_one._get_windows()
+
+
 # ---------------------------------------------------------------------------
 # noinspection PyProtectedMember
-
-
 class TestRatioCount:
     def test_get_windows(self):
-        np.testing.assert_array_equal(rw_ratio_windows,
-                                      ['ha ', 'a h', ' ha', 'ha ', 'a h',
-                                       ' ha', 'ha ', 'a h', ' ha',
-                                       'ha ', 'a l', ' la', 'la ', 'a t',
-                                       ' ta', 'ta ', 'a h', ' ha'])
+        np.testing.assert_array_equal(
+            rw_ratio_windows,
+            ['ha ', 'a h', ' ha', 'ha ', 'a h', ' ha', 'ha ', 'a h', ' ha',
+             'ha ', 'a l', ' la', 'la ', 'a t', ' ta', 'ta ', 'a h', ' ha'])
 
     def test_token_ratio_windows(self):
         pd.testing.assert_series_equal(
             left=rw_ratio_model_one._find_token_ratio_in_windows(
-                numerator_token="t", denominator_token="a",
-                windows=rw_ratio_windows),
+                numerator_token="t",
+                denominator_token="a",
+                windows=rw_ratio_windows
+            ),
             right=pd.Series(
-                data=[0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0,
-                      0.0, 0.0, 0.0, .5, .5, .5, 0.0, 0.0],
-            ), check_names=False)
+                data=[0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0,
+                      0.0, 0.0, 0.0, 0.0, 0.5, 0.5, 0.5, 0.0, 0.0],
+            ),
+            check_names=False)
 
     def test_generate_rwa_graph(self):
-        assert rw_ratio_model_one._generate_rwa_graph()['data'][0][
-                   'type'] == 'scattergl'
-        assert (rw_ratio_model_one._generate_rwa_graph()['data'][0]['x'] ==
-                [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15,
-                 16, 17]).all()
-        assert (rw_ratio_model_one._generate_rwa_graph()['data'][0]['y'] ==
-                [0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0,
-                 0.0, 0.0, 0.0, .5, .5, .5, 0.0, 0.0]).all()
+        assert rw_ratio_model_one._generate_rwa_graph()['data'][0]['type'] == \
+               'scattergl'
+        np.testing.assert_array_equal(
+            rw_ratio_model_one._generate_rwa_graph()['data'][0]['x'],
+            [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17]
+        )
+
+        np.testing.assert_array_equal(
+            rw_ratio_model_one._generate_rwa_graph()['data'][0]['y'],
+            [0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0,
+             0.5, 0.5, 0.5, 0.0, 0.0]
+        )
 
     def test_find_milestone(self):
-        assert rw_ratio_model_one.\
-                   _find_mile_stone_windows_indexes_in_all_windows(
-                    rw_ratio_windows) == {'t': [15], 'a': [1, 4, 7, 10, 13,
-                                                           16]}
+        assert \
+            rw_ratio_model_one._find_mile_stone_windows_indexes_in_all_windows(
+                rw_ratio_windows) == {'t': [15], 'a': [1, 4, 7, 10, 13, 16]}
 
 
 # --------------------test by average count-----------------------------------
@@ -103,38 +108,39 @@ rw_average_count_model_one = RollingWindowsModel(
 # noinspection PyProtectedMember
 rw_average_windows = rw_average_count_model_one._get_windows()
 
+
 # ---------------------------------------------------------------------------
 # noinspection PyProtectedMember
 
 
 class TestAverageCount:
     def test_get_windows(self):
-        assert (rw_average_count_model_one._get_windows() ==
-                ['ha ', 'a h', ' ha', 'ha ', 'a h', ' ha', 'ha ', 'a h', ' ha',
-                 'ha ', 'a l', ' la', 'la ', 'a t', ' ta', 'ta ', 'a h',
-                 ' ha']).all()
+        np.testing.assert_array_equal(
+            rw_average_count_model_one._get_windows(),
+            ['ha ', 'a h', ' ha', 'ha ', 'a h', ' ha', 'ha ', 'a h', ' ha',
+             'ha ', 'a l', ' la', 'la ', 'a t', ' ta', 'ta ', 'a h', ' ha']
+        )
 
     def test_token_average_windows(self):
-        assert (rw_average_count_model_one._find_tokens_average_in_windows(
-            rw_average_windows).loc['ta', 0:17] == [0, 0, 0, 0,
-                                                    0, 0, 0, 0,
-                                                    0, 0, 0.0, 0.0, 0.0,
-                                                    0.0, 1 / 3, 1 / 3, 0,
-                                                    0]).all()
-        assert (rw_average_count_model_one._find_tokens_average_in_windows(
-            rw_average_windows).loc['ha', 0:17] == [1 / 3, 0, 1 / 3, 1 / 3,
-                                                    0, 1 / 3, 1 / 3, 0,
-                                                    1 / 3, 1 / 3, 0.0, 0.0,
-                                                    0.0, 0.0, 0.0, 0.0, 0,
-                                                    1 / 3]).all()
+        np.testing.assert_array_equal(
+            rw_average_count_model_one._find_tokens_average_in_windows(
+                rw_average_windows).loc['ta', 0:17],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1 / 3, 1 / 3, 0, 0]
+        )
+        np.testing.assert_array_equal(
+            rw_average_count_model_one._find_tokens_average_in_windows(
+                rw_average_windows).loc['ha', 0:17],
+            [1 / 3, 0, 1 / 3, 1 / 3, 0, 1 / 3, 1 / 3, 0, 1 / 3, 1 / 3, 0.0, 0,
+             0, 0, 0, 0, 0, 1 / 3]
+        )
 
     def test_generate_rwa_graph(self):
         assert rw_average_count_model_one._generate_rwa_graph()['data'][0][
                    'type'] == 'scattergl'
-        assert (rw_average_count_model_one._generate_rwa_graph()['data'][0][
-                    'x'] ==
-                [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15,
-                 16, 17]).all()
+        np.testing.assert_array_equal(
+            rw_average_count_model_one._generate_rwa_graph()['data'][0]['x'],
+            [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17]
+        )
         assert rw_average_count_model_one._generate_rwa_graph()['data'][1][
                    'mode'] == 'lines'
         assert rw_average_count_model_one._generate_rwa_graph()['data'][1][
@@ -143,10 +149,11 @@ class TestAverageCount:
                    'line'] == {'color': 'rgb(55,126,184)'}
 
     def test_find_milestone(self):
-        assert rw_average_count_model_one. \
-                   _find_mile_stone_windows_indexes_in_all_windows \
-                   (rw_average_windows) == {'t': [15],
-                                            'a': [1, 4, 7, 10, 13, 16]}
+        assert \
+            rw_average_count_model_one.\
+            _find_mile_stone_windows_indexes_in_all_windows(
+                rw_average_windows) == {'t': [15],
+                                        'a': [1, 4, 7, 10, 13, 16]}
 
 
 # --------------------test static methods-------------------------------
@@ -177,9 +184,10 @@ rw_test_find_string = RollingWindowsModel._find_string_in_window(
 
 class TestStaticMethods:
     def test_get_letters_window(self):
-        np.testing.assert_array_equal(rw_test_letters[0:9],
-                                      ['he', 'el', 'll', 'lo', 'o ', ' g',
-                                       'go', 'oo', 'od'])
+        np.testing.assert_array_equal(
+            rw_test_letters[0:9],
+            ['he', 'el', 'll', 'lo', 'o ', ' g', 'go', 'oo', 'od']
+        )
 
     def test_get_words_window(self):
         np.testing.assert_array_equal(rw_test_words[0:3],
