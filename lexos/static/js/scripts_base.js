9110ca75c0e6acf67a5414db4bf8b3830313d9e6
mathewleblanc <leblanc_mathew@wheatoncollege.edu>
2018-06-29 10:59:28 -0400

    added browser detection!_!
    peep my new mixtape
    
    soundcloud/thefakemathewjosephleblanc

--- lexos/static/js/scripts_base.js
+++ lexos/static/js/scripts_base.js
@@ -136,6 +136,7 @@ $(function () {
       window.location = window.location.pathname
     })
   })
+  browserDetection()
 })
 
 /**
@@ -177,7 +178,59 @@ function scrollTop () {
     }
   })
   $('#scroll').click(function () {
-    $('html, body').animate({ scrollTop: 0 }, 600)
+    $('html, body').animate({scrollTop: 0}, 600)
     return false
   })
+}
+
+/**
+ * Display a warning in an alert if using an unsupported browser. Supported
+ * browsers include Chrome and Firefox. This is hardcoded; a library would
+ * be much better as a long term solution as browsers are updated.
+ * @returns {void} - returns nothing
+ */
+function browserDetection () {
+
+  const browserWarningMsg = `You are currently using an unsupported browser.\
+  Some Lexos features may not function properly or be unavailable. Please\
+  use either Chrome or Firefox when using Lexos.`
+
+  // Opera 8.0+
+  const isOpera = (!!window.opr && !!opr.addons) || !!window.opera || navigator.userAgent.indexOf(' OPR/') >= 0
+
+  // Firefox 1.0+
+  const isFirefox = typeof InstallTrigger !== 'undefined'
+
+  // Safari 3.0+ "[object HTMLElementConstructor]"
+  const isSafari = /constructor/i.test(window.HTMLElement) || (function (p) { return p.toString() === '[object SafariRemoteNotification]' })(!window['safari'] || (typeof safari !== 'undefined' && safari.pushNotification))
+
+  // Internet Explorer 6-11
+  const isIE = /*@cc_on!@*/false || !!document.documentMode
+
+  // Edge 20+
+  const isEdge = !isIE && !!window.StyleMedia
+
+  // Chrome 1+
+  const isChrome = !!window.chrome && !!window.chrome.webstore
+
+  // If first time on page, show error message if browser is not supported
+  if (typeof(Storage) !== 'undefined') {
+    // check browser if user has not previously visited page
+    if (!sessionStorage.visited) {
+      if (isOpera) {
+        alert(browserWarningMsg)
+      }
+      else if (isSafari) {
+        alert(browserWarningMsg)
+      }
+      else if (isIE) {
+        alert(browserWarningMsg)
+      }
+      else if (isEdge) {
+        alert(browserWarningMsg)
+      }
+      // Set visited to true after the user visits the page
+      sessionStorage.visited = true
+    }
+  }
 }
