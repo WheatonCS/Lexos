3989d8f5c72037d1b18a9fa16c4dd8103ba4a2b0
Weiqi97 <weltch1997@gmail.com>
2018-08-04 00:11:26 -0400

    Add named tuple to hold options

---
+++ lexos/receivers/bct_receiver.py
@@ -0,0 +1,44 @@
+"""This is the receiver for the bootstrap consensus tree model."""
+
+from typing import NamedTuple
+from lexos.receivers.base_receiver import BaseReceiver
+
+
+class BCTOption(NamedTuple):
+    """The typed tuple to implement BCT options."""
+
+    # The method to find consensus among bootstrap trees.
+    # Available options are: 'top', 'right', 'bottom', or 'left', see:
+    # "http://biopython.org/DIST/docs/api/Bio.Phylo.Consensus-module.html"
+    consensus_method: str
+
+    # The distance metric to send to pdist
+    # See:
+    # "https://docs.scipy.org/doc/scipy/reference/generated/scipy.spatial.distance.pdist.html"
+    dist_metric: str
+
+    # the linkage method to send to scipy.cluster.hierarchy.linkage
+    # See:
+    # "https://docs.scipy.org/doc/scipy-0.14.0/reference/generated/scipy.cluster.hierarchy.linkage.html"
+    linkage_method: str
+
+
+class DendroReceiver(BaseReceiver):
+    """This is the class to receive dendro options."""
+
+    def __init__(self):
+        """Get all the dendrogram options using the receiver."""
+        super().__init__()
+
+    def options_from_front_end(self) -> DendroOption:
+        """Get the dendrogram option from front end.
+
+        :return: a DendroOption object to hold all the options.
+        """
+        orientation = self._front_end_data['orientation']
+        linkage_method = self._front_end_data['linkage']
+        metric = self._front_end_data['metric']
+
+        return DendroOption(orientation=orientation,
+                            linkage_method=linkage_method,
+                            dist_metric=metric)
