b8d72f5cf7883d2a237a210b39a92d9d73f84642
Weiqi97 <weltch1997@gmail.com>
2018-08-05 10:46:53 -0400

    Fix naming issue

--- lexos/static/js/scripts_bct.js
+++ lexos/static/js/scripts_bct.js
@@ -15,8 +15,8 @@ function generateBCT () {
   utility.sendAjaxRequest('/bct_analysis_result', form)
     .done(
       function (response) {
-        const static_url = $('#static-url').data().url
-        $('#bct-result').html(`<img src="${static_url}/${response}">`)
+        const staticUrl = $('#static-url').data().url
+        $('#bct-result').html(`<img src="${staticUrl}/${response}">`)
       })
     .fail(
       function () {
