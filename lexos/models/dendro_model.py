f300bdf53238457c2d52dbc2d3f788289a030d19
Weiqi97 <weltch1997@gmail.com>
2018-08-06 14:29:07 -0400

    Fix dendro method

--- lexos/models/dendro_model.py
+++ lexos/models/dendro_model.py
@@ -140,7 +140,7 @@ class DendrogramModel(BaseModel):
         dummy_scatter = self.get_dummy_scatter(x_value=x_value)
 
         # Add dummy scatter to the figure.
-        figure.add_trace(trace=dummy_scatter)
+        figure.data.append(dummy_scatter)
 
         # Return the formatted figure.
         return figure
