ec0b53d2d8fcdd84a997ee51469dac5db4e44724
Myles-Trevino <myles.trevino@gmail.com>
2019-05-15 22:37:20 -0400

    Make Tokenizer fully functional
    Added support for ordering by column, data orientation, showing and hiding columns, changing the number of terms per page. Additionally, the table is hidden until it loads, and the terms column is labelled.

--- lexos/views/tokenizer_view.py
+++ lexos/views/tokenizer_view.py
@@ -17,37 +17,45 @@ from lexos.helpers import constants as const
 # for more detail:
 # http://exploreflask.com/en/latest/blueprints.html
 # http://flask.pocoo.org/docs/0.12/blueprints/
-tokenizer_blueprint = Blueprint('tokenizer', __name__)
+tokenizer_blueprint = Blueprint("tokenizer", __name__)
 
 
-def get_session_dtm_options():
+def get_session_dtm_options(default_orientation: bool =
+                            True) -> Dict[str, object]:
+    """ Returns the cached options for the Tokenizer
+
+    :param default_orientation: Whether to use the default orientation
+    :return: The cached options for the Tokenizer
+    """
+
     return {
-        'cullnumber': session['analyoption']['cullnumber'],
-        'tokenType': session['analyoption']['tokenType'],
-        'normalizeType': session['analyoption']['normalizeType'],
-        'csvdelimiter': session['csvoptions']['csvdelimiter'],
-        'mfwnumber': '1',
-        'csvorientation': session['csvoptions']['csvorientation'],
-        'tokenSize': session['analyoption']['tokenSize'],
-        'norm': session['analyoption']['norm']
+        "cullnumber": session["analyoption"]["cullnumber"],
+        "tokenType": session["analyoption"]["tokenType"],
+        "normalizeType": session["analyoption"]["normalizeType"],
+        "csvdelimiter": session["csvoptions"]["csvdelimiter"],
+        "mfwnumber": "1",
+        "csvorientation": "filecolumn" if default_orientation else
+                          session["analyoption"]["csvorientation"],
+        "tokenSize": session["analyoption"]["tokenSize"],
+        "norm": session["analyoption"]["norm"]
     }
 
 
 def get_dtm_matrix(dtm_options: Dict[str, object],
                    file_manager: FileManager) -> List[list]:
-    """Gets the DTM matrix.
+    """Gets the DTM as a list of tuples.
 
     :param dtm_options: The options to use in generating the DTM.
     :param file_manager: The file manager to use.
-    :return: The DTM matrix.
+    :return: The DTM as a list of tuples.
     """
 
     start_time = timer()
-    dtm_matrix = utility.generate_csv_matrix_from_ajax(
+    dtm = utility.generate_csv_matrix_from_ajax(
         dtm_options, file_manager, round_decimal=True)
-    print("DTM matrix generation delta:", timer() - start_time)
+    print("DTM generation delta:", timer()-start_time)
 
-    return dtm_matrix
+    return dtm
 
 
 @tokenizer_blueprint.route("/tokenizer", methods=["GET", "POST"])
@@ -72,12 +80,12 @@ def tokenizer():
         header_labels = natsorted(header_labels)
 
         # Set the default session options
-        if 'analyoption' not in session:
-            session['analyoption'] = constants.DEFAULT_ANALYZE_OPTIONS
-        if 'csvoptions' not in session:
-            session['csvoptions'] = constants.DEFAULT_CSV_OPTIONS
+        if "analyoption" not in session:
+            session["analyoption"] = constants.DEFAULT_ANALYZE_OPTIONS
+        if "csvoptions" not in session:
+            session["csvoptions"] = constants.DEFAULT_CSV_OPTIONS
 
-        csv_orientation = session['csvoptions']['csvorientation']
+        csv_orientation = session["csvoptions"]["csvorientation"]
 
         # If there are active documents, generate the base DTM DataTable
         if num_active_docs > 0:
@@ -95,15 +103,12 @@ def tokenizer():
             row_labels = [dtm[i][0] for i in range(1, maximum_rows)]
 
             # Create a PANDAS DataFrame from the list of tuples
-            start_time = timer()
-            dtm_dataframe = pd.DataFrame(data, columns=column_labels,
-                                         index=row_labels)
-            print("CSV to PANDAS conversion delta:", timer() - start_time)
+            dtm_dataframe = pd.DataFrame(
+                data, columns=column_labels, index=row_labels)
+            dtm_dataframe.columns.name = "Terms"
 
             # Convert the DataFrame to HTML
-            start_time = timer()
             base_dtm_table_html = dtm_dataframe.to_html().replace('\n', '')
-            print("PANDAS to HTML table conversion delta:", timer()-start_time)
 
             # Get the number of rows in the DTM
             row_count = len(dtm_dataframe.index)
@@ -115,7 +120,7 @@ def tokenizer():
 
         # Render the page
         return render_template(
-            'tokenizer.html',
+            "tokenizer.html",
             draw=1,  # Used by DataTable
             itm="tokenize",
             labels=labels,
@@ -128,8 +133,7 @@ def tokenizer():
     # If a POST request was received
     if request.method == "POST":
         # If the "Download CSV" button was clicked, send the CSV file
-        if 'get-csv' in request.form:
-
+        if "get-csv" in request.form:
             print("Sending the CSV file for download.")
 
             save_path, file_extension = utility.generate_csv(file_manager)
@@ -147,7 +151,7 @@ def update_datatable():
     :return: The data requested by the DataTable.
     """
 
-    print("Received a DataTable request.")
+    print("Received a DataTable update request.")
 
     # Cache the set options
     session_manager.cache_analysis_option()
@@ -161,18 +165,20 @@ def update_datatable():
     starting_index = sent_data["start"]
     maximum_selection_size = sent_data["length"]
     search_term = sent_data["search"]["value"]
+    column_to_order_by = sent_data["order"][0]["column"]
+    descending_order = sent_data["order"][0]["dir"] != "asc"
 
     # Get the DTM, excluding the column labels
-    print("DTM Options:", get_session_dtm_options())
-    unfiltered_dtm = get_dtm_matrix(
-        get_session_dtm_options(), file_manager)[1:]
-    unfiltered_dtm_size = len(unfiltered_dtm)
+    dtm = get_dtm_matrix(get_session_dtm_options(), file_manager)[1:]
+    raw_dtm_size = len(dtm)
 
     # Apply the search term if there is one
     if search_term:
-        print("Applying search term: ", search_term)
-    dtm = [r for r in unfiltered_dtm if search_term in r[0]]
+        dtm = [r for r in dtm if search_term in r[0]]
     dtm_size = len(dtm)
+
+    # Apply ordering
+    dtm.sort(key=lambda t: t[column_to_order_by], reverse=descending_order)
 
     # Get the selection size
     selection_size = dtm_size-starting_index
@@ -186,8 +192,7 @@ def update_datatable():
     # Send the selection data back to the DataTable
     return json.dumps({
         "draw": int(sent_data.get("draw"))+1,  # DataTable wants 1 added
-        "recordsTotal": unfiltered_dtm_size,
+        "recordsTotal": raw_dtm_size,
         "recordsFiltered": dtm_size,
         "data": selection
     })
-
