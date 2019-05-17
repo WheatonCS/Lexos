fbffeffd06c21a349a16002f2b2ccb046ae0031c
Myles-Trevino <myles.trevino@gmail.com>
2019-05-16 21:36:08 -0400

    Rewrite upload page

--- lexos/static/js/scripts_upload.js
+++ lexos/static/js/scripts_upload.js
@@ -1,275 +1,171 @@
-/**
- * Alias function.
- * @return {object} element id object.
- * @param {string} id - id of an html element.
- */
-function $id (id) {
-  return document.getElementById(id)
-}
+let total_files = 0;
+let upload_count = 0;
 
 /**
  * Checks if the file type is valid.
  * @return {boolean} bool - true if file type is valid.
- * @param {string}  filename - name of the file.
+ * @param {string} filename - The name of the file.
  */
-function AllowedFileType (filename) {
-  const allowedFileTypes = ['txt', 'xml', 'html', 'sgml', 'lexos']
-  // Get the file file extension
-  let fileType = filename.split('.')[filename.split('.').length - 1]
-  // Search uploaded file's extension to the list 'allowedFileTypes'
-  // Return -1 if the file format is different from the list above
-  return $.inArray(fileType, allowedFileTypes) > -1
+function is_file_type_valid(filename){
+    const allowed_file_types = ["txt", "xml", "html", "sgml", "lexos"];
+
+    // Get the file file extension
+    let fileType = filename.split('.')[filename.split('.').length - 1];
+
+    // Return false if the file format is not in the list of allowed types
+    return $.inArray(fileType, allowed_file_types) > -1;
 }
 
-/**
- * File Drag Hover.
- * @return {void}
- * @param {object} e - event.
- */
-function fileDragHover (e) {
-  e.stopPropagation()
-  e.preventDefault()
-  e.target.className = (e.type === 'dragover' ? 'hover' : '')
-}
 
 /**
+ * Uploads the given file.
  * @return {void}
- * @description { Set progress bar back to default.
-  when a file is uploaded: bar changes to 'Complete'.
-  After the upload it set the bar back to 'Ready for Files to Upload'.
+ * @param {object} file - The file to upload.
  */
-function resetProgressBar () {
-  const progressBar = $('#progress-bar')
-  const status = $('#status')
-  progressBar.html('').css({'width': '0px'})
-  progressBar.show()
-  status.css('z-index', 50000)
-  status.show()
-}
+function upload_file(file){
 
-/**
- * Upload and display file contents.
- * @return {void}
- * @param {object} file - file object.
- * @param {int} fileSize - size of the File uploaded.
- */
-function UploadAndParseFile (file, fileSize) {
-  let filename = file.name.replace(/ /g, '_')
-  const status = $('#status-analyze')
-  // Make the loading icon circle visible
-  status.css({'visibility': 'visible'})
+    // Check if the file size is within limits
+    if(file.size < $("#MAX_FILE_SIZE").value) return;
 
-  if (AllowedFileType(file.name) && file.size <= fileSize) {
-    if (file.size === 0) {
-      $('#error-modal-message').html(`Cannot process blank file -- ${file.name}`)
-      $('#error-modal').modal()
-      status.css({'visibility': 'hidden'})
-    } else {
-      sendAjaxRequest(file, filename)
-        .done(function () {
-          let reader = new FileReader()
-          reader.onload = function (e) {
-            const template = $($('#file-preview-template').html())
-            template.find('.file-name').html(filename)
-            // Truncate the file name with css
-            template.find('.file-name').css({
-              'width': '90%',
-              'margin': 'auto',
-              'white-space': 'nowrap',
-              'overflow': 'hidden',
-              'text-overflow': 'ellipsis'
-            })
-            let fileType = ''
-            if (file.type === '') {
-              fileType = 'Lexos Workspace'
-              template.find('.file-information').find('.file-type').html(fileType)
-            } else {
-              template.find('.file-information').find('.file-type').html(file.type)
-              e.target.result.replace(/</g, '&lt;').replace(/>/g, '&gt;')
-            }
-            let fileSize = file.size
-            if (fileSize < 1024) {
-              template.find('.file-information').find('.file-size').html(`${file.size} bytes`)
-            } else if (file.size < 1048576) {
-              const fileSizeInKB = (file.size / 1024).toFixed(1)
-              template.find('.file-information').find('.file-size').html(`${fileSizeInKB} KB`)
-            } else {
-              const fileSizeInMB = (file.size / 1024 / 1024).toFixed(1)
-              template.find('.file-information').find('.file-size').html(`${fileSizeInMB} MB`)
-            }
-            $('#manage-previews').prepend(template)
-          }
-          reader.readAsText(file)
-          $('#activeDocIcon').css('display', 'block')
-          status.hide()
-        })
+    // Replace the file name's spaces with underscores
+    let file_name = file.name.replace(/ /g, '_');
 
-        .fail(function (jqXHR, textStatus, errorThrown) {
-          alert(`${textStatus} : ${errorThrown}`)
-          status.css({'visibility': 'hidden'})
-        })
-        .always(function () {
-          status.css({'visibility': 'hidden'})
-        })
+    // Check if the file is of an invalid type
+    if(!is_file_type_valid(file.name)){
+        $("#error-modal-message").html(`The file `+
+            `"${file_name}" is of an invalid type.`);
+        $("#error-modal").modal();
+        return;
     }
-  } else if (!AllowedFileType(file.name)) {
-    $('#error-modal-message').html(`Upload for  ${filename}  failed.\n\nInvalid file type.`)
-    $('#error-modal').modal()
-    // These are to hide the loading icon.
-    status.css({'visibility': 'hidden'})
-    status.css({'opacity': '0'})
-  } else {
-    // These are to hide the loading icon.
-    status.css({'visibility': 'hidden'})
-    status.css({'opacity': '0'})
-    const MAX_FILE_SIZE_INT = $('#MAX_FILE_SIZE_INT').val()
-    const MAX_FILE_SIZE_UNITS = $('#MAX_FILE_SIZE_UNITS').val()
-    $('#error-modal-message').html(`Upload for ${filename}  failed.\n\nFile bigger than
-     ${MAX_FILE_SIZE_INT} ${MAX_FILE_SIZE_UNITS}B`)
-    $('#error-modal').modal()
-    // Without this, it puts a blue background on the progress bar.
-    $('#progress').css('background', 'transparent')
-  }
+
+    // Check if the file is empty
+    if(file.size === 0){
+        $("#error-modal-message").html(`The file "${file_name}" is blank.`);
+        $("#error-modal").modal();
+        return;
+    }
+
+    // Upload the file
+    send_ajax_request(file, file_name)
+        .fail(function(jq_XHR, status, error)
+            { alert(`${status}: ${error}`); })
+        .done(function(){ upload_success_callback(file_name); });
 }
 
 /**
- * @return {ajax} ajax data.
- * @param {object} file - file object.
- * @param {string} filename - name of the file.
+ * Called when a file has successfully uploaded.
+ * @return {void}
  */
-function sendAjaxRequest (file, filename) {
+let uploaded_file_names = [];
+function upload_success_callback(file_name)
+{
+    // Update the document count and status elements
+    ++upload_count;
+
+    let active_documents_element = $("#active-document-count");
+    let active_documents = parseInt(active_documents_element.text())+1;
+    active_documents_element.text(active_documents.toString());
+
+    const status_element = $("#upload-status");
+    status_element.text(`${upload_count} of ${total_files} uploaded`);
+
+    // Update the uploaded documents list
+    uploaded_file_names.push(file_name);
+
+    let string = "";
+    for(const file_name of uploaded_file_names) string += file_name+", ";
+    string = string.substring(0, string.length-2);
+
+    $("#upload-list").text(string);
+}
+
+
+/**
+ * @return {ajax} The AJAX data.
+ * @param {object} file - The file object.
+ * @param {string} file_name - The name of the file.
+ */
+function send_ajax_request(file, file_name){
   return $.ajax({
-    type: 'POST',
+    type: "POST",
     url: document.URL,
     data: file,
     processData: false,
-    async: false,
+    async: false, /*Async results in errors*/
     contentType: file.type,
-    headers: {'X-FILENAME': encodeURIComponent(filename)}
-  })
+    headers: {"X-FILENAME": encodeURIComponent(file_name)}
+  });
 }
 
-/**
- * Drag and drop of files.
- * @return {void}
- * */
-function Init () {
-  const fileselect = $id('fileselect')
-  const filedrag = $id('dragndrop')
-
-  // File select
-  fileselect.addEventListener('change', FileSelectHandler, false)
-
-  // Is XHR2 available?
-  let xhr = new XMLHttpRequest()
-  if (xhr.upload) {
-    // file drop
-    filedrag.addEventListener('dragover', fileDragHover, false)
-    filedrag.addEventListener('dragleave', fileDragHover, false)
-    filedrag.addEventListener('drop', FileSelectHandler, false)
-  }
-}
 
 /**
- * file selection.
+ * Uploads the selected files.
  * @return {void}
- * @param {object} e - event.
+ * @param {object} event - The event.
  */
-function FileSelectHandler (e) {
-  const counter = $('#counter')
-  // Value of the input tag so that it gets the number of active files
-  let numberOfFileDone = parseInt(counter.val())
-  // cancel event and hover styling
-  fileDragHover(e)
-  // fetch FileList object
-  let files = e.target.files || e.dataTransfer.files
-  // Total number of files uploaded.
-  let totalFiles = files.length
-  // Resets the progress bar to defualt message after a file upload.
-  resetProgressBar()
+function file_selection_handler(event){
+    let files = event.target.files || event.originalEvent.dataTransfer.files;
+    total_files = files.length;
+    upload_count = 0;
 
-  const progress = $('#progress')
-  const progressBar = $('#progress-bar')
+    if(!files.length) return;
+    $("#upload-status").text("0 of "+total_files);
 
-  // Process all File objects
-  for (let f of files) {
-    let added = 0
-
-    if (f.size < $id('MAX_FILE_SIZE').value) {
-      numberOfFileDone += 1
-      added = 1
-    }
-    let fileSize = $id('MAX_FILE_SIZE').value
-    UploadAndParseFile(f, fileSize)
-    // Loading progress bar.
-    if (f.type === '') {
-      progress.html('Loading Workspace')
-    } else {
-      let calculatedWidth = String(180 * numberOfFileDone / totalFiles) + 'px'
-      progress.html(numberOfFileDone + 'of' + totalFiles).css('color', '#3498DB')
-
-      progressBar.css({'width': calculatedWidth})
-      if (numberOfFileDone / totalFiles > 0.5) {
-        progress.css('color', '#FFF')
-      }
-      progressBarStatus(f, added)
-      const faFolderOpen = $('.fa-folder-open-o')
-      faFolderOpen[0].dataset.originalTitle = `You have ${numberOfFileDone} active document(s)`
-      faFolderOpen.fadeIn(200)
-    }
-  }
-  showProgress()
-  // Convert the integer back to string and put it as a value in the input tag.
-  let numActiveFile = numberOfFileDone.toString()
-  counter.attr('value', numActiveFile)
-  $('#status').delay(1200).hide(0)
+    for(let file of files) upload_file(file);
 }
 
+
 /**
- * Displays the message on the 'progress' bar.
+ * Intercepts the file drag and drop events.
+ * @return {void}
+ * @param {object} event - The event.
+ */
+function drag_and_drop_intercept(event){
+  event.stopPropagation();
+  event.preventDefault();
+  event.target.className = (event.type === "dragover" ? "hover" : '');
+}
+
+
+/**
+ * Initializes callbacks.
  * @return {void}
  */
-function showProgress () {
-  $('#status').css('z-index', 50000).show()
-  $('#progress').html('Ready For Files To Upload').css('color', '#074178').delay(3000).show()
-  $id('fileselect').value = ''
-  // this allows the event to fire on "change" in chrome. the value property changing is the
-  // normal trigger, for some reason firefox overwrote this with their own behavior.
-}
-/**
- * Changes the message on the progress bar according to the file.
- * if file valid: "Complete!" else: "Invalid File!"
- * @return {void}
- * @param {string} f - name of the file.
- * @param {int} added - 1 id file is added.
- */
-function progressBarStatus (f, added) {
-  if (added === 1 && f.size < $id('MAX_FILE_SIZE').value && f.size !== 0 && AllowedFileType(f.name)) {
-    $('#progress-bar').html('Complete!').css({
-      'color': '#FFF',
-      'text-align': 'center',
-      'width': '175px',
-      'height': '20px'
-    }).fadeOut(2000)
-  } else {
-    $('#progress-bar').html('Invalid File!').css({
-      'color': '#FFF',
-      'text-align': 'center',
-      'width': '175px',
-      'height': '20px'
-    }).fadeOut(2000)
-  }
-}
+$(function(){
 
-$(function () {
-  // Message for when you hover on the open folder icon on the top right.
-  $('[data-toggle="tooltip"]').tooltip()
-  $('#uploadbrowse').click(function () {
-    $('#fileselect').click()
-  })
+    // Check for prerequisites
+    if(!window.File || !window.FileList ||
+        !window.FileReader || !new XMLHttpRequest()) return;
 
-  // Call initialization function
-  if (window.File && window.FileList && window.FileReader) {
-    Init()
-  }
+    // "Browse.." button input redirect
+    $("#browse-button").click(function(){ $("#file-select-input").click(); });
+
+    // File select input callback
+    $("#file-select-input").change(file_selection_handler);
+
+    // File drop section
+    const drag_and_drop_section = $("#drag-and-drop-section");
+    $("body").on("dragenter drop", drag_and_drop_intercept);
+    drag_and_drop_section.on("dragover dragenter", drag_and_drop_intercept);
+    drag_and_drop_section.on("drop", file_selection_handler);
+
+    //File drop section highlighting
+    let drag_counter = 0;
+    drag_and_drop_section.on("dragenter", function(){
+        ++drag_counter;
+        $("#drag-and-drop-section").css({
+            "color": "#FF0000", "border-color": "#FF0000"});
+    });
+
+    drag_and_drop_section.on("dragleave", function(){
+        if(--drag_counter) return;
+        $("#drag-and-drop-section").css({
+            "color": "#000000", "border-color": "#000000"});
+    });
+
+    drag_and_drop_section.on("drop", function(){
+        drag_counter = 0;
+        $("#drag-and-drop-section").css({
+            "color": "#000000", "border-color": "#000000"});
+    });
 })
