f05ec84f65774b48d4fcebc09258af9338549267
maggieshafer <shafer_margaret@wheatoncollege.edu>
2019-06-06 14:14:30 -0400

    Merged my statistics branch for old UI with this branch and made the graph look better with new UI

--- lexos/models/stats_model.py
+++ lexos/models/stats_model.py
@@ -1,6 +1,7 @@
 """This is the Stats model which gets basic statistics."""
 
 import pandas as pd
+import numpy as np
 import plotly.graph_objs as go
 from plotly import tools
 from plotly.offline import plot
@@ -99,6 +100,20 @@ class StatsModel(BaseModel):
             token_type = dtm_options.token_option.token_type
             return "terms" if token_type == "word" else "character n-grams"
 
+    def __get_quartile__(self, index: float, arr: np.array) -> float:
+        if index % 1 == .5:
+            ind = int(index)
+            return arr[ind] * .5 + arr[ind + 1] * .5
+        elif index % 1 == .25:
+            ind = int(index)
+            return arr[ind] * .75 + arr[ind + 1] * .25
+        elif index % 1 == .75:
+            ind = int(index)
+            return arr[ind] * .25 + arr[ind + 1] * .75
+        else:
+            ind = int(index)
+            return arr[ind]
+
     def get_corpus_stats(self) -> CorpusStats:
         """Convert word lists completely to statistic.
 
@@ -118,9 +133,16 @@ class StatsModel(BaseModel):
         # Get the standard deviation of the file word counts.
         std_deviation = file_sizes.std(axis="index")
 
+        # Get quartile indices
+        arr = np.array(file_sizes)
+        arr.sort()
+        length = len(arr)
+        q1_index = length * .25 + .5 - 1
+        q3_index = length * .75 + .5 - 1
+
         # Get the iqr of the file word counts.
-        first_quartile = file_sizes.quantile(0.25)
-        third_quartile = file_sizes.quantile(0.75)
+        first_quartile = self.__get_quartile__(index=q1_index, arr=arr)
+        third_quartile = self.__get_quartile__(index=q3_index, arr=arr)
         iqr = third_quartile - first_quartile
 
         # Standard error analysis: assume file sizes are normally distributed;
@@ -221,8 +243,9 @@ class StatsModel(BaseModel):
 
         # Set up the points.
         scatter_plot = go.Scatter(
-            x=[0 for _ in labels],
+            x=[_ for _ in labels],
             y=self._active_doc_term_matrix.sum(1).values,
+            name="Corpus Scatter Plot",
             hoverinfo="text",
             mode="markers",
             marker=dict(color="#47BCFF"),
@@ -238,32 +261,40 @@ class StatsModel(BaseModel):
         )
 
         # Create a figure with two subplots and fill the figure.
-        figure = tools.make_subplots(rows=1, cols=2, shared_yaxes=True)
+        figure = tools.make_subplots(rows=1, cols=2, shared_yaxes=False)
         figure.append_trace(trace=scatter_plot, row=1, col=1)
         figure.append_trace(trace=box_plot, row=1, col=2)
 
         # Hide useless information on x-axis and set up title.
         figure.layout.update(
             autosize=True,
-            height=340,
+            height=300,
             showlegend=False,
             margin=dict(
                 r=0,
-                b=0,
+                b=25,
                 t=0,
                 pad=4
             ),
             xaxis=dict(
+                title="Scatter Plot of Text Size",
                 showgrid=False,
                 zeroline=False,
-                showline=False,
+                showline=True,
                 showticklabels=False
             ),
+            yaxis=dict(
+                showline=True
+            ),
             xaxis2=dict(
+                title="Box Plot of Text Size",
                 showgrid=False,
                 zeroline=False,
-                showline=False,
+                showline=True,
                 showticklabels=False
+            ),
+            yaxis2=dict(
+                showline=True
             ),
             hovermode="closest"
         )
